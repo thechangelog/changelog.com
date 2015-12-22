eaea1b43d1967f9724d8a525227a7fe3b53f163e
Jerod Santo <jerod@changelog.com>
2015-12-22 13:45:28 -0600

    add `active_class` function and use it on admin nav

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule Changelog.LayoutView do
   use Changelog.Web, :view
+
+  def active_class(conn, controllers) when is_list(controllers) do
+    if Enum.member?(controllers, Phoenix.Controller.controller_module(conn)) do
+      "active"
+    else
+      ""
+    end
+  end
+
+  def active_class(conn, controllers) do
+    active_class(conn, [controllers])
+  end
 end
