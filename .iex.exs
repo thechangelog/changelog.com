984bf279165a4ace1affc767f8e93a3660e8671c
Jerod Santo <jerod@changelog.com>
2015-12-28 10:14:29 -0600

    helper function to update a record in the console

--- .iex.exs
+++ .iex.exs
@@ -4,3 +4,9 @@ alias Changelog.Person
 import Ecto
 import Ecto.Changeset
 import Ecto.Query, only: [from: 1, from: 2]
+
+defmodule H do
+  def update(model, id, attrs \\ %{}) do
+    Repo.update Ecto.Changeset.change(Repo.get(model, id), attrs)
+  end
+end
