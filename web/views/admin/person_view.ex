43b5863b0d1000321abb9c390115b83b823b6a25
Jerod Santo <jerod@changelog.com>
2015-12-29 10:49:51 -0600

    add avatar_url to people

--- web/views/admin/person_view.ex
+++ web/views/admin/person_view.ex
@@ -1,4 +1,22 @@
 defmodule Changelog.Admin.PersonView do
   use Changelog.Web, :view
   use Changelog.Helpers.ViewHelpers
+
+  def avatar_url(person) do
+    gravatar_url(person.email, 100)
+  end
+
+  def avatar_url(person, size) do
+    gravatar_url(person.email, size)
+  end
+
+  defp gravatar_url(email, size) do
+    hash = email
+      |> String.strip
+      |> String.downcase
+      |> :erlang.md5
+      |> Base.encode16(case: :lower)
+
+    "https://secure.gravatar.com/avatar/#{hash}.jpg?s=#{size}&d=mm"
+  end
 end
