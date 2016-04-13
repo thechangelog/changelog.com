50a6510e9d9011323281d699e8c5b185b9f767d9
Jerod Santo <jerod@changelog.com>
2016-04-13 12:04:23 -0500

    number episode titles when slug starts with a numeric

--- web/views/podcast_view.ex
+++ web/views/podcast_view.ex
@@ -1,3 +1,10 @@
 defmodule Changelog.PodcastView do
   use Changelog.Web, :view
+
+  def numbered_episode_title(episode) do
+    case Float.parse(episode.slug) do
+      {_, _} -> "#{episode.slug}: #{episode.title}"
+      :error -> episode.title
+    end
+  end
 end
