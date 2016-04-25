8248ede43e43bd7f12765d2b90792cef4d7905ba
Jerod Santo <jerod@changelog.com>
2016-04-25 11:47:04 -0500

    more IEX convenience functions

--- .iex.exs
+++ .iex.exs
@@ -5,6 +5,14 @@ import Ecto.Changeset
 import Ecto.Query, only: [from: 1, from: 2]
 
 defmodule H do
+  def get(Episode, id) do
+    Repo.get(Episode, id) |> Repo.preload(:podcast)
+  end
+
+  def get(model, id) do
+    Repo.get(model, id)
+  end
+
   def update(model, id, attrs \\ %{}) do
     Repo.update Ecto.Changeset.change(Repo.get(model, id), attrs)
   end
