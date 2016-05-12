005bbec11ce15780d4b5ada9b535eb3561e25ec3
Jerod Santo <jerod@changelog.com>
2016-05-12 16:49:07 -0500

    add status labels to posts, episodes

--- web/views/admin/episode_view.ex
+++ web/views/admin/episode_view.ex
@@ -6,4 +6,12 @@ defmodule Changelog.Admin.EpisodeView do
   def audio_filename(episode), do: EpisodeView.audio_filename(episode)
   def audio_url(episode), do: EpisodeView.audio_url(episode)
   def megabytes(episode), do: EpisodeView.megabytes(episode)
+
+  def status_label(episode) do
+    if episode.published do
+      content_tag :span, "Published", class: "ui tiny green label"
+    else
+      content_tag :span, "Draft", class: "ui tiny yellow label"
+    end
+  end
 end
--- web/views/admin/post_view.ex
+++ web/views/admin/post_view.ex
@@ -1,5 +1,13 @@
 defmodule Changelog.Admin.PostView do
   use Changelog.Web, :view
 
+  def status_label(post) do
+    if post.published do
+      content_tag :span, "Published", class: "ui tiny green label"
+    else
+      content_tag :span, "Draft", class: "ui tiny yellow label"
+    end
+  end
+
   import Scrivener.HTML
 end
