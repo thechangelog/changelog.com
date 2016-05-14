e978afd206d3de8ebfea974b37134551e2937ac1
Jerod Santo <jerod@changelog.com>
2016-05-14 15:06:43 -0500

    import old posts, assigning channels when they exist

--- import/import.rb
+++ import/import.rb
@@ -11,8 +11,27 @@ class Nokogiri::XML::Element
   end
 end
 
-class Tag < Struct.new(:id, :name, :slug); end
-class Author < Struct.new(:id, :name, :email, :handle); end
+class Author
+  attr_reader :el
+  def initialize(el); @el = el; end
+  def email; el.child_with_name("author_email").text; end
+  def handle; el.child_with_name("author_login").text; end
+  def id; el.child_with_name("author_id").text.to_i; end
+  def name; el.child_with_name("author_display_name").text; end
+end
+
+class Post
+  attr_reader :el
+  def initialize(el); @el = el; end
+  def author_handle; el.xpath("dc:creator").text; end
+  def body; el.xpath("content:encoded").text; end
+  def category; el.xpath("category[@domain='category']").text; end
+  def podcast?; category == "Podcast"; end
+  def published_at; DateTime.parse(el.child_with_name("pubDate").text); end
+  def slug; el.child_with_name("post_name").text; end
+  def title; el.child_with_name("title").text; end
+  def tags; el.xpath("category[@domain='post_tag']").map(&:text); end
+end
 
 class Importer
   def db
@@ -26,24 +45,11 @@ class Importer
   end
 
   def authors
-    @authors ||= doc.xpath("//channel/wp:author").map { |el|
-      Author.new(
-        el.child_with_name("author_id").text.to_i,
-        el.child_with_name("author_display_name").text,
-        el.child_with_name("author_email").text,
-        el.child_with_name("author_login").text
-      )
-    }
+    @authors ||= doc.xpath("//channel/wp:author").map { |el| Author.new el }
   end
 
-  def tags
-    @tags ||= doc.xpath("//channel/wp:tag").map { |el|
-      Tag.new(
-        el.child_with_name("term_id").text.to_i,
-        el.child_with_name("tag_name").text,
-        el.child_with_name("tag_slug").text
-      )
-    }
+  def posts
+    @posts ||= doc.xpath("//channel/item").map { |el| Post.new el }.reject(&:podcast?)
   end
 
   def import_people
@@ -58,13 +64,27 @@ class Importer
     end
   end
 
-  def import_topics
-    tags.each do |tag|
+  def import_posts
+    posts.each do |post|
       handling_data_issues do
-        db[:topics].insert({
-          name: tag.name,
-          slug: tag.slug
+        author_id = db[:people].where(handle: post.author_handle).first[:id]
+        post_id = db[:posts].insert({
+          title: post.title,
+          slug: post.slug,
+          published: true,
+          published_at: post.published_at,
+          body: post.body,
+          author_id: author_id
         }.merge(timestamps))
+
+        post.tags.each do |tag|
+          if channel = db[:channels].where(name: tag).first
+            db[:post_channels].insert({
+              channel_id: channel[:id],
+              post_id: post_id
+            }.merge(timestamps))
+          end
+        end
       end
     end
   end
@@ -87,5 +107,5 @@ end
 
 importer = Importer.new
 
-importer.import_topics
 importer.import_people
+importer.import_posts
