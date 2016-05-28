a0fa1484137334246978a3d657e1e1dca4862dec
Jerod Santo <jerod@changelog.com>
2016-05-28 10:19:10 -0500

    add episode importing via RSS feed

--- import/import.rb
+++ import/import.rb
@@ -6,30 +6,38 @@ require "sequel"
 require "pry"
 
 class Nokogiri::XML::Element
-  def child_with_name name
+  def child_named name
     self.children.find { |c| c.name == name }
   end
 end
 
-class Author
+class DataWrapper
   attr_reader :el
   def initialize(el); @el = el; end
-  def email; el.child_with_name("author_email").text; end
-  def handle; el.child_with_name("author_login").text; end
-  def id; el.child_with_name("author_id").text.to_i; end
-  def name; el.child_with_name("author_display_name").text; end
+  def published_at; DateTime.parse(el.child_named("pubDate").text); end
 end
 
-class Post
-  attr_reader :el
-  def initialize(el); @el = el; end
+class Author < DataWrapper
+  def email; el.child_named("author_email").text; end
+  def handle; el.child_named("author_login").text; end
+  def id; el.child_named("author_id").text.to_i; end
+  def name; el.child_named("author_display_name").text; end
+end
+
+class Episode < DataWrapper
+  def guid; el.child_named("guid").text; end
+  def slug; el.child_named("link").text.split("/").last; end
+  def summary; el.xpath("content:encoded").text; end
+  def title; el.child_named("title").text.gsub(/\A\d+:\s/, ""); end
+end
+
+class Post < DataWrapper
   def author_handle; el.xpath("dc:creator").text; end
   def body; el.xpath("content:encoded").text; end
   def category; el.xpath("category[@domain='category']").text; end
   def podcast?; category == "Podcast"; end
-  def published_at; DateTime.parse(el.child_with_name("pubDate").text); end
-  def slug; el.child_with_name("post_name").text; end
-  def title; el.child_with_name("title").text; end
+  def slug; el.child_named("post_name").text; end
+  def title; el.child_named("title").text; end
   def tags; el.xpath("category[@domain='post_tag']").map(&:text); end
 end
 
@@ -50,6 +58,32 @@ class Importer
 
   def posts
     @posts ||= doc.xpath("//channel/item").map { |el| Post.new el }.reject(&:podcast?)
+  end
+
+  def episodes
+    @episodes ||= doc.xpath("//channel/item").reverse.map { |el| Episode.new el }
+  end
+
+  def import_episodes
+    begin
+      podcast_id = db[:podcasts].where(slug: ENV["PODCAST_SLUG"]).first[:id]
+    rescue
+      abort "Must specify valid PODCAST_SLUG env var"
+    end
+
+    episodes.each do |episode|
+      handling_data_issues do
+        db[:episodes].insert({
+          podcast_id: podcast_id,
+          title: episode.title,
+          slug: episode.slug,
+          published: true,
+          published_at: episode.published_at,
+          recorded_at: episode.published_at,
+          summary: episode.summary
+        }.merge(timestamps))
+      end
+    end
   end
 
   def import_people
@@ -107,5 +141,6 @@ end
 
 importer = Importer.new
 
-importer.import_people
-importer.import_posts
+# importer.import_people
+# importer.import_posts
+# importer.import_episodes
