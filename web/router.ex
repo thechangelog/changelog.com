5d476626d958b09fce98809a4c5732501d39cdde
Jerod Santo <jerod@changelog.com>
2016-06-01 08:36:50 -0500

    add dev route for previewing sent emails

--- web/router.ex
+++ web/router.ex
@@ -1,6 +1,10 @@
 defmodule Changelog.Router do
   use Changelog.Web, :router
 
+  if Mix.env == :dev do
+    forward "/sent_emails", Bamboo.EmailPreviewPlug
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
