910c2a0c0f878f42ace0ded75efa369fd4e6ceaf
Jerod Santo <jerod@changelog.com>
2016-07-04 11:48:28 -0500

    require Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Changelog.Mixfile do
   def project do
     [app: :changelog,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
