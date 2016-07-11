23f5b7305657a4a0cf030c1730ea3d6875fd73f9
Jerod Santo <jerod@changelog.com>
2016-07-11 15:03:46 -0500

    get css/sass and semantic ui working with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 var webpack = require("webpack");
 var merge = require("webpack-merge");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var common = {
   module: {
     loaders: [
       {
         test: /\.js$/,
-        exclude: [/node_modules/, /vendor/],
+        exclude: [/node_modules/, /semantic/],
         loader: "babel",
         query: {
           presets: ["es2015"]
@@ -16,6 +17,18 @@ var common = {
       {
         test: /\.hbs$/,
         loader: "handlebars-loader"
+      },
+      {
+        test: [/\.sass$/, /\.css$/],
+        loader: ExtractTextPlugin.extract("style", "css!sass")
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "file?name=/images/[name].[ext]"
+      },
+      {
+        test: /\.(ttf|eot|svg|woff2?)$/,
+        loader: "file?name=/fonts/[name].[ext]",
       }
     ]
   },
@@ -26,26 +39,33 @@ var common = {
 
 module.exports = [
   merge(common, {
-    entry: "./web/static/js/app.js",
+    entry: [
+      "./web/static/css/app.css",
+      "./web/static/js/app.js"
+    ],
     output: {
-      path: "./priv/static/js",
-      filename: "app.js"
+      path: "./priv/static",
+      filename: "js/app.js"
     },
     plugins: [
-      new CopyWebpackPlugin([{ from: "./web/static/assets" }])
+      new CopyWebpackPlugin([{ from: "./web/static/assets"}]),
+      new ExtractTextPlugin("css/app.css")
     ]
   }),
   merge(common, {
     entry: [
-      "./web/static/vendor/semantic.js",
+      "./web/static/semantic/semantic.css",
+      "./web/static/semantic/semantic.js",
+      "./web/static/css/admin.css",
       "./web/static/js/admin.js"
     ],
     output: {
-      path: "./priv/static/js",
-      filename: "admin.js"
+      path: "./priv/static",
+      filename: "js/admin.js"
     },
     plugins: [
-      new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"})
+      new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"}),
+      new ExtractTextPlugin("css/admin.css")
     ]
   })
 ];
