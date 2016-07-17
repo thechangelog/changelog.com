380df221f4dc8d81705c4b89e38af7695b8b4f68
Jerod Santo <jerod@changelog.com>
2016-07-17 16:10:14 -0500

    more iex helpers

--- .iex.exs
+++ .iex.exs
@@ -5,15 +5,12 @@ import Ecto.Changeset
 import Ecto.Query, only: [from: 1, from: 2]
 
 defmodule H do
-  def get(Episode, id) do
-    Repo.get(Episode, id) |> Repo.preload(:podcast)
-  end
-
-  def get(model, id) do
-    Repo.get(model, id)
-  end
+  def get(Episode, id), do: Repo.get(Episode, id) |> Repo.preload(:podcast)
+  def get(model, id), do: Repo.get(model, id)
 
   def update(model, id, attrs \\ %{}) do
     Repo.update Ecto.Changeset.change(Repo.get(model, id), attrs)
   end
+
+  def delete(model, id), do:  Repo.delete get(model, id)
 end
