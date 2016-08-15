e1a069af395c228296c573598cc83953b7fba441
Jerod Santo <jerod@changelog.com>
2016-08-15 09:41:18 -0500

    make smart quotes on featured podcast content optional

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule Changelog.PageView do
   use Changelog.Web, :view
+
+  def with_smart_quotes(string) do
+    string
+    |> String.replace_leading("\"", "“")
+    |> String.replace_trailing("\"", "”")
+  end
 end
