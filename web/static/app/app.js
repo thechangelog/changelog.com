c4b302089ef8880639960582423c4f167731fd17
Jerod Santo <jerod@changelog.com>
2016-09-05 09:25:22 -0500

    change all “show” references to “podcast”

--- web/static/app/app.js
+++ web/static/app/app.js
@@ -5,7 +5,7 @@ import { makeToggles } from "./toggles.js";
   // Toggles
   makeToggles('.navigation-bar-link_dropdown', undefined, 'navigation-bar-link--is-open');
   makeToggles('.navigation-bar_menu-button', '.navigation-bar_tray', 'navigation-bar_tray--is-open');
-  makeToggles('.show-summary-widget_toggle', '.show-summary-widget_menu', 'show-summary-widget_menu--is-open');
+  makeToggles('.podcast-summary-widget_toggle', '.podcast-summary-widget_menu', 'podcast-summary-widget_menu--is-open');
 
   // Poppers
   makePoppers('.podcast-menu_more-button', '.podcast-menu-tooltip');
