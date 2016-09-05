2488de81ccbbf796c0dc70d32f146f33647908e6
Jerod Santo <jerod@changelog.com>
2016-09-05 15:53:23 -0500

    simplify JS, fixing toggles and poppers

--- web/static/app/app.js
+++ web/static/app/app.js
@@ -1,18 +1,19 @@
-import { makePoppers } from "./poppers.js";
-import { makeToggles } from "./toggles.js";
-
-(function(makePoppers, makeToggles){
-  // Toggles
-  makeToggles('.navigation-bar-link_dropdown', undefined, 'navigation-bar-link--is-open');
-  makeToggles('.navigation-bar_menu-button', '.navigation-bar_tray', 'navigation-bar_tray--is-open');
-  makeToggles('.podcast-summary-widget_toggle', '.podcast-summary-widget_menu', 'podcast-summary-widget_menu--is-open');
-
-  // Poppers
-  makePoppers('.podcast-menu_more-button', '.podcast-menu-tooltip');
-  makePoppers('.share-podcast_btn', '.share-podcast-tooltip');
-}(makePoppers, makeToggles));
-
-import Player from "components/player";
+import Popper from "popper.js";
 import { u } from "umbrellajs";
 
-window.player = new Player(u("#player"));
+u(".navigation-bar_menu-button").handle("click", function(event) {
+  u(".navigation-bar_tray").toggleClass("navigation-bar_tray--is-open");
+});
+
+u(".podcast-summary-widget_toggle").handle("click", function(event) {
+  u(this).siblings(".podcast-summary-widget_menu").toggleClass("podcast-summary-widget_menu--is-open");
+});
+
+u(".podcast-menu_more-button").each(function(node, i) {
+  const tooltip = u(node).siblings(".podcast-menu-tooltip").first();
+  new Popper(node, tooltip, {placement: "bottom"});
+
+  u(node).handle("click", function(event) {
+    u(tooltip).toggleClass("tooltip--is-open");
+  })
+});
