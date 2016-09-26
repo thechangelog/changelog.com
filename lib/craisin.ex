cec6e8cafbaf40de7f894f2f213ff387ab541309
Jerod Santo <jerod@changelog.com>
2016-09-26 08:53:39 -0500

    log when CM API doesn’t like our requests

--- lib/craisin.ex
+++ lib/craisin.ex
@@ -1,6 +1,8 @@
 defmodule Craisin do
   use HTTPoison.Base
 
+  require Logger
+
   @auth Base.encode64((System.get_env("CM_API_TOKEN") || "") <> ":x")
 
   defp process_url(url) do
@@ -14,6 +16,12 @@ defmodule Craisin do
   defp process_response_body(body), do: JSX.decode!(body)
 
   def handle({:ok, %HTTPoison.Response{status_code: 200, body: body}}), do: body
-  def handle({:ok, %HTTPoison.Response{status_code: 401}}), do: %{}
-  def handle({:error, %HTTPoison.Error{reason: reason}}), do: IO.puts(elem(reason, 1))
+  def handle({:ok, %HTTPoison.Response{status_code: 401, body: body}}) do
+    Logger.debug(body["Message"])
+    %{}
+  end
+  def handle({:error, %HTTPoison.Error{reason: reason}}) do
+    Logger.debug(elem(reason, 1))
+    %{}
+  end
 end
