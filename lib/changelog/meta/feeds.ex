e55200d6df7a8833e8e40fe204710f534acd8a1c
Jerod Santo <jerod@changelog.com>
2016-09-27 22:06:54 -0500

    new feeds upside ya <head>

--- lib/changelog/meta/feeds.ex
+++ lib/changelog/meta/feeds.ex
@@ -1,17 +1,28 @@
 defmodule Changelog.Meta.Feeds do
+  alias Changelog.{PostView}
+
   import Changelog.Router.Helpers
 
   def rss_feeds(assigns), do: assigns |> get
 
   defp get(%{podcast: podcast}) do
-    shared ++ [%{url: podcast_url(Changelog.Endpoint, :feed, podcast.slug), title: "#{podcast.name} Podcast Feed"}]
+    shared ++ [%{url: podcast_url(conn, :feed, podcast.slug), title: "#{podcast.name} Podcast Feed"}]
+  end
+
+  defp get(%{view_module: PostView}) do
+    shared ++ [%{url: post_url(conn, :feed), title: "Posts Feed"}]
   end
 
   defp get(_), do: shared
 
   defp shared do
     [
-      %{url: podcast_url(Changelog.Endpoint, :feed, "master"), title: "Changelog Master Feed (All Shows)"}
+      %{url: page_url(conn, :feed), title: "Fire Hose Feed (All Shows + All Posts)"},
+      %{url: podcast_url(conn, :feed, "master"), title: "Master Feed (All Shows)"}
     ]
+  end
+
+  defp conn do
+    Changelog.Endpoint
   end
 end
