b52d686e491b089039e7439d317f72ed76e815e2
Cody Peterson <cody@codyjamespeterson.com>
2016-09-29 10:49:31 -0700

    tweak slider height js

--- web/static/app/app.js
+++ web/static/app/app.js
@@ -28,6 +28,23 @@ u(document).handle("click", ".js-featured-previous", function(event) {
   featured.slide(-1);
 });
 
+function tallest_slide() {
+  let tallestFeatured = 0;
+  // 1. Set height of all .featured_podcast and .featured to "auto"
+  u(".featured_podcast_wrap, .featured").attr("height", "auto");
+
+  // 2. Find the tallest instance of .featured_podcast
+  u(".featured_podcast_wrap").each(function(el) {
+    let featuredHeight = u(el).size().height;
+    if (featuredHeight > tallestFeatured) {
+      tallestFeatured = featuredHeight;
+    }
+  });
+
+  // 3. Set height of all .featured_podcast and .featured to value found in step 2
+  u(".featured").attr("style", "height: " + tallestFeatured + "px;");
+}
+
 // On Page Load
 u(document).on("turbolinks:load", function() {
   u(".podcast-menu_more-button").each(function(node, i) {
@@ -42,24 +59,13 @@ u(document).on("turbolinks:load", function() {
       u(tooltip).toggleClass("tooltip--is-open");
     });
   });
+
+  tallest_slide();
 });
 
 // On Resize
 window.onresize = function() {
-  let tallestFeatured = 0;
-  // 1. Set height of all .featured_podcast and .featured to "auto"
-  u(".featured_podcast, .featured").attr("height", "auto");
-
-  // 2. Find the tallest instance of .featured_podcast
-  u(".featured_podcast").each(function(el) {
-    let featuredHeight = u(el).size().height;
-    if (featuredHeight > tallestFeatured) {
-      tallestFeatured = featuredHeight;
-    }
-  });
-
-  // 3. Set height of all .featured_podcast and .featured to value found in step 2
-  u(".featured_podcast, .featured").attr("height", tallestFeatured);
+  tallest_slide();
 }
 
 Turbolinks.start();
