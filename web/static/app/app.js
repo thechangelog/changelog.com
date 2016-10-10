ac6ca03b12d41cc5b69e51be8a685470057f4948
Jerod Santo <jerod@changelog.com>
2016-10-10 10:31:43 -0500

    clean up in aisle app.js

--- web/static/app/app.js
+++ web/static/app/app.js
@@ -35,15 +35,7 @@ u(document).on("click", "a[href^=http]", function(event) {
   }
 });
 
-u(document).handle("click", ".js-featured-next", function(event) {
-  featured.slide(+1);
-});
-
-u(document).handle("click", ".js-featured-previous", function(event) {
-  featured.slide(-1);
-});
-
-// Open a URL in a popup window (for Facebook sharing, etc.)
+// open share dialogs in their own window
 u(document).handle("click", ".js-share-popup", function(event) {
   var h, href, left, shareWindow, top, w;
   href = u(event.target).attr("href");
@@ -51,17 +43,19 @@ u(document).handle("click", ".js-share-popup", function(event) {
   h = 300;
   left = (screen.width / 2) - (w / 2);
   top = (screen.height / 2) - (h / 2);
-  shareWindow = window.open(href, "Changelog", "location=1,status=1,scrollbars=1, width=" + w + ",height=" + h + ",top=" + top + ",left=" + left);
+  shareWindow = window.open(href, "Changelog", `location=1,status=1,scrollbars=1,width=${w},height=${h},top=${top},left=${left}`);
   shareWindow.opener = null;
 });
 
-// Make sure all slider slides are the same height
+// handle featured sliders
+u(document).handle("click", ".js-featured-next", function(event) { featured.slide(+1); });
+u(document).handle("click", ".js-featured-previous", function(event) { featured.slide(-1); });
+
+// ensure all slider slides are the same height
 function tallestSlide() {
   let tallestFeatured = 0;
-  // 1. Set height of .featured to "auto"
   u(".featured").attr("height", "auto");
 
-  // 2. Find the tallest instance of .featured_podcast
   u(".featured_podcast_wrap").each(function(el) {
     let featuredHeight = u(el).size().height;
     if (featuredHeight > tallestFeatured) {
@@ -69,11 +63,14 @@ function tallestSlide() {
     }
   });
 
-  // 3. Set height of all .featured_podcast and .featured to value found in step 2
   u(".featured").attr("style", "height: " + tallestFeatured + "px;");
 }
 
-// On Page Load
+window.onresize = function() {
+  tallestSlide();
+}
+
+// on page load
 u(document).on("turbolinks:load", function() {
   u(".podcast-menu_more-button").each(function(node, i) {
     const tooltip = u(node).siblings(".podcast-menu-tooltip").first();
@@ -90,10 +87,5 @@ u(document).on("turbolinks:load", function() {
 
   tallestSlide();
 });
-
-// On Resize
-window.onresize = function() {
-  tallestSlide();
-}
 
 Turbolinks.start();
