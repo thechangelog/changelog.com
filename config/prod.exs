2a46773734741886e5e1e15d6f35360aae1d65c6
Jerod Santo <jerod@changelog.com>
2016-10-10 09:51:58 -0500

    disable production error debugging

--- config/prod.exs
+++ config/prod.exs
@@ -4,16 +4,11 @@ config :changelog, Changelog.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "2016.changelog.com", port: 443],
   static_url: [scheme: "https", host: "cdn.changelog.com", port: 443],
-  cache_static_manifest: "priv/static/manifest.json",
-  # remove once app is stable and ready to go live
-  debug_errors: true
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Temporary log debug, replace with commented logger once app is stable and ready to go live
 # config :logger, level: :info
 config :logger, :console, level: :debug, format: "[$level] $message\n"
-
-# Temporary production debug, remove once app is stable and ready to go live
-config :phoenix, :stacktrace_depth, 10
 
 config :arc,
   storage_dir: "/uploads"
@@ -29,6 +24,3 @@ config :changelog, Changelog.Mailer,
   port: 587,
   username: {:system, "CM_SMTP_TOKEN"},
   password: {:system, "CM_SMTP_TOKEN"}
-
-config :arc,
-  storage_dir: "/uploads"
