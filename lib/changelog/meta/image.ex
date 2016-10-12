fbd1de6c6337708ea59259f5b836f715409f23e9
Adam Stacoviak <adam@stacoviak.com>
2016-10-11 21:56:34 -0500

    Add share images for fb and twitter
    - Update share images to png vs jpg

--- lib/changelog/meta/image.ex
+++ lib/changelog/meta/image.ex
@@ -2,9 +2,9 @@ defmodule Changelog.Meta.Image do
   def fb_image(assigns), do: assigns |> get_fb
   def twitter_image(assigns), do: assigns |> get_twitter
 
-  defp get_fb(%{podcast: podcast}), do: "/images/share/#{podcast.slug}-fb.jpg"
-  defp get_fb(_), do: "/images/share/sitewide-fb.jpg"
+  defp get_fb(%{podcast: podcast}), do: "/images/share/#{podcast.slug}-fb.png"
+  defp get_fb(_), do: "/images/share/sitewide-fb.png"
 
-  defp get_twitter(%{podcast: podcast}), do: "/images/share/#{podcast.slug}-twitter.jpg"
-  defp get_twitter(_), do: "/images/share/sitewide-twitter.jpg"
+  defp get_twitter(%{podcast: podcast}), do: "/images/share/#{podcast.slug}-twitter.png"
+  defp get_twitter(_), do: "/images/share/sitewide-twitter.png"
 end
