908e144fb6c912cfea755e00f3ae94c580f15482
Jerod Santo <jerod@changelog.com>
2016-10-14 13:03:59 -0500

    error view tests are no longer relevant

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,21 +1,3 @@
 defmodule Changelog.ErrorViewTest do
   use Changelog.ConnCase, async: true
-
-  # Bring render/3 and render_to_string/3 for testing custom views
-  import Phoenix.View
-
-  test "renders 404.html" do
-    assert render_to_string(Changelog.ErrorView, "404.html", []) ==
-           "Page not found"
-  end
-
-  test "render 500.html" do
-    assert render_to_string(Changelog.ErrorView, "500.html", []) ==
-           "Server internal error"
-  end
-
-  test "render any other" do
-    assert render_to_string(Changelog.ErrorView, "505.html", []) ==
-           "Server internal error"
-  end
 end
