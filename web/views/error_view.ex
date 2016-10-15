78b31982190dbf6ec958c74cba45d1f5f0e09791
Jerod Santo <jerod@changelog.com>
2016-10-15 18:17:16 +0100

    simplify error pages further, fixing data missing bug

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,8 +1,6 @@
 defmodule Changelog.ErrorView do
   use Changelog.Web, :view
 
-  alias Changelog.LayoutView
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
