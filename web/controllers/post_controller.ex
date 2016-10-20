3e8f9b34183cc00bb5ec5eb733c1989f44746384
Jerod Santo <jerod@changelog.com>
2016-10-20 11:23:55 +0100

    add working posts index skeleton

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -5,13 +5,23 @@ defmodule Changelog.PostController do
 
   plug Changelog.Plug.RequireAdmin, "before preview" when action in [:preview]
 
+  def index(conn, params) do
+    page = Post
+    |> Post.published
+    |> order_by([p], desc: p.published_at)
+    |> preload(:author)
+    |> Repo.paginate(params)
+
+    render(conn, :index, posts: page.entries, page: page)
+  end
+
   def show(conn, %{"id" => slug}) do
     post = Repo.get_by!(Post, slug: slug, published: true) |> Post.preload_all
-    render conn, "show.html", post: post
+    render(conn, "show.html", post: post)
   end
 
   def preview(conn, %{"id" => slug}) do
     post = Repo.get_by!(Post, slug: slug) |> Post.preload_all
-    render conn, "show.html", post: post
+    render(conn, "show.html", post: post)
   end
 end
