3bc1afa7be61b71c951fd546d96363c8a0d187da
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-11-09 01:13:23 +0000

    try to make comma_separated_names a bit more elixiry

--- web/views/person_view.ex
+++ web/views/person_view.ex
@@ -39,22 +39,13 @@ defmodule Changelog.PersonView do
     |> Enum.join(", ")
   end
 
-  def comma_separated_names(people) when not is_list(people), do: comma_separated_names([])
-  def comma_separated_names(people) do
-    # I bet this can be more Elixirey by using head/tail and recursion, but I'm
-    # not quite there yet, because the tail case has 2 elements, which I can't
-    # quite figure out – JMS
-    case length(people) do
-      0 -> ""
-      1 -> List.first(people).name
-      2 -> "#{List.first(people).name} and #{List.last(people).name}"
-      _ ->
-        last = List.last(people)
-        rest = List.delete_at(people, -1)
-        commas = Enum.map(rest, &(&1.name)) |> Enum.join(", ")
-        "#{commas}, and #{last.name}"
-    end
-  end
+  @spec comma_separated_names([binary()]) :: binary()
+  def comma_separated_names(people)
+  def comma_separated_names([first]),                do: first.name
+  def comma_separated_names([first, second]),        do: "#{first.name} and #{second.name}"
+  def comma_separated_names([first, second, third]), do: "#{first.name}, #{second.name}, and #{third.name}"
+  def comma_separated_names([first | rest]),         do: "#{first.name}, #{comma_separated_names(rest)}"
+  def comma_separated_names(_unhandled),             do: ""
 
   def external_url(person) do
     cond do
