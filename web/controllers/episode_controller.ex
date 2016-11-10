ae86755fae5c8e49dd979f796547e32fad6f6eba
Cody Peterson <cody@codyjamespeterson.com>
2016-11-10 14:31:12 -0800

    create embed route and layout

--- web/controllers/episode_controller.ex
+++ web/controllers/episode_controller.ex
@@ -17,6 +17,20 @@ defmodule Changelog.EpisodeController do
     render(conn, :show, podcast: podcast, episode: episode)
   end
 
+  def embed(conn, %{"podcast" => podcast, "slug" => slug}) do
+    podcast = Repo.get_by!(Podcast, slug: podcast)
+
+    episode =
+      assoc(podcast, :episodes)
+      |> Episode.published
+      |> Repo.get_by!(slug: slug)
+      |> Episode.preload_all
+
+    conn
+    |> put_layout("embed.html")
+    |> render(:embed, podcast: podcast, episode: episode)
+  end
+
   def preview(conn, %{"podcast" => podcast, "slug" => slug}) do
     podcast = Repo.get_by!(Podcast, slug: podcast)
 
