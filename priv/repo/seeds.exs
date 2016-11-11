c2c745525b6c6736c3b3883e6aaa314e52f06f96
stve <steve.agalloco@gmail.com>
2016-11-11 00:11:28 -0500

    add Episode DB seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,7 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Changelog.Repo
-alias Changelog.{Person, Podcast}
+alias Changelog.{Episode, Person, Podcast}
 
 # People
 
@@ -77,3 +77,73 @@ rfc = Repo.insert! %Podcast{
   description: "Exploring different perspectives in open source sustainability. It's about the human side of code.",
   itunes_url: "https://itunes.apple.com/us/podcast/request-for-commits/id1141345001?mt=2"
 }
+
+# Podcast Episodes
+
+changelog_episode = Repo.insert! %Episode{
+  guid: "51",
+  slug: "51",
+  title: "The Road to Font Awesome 5 with Dave Gandy",
+  summary: "Dave Gandy joined the show to talk about the history of Font Awesome, what's to come in Font Awesome 5 and their Kickstarter to fund Font Awesome 5 Pro, and how everything they're doing is funneling back into the forever free and open source — Font Awesome Free.",
+  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
+  published: true,
+  published_at: Timex.now,
+  recorded_at: Timex.now,
+  bytes: 1234,
+  duration: 6840,
+  podcast: changelog
+}
+changelog_episode |> Ecto.Changeset.change(
+  audio_file: %{file_name: "web/static/assets/california.mp3", updated_at: Ecto.DateTime.utc}
+) |> Repo.update!
+
+founderstalk_episode = Repo.insert! %Episode{
+  guid: "51",
+  slug: "51",
+  title: "Sam Soffes / Onward",
+  summary: "TCould this be Sam's final appearance on Founders Talk? Only time will tell.",
+  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
+  published: true,
+  published_at: Timex.now,
+  recorded_at: Timex.now,
+  bytes: 1234,
+  duration: 6840,
+  podcast: founderstalk
+}
+founderstalk_episode |> Ecto.Changeset.change(
+  audio_file: %{file_name: "web/static/assets/california.mp3", updated_at: Ecto.DateTime.utc}
+) |> Repo.update!
+
+gotime_episode = Repo.insert! %Episode{
+  guid: "23",
+  slug: "23",
+  title: "Open Sourcing Chain's Developer Platform with Tess Rinearson",
+  summary: "Tess Rinearson joined the show to talk about Chain launching their open source developer platform, choosing an open source license, open sourcing Chain Core, and the future of this powerful blockchain written in Go.",
+  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
+  published: true,
+  published_at: Timex.now,
+  recorded_at: Timex.now,
+  bytes: 1234,
+  duration: 6840,
+  podcast: gotime
+}
+gotime_episode |> Ecto.Changeset.change(
+  audio_file: %{file_name: "web/static/assets/california.mp3", updated_at: Ecto.DateTime.utc}
+) |> Repo.update!
+
+rfc_episode = Repo.insert! %Episode{
+  guid: "9",
+  slug: "9",
+  title: "Open Source & Licenses with Heather Meeker",
+  summary: "Heather Meeker joined the show to talk about open source licensing, why open source licenses are historically significant, how much developers really need to know, and how much developers think they know. We also talk about mixing commercial and open source licenses, and how lawyers keep up with an ever-changing landscape.",
+  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
+  published: true,
+  published_at: Timex.now,
+  recorded_at: Timex.now,
+  bytes: 1234,
+  duration: 6840,
+  podcast: rfc
+}
+rfc_episode |> Ecto.Changeset.change(
+  audio_file: %{file_name: "web/static/assets/california.mp3", updated_at: Ecto.DateTime.utc}
+) |> Repo.update!
