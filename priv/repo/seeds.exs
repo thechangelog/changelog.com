5822c5c26f0d1f513050e75f27b0498c59036592
stve <steve.agalloco@gmail.com>
2016-11-11 00:09:26 -0500

    add People DB seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Changelog.Repo
+alias Changelog.{Person}
+
+# People
+
+Repo.insert! %Person{
+  name: "Adam Stacoviak",
+  email: "adam@stacoviak.com",
+  handle: "adamstac",
+  twitter_handle: "adamstac",
+  github_handle: "adamstac",
+  bio: "Founder & Editor-in-Chief of @Changelog",
+  website: "http://adamstacoviak.com/",
+  admin: true
+}
+
+Repo.insert! %Person{
+  name: "Jerod Santo",
+  email: "jerod.santo@gmail.com",
+  handle: "jerodsanto",
+  twitter_handle: "jerodsanto",
+  github_handle: "jerodsanto",
+  bio: "Professional `binding.pry` typer. Also @changelog, @objectlateral, @interfaceschool",
+  website: "http://jerodsanto.net/",
+  admin: true
+}
