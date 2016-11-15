3b5c83ec4deefbdc473413eef60db1ce5daf89e3
Jerod Santo <jerod@changelog.com>
2016-11-15 09:27:48 -0600

    add recent episode stats to admin dashboard

--- web/controllers/admin/page_controller.ex
+++ web/controllers/admin/page_controller.ex
@@ -3,6 +3,8 @@ defmodule Changelog.Admin.PageController do
 
   alias Changelog.{Episode, Newsletter, Post}
 
+  plug Changelog.Plug.LoadPodcasts, "index" when action in [:index]
+
   def index(conn, _params) do
     newsletters = [
       %Newsletter{name: "Weekly", list_id: "eddd53c07cf9e23029fe8a67fe84731f", web_id: "82E49C221D20C4F7"},
@@ -17,18 +19,20 @@ defmodule Changelog.Admin.PageController do
       %Newsletter{newsletter | stats: stats}
     end)
 
-    draft_episodes =
-      Episode.unpublished
-      |> Episode.newest_last(:recorded_at)
-      |> Repo.all
-      |> Episode.preload_podcast
-
-    draft_posts =
-      Post.unpublished
-      |> Post.newest_last(:inserted_at)
-      |> Repo.all
-      |> Post.preload_author
-
     render(conn, :index, newsletters: newsletters, draft_episodes: draft_episodes, draft_posts: draft_posts)
+  end
+
+  defp draft_episodes do
+    Episode.unpublished
+    |> Episode.newest_last(:recorded_at)
+    |> Repo.all
+    |> Episode.preload_podcast
+  end
+
+  defp draft_posts do
+    Post.unpublished
+    |> Post.newest_last(:inserted_at)
+    |> Repo.all
+    |> Post.preload_author
   end
 end
--- web/views/admin/page_view.ex
+++ web/views/admin/page_view.ex
@@ -1,5 +1,15 @@
 defmodule Changelog.Admin.PageView do
   use Changelog.Web, :view
 
-  alias Changelog.Admin.NewsletterView
+  alias Changelog.{Episode, Repo, TimewView}
+  alias Changelog.Admin.{NewsletterView, EpisodeView, PodcastView}
+
+  def recent_episodes(podcast, limit \\ 5) do
+    podcast
+    |> Ecto.assoc(:episodes)
+    |> Episode.published
+    |> Episode.newest_first
+    |> Episode.limit(limit)
+    |> Repo.all
+  end
 end
