31a887636f55ed1fa2245ea49f08dbef2db4e903
Jerod Santo <jerod@changelog.com>
2016-11-20 16:05:33 -0600

    stop logging podcast feed referers

--- web/controllers/feed_controller.ex
+++ web/controllers/feed_controller.ex
@@ -38,12 +38,6 @@ defmodule Changelog.FeedController do
       |> Repo.all
       |> Episode.preload_all
 
-    referer = get_req_header(conn, "referer")
-    if !is_nil(referer) do
-      agent = get_req_header(conn, "user-agent")
-      Logger.info("Feed referer for #{podcast.name}: #{referer} – #{agent}")
-    end
-
     conn
     |> put_layout(false)
     |> put_resp_content_type("application/xml")
