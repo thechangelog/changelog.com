d9f4fb403c61b00d33d21fa003006dbcd0a8fef1
Jerod Santo <jerod@changelog.com>
2016-11-23 14:27:07 -0600

    refactor downloads_by_os/downloads_by_browser functions

--- web/models/episode_stat.ex
+++ web/models/episode_stat.ex
@@ -29,14 +29,13 @@ defmodule Changelog.EpisodeStat do
     |> Enum.map(&(Map.get(&1.demographics, "agents")))
     |> Enum.map(fn(agents) ->
       agents
-      |> Enum.filter(fn({agent, _downloads}) -> String.match?(agent, ~r/^Mozilla\//) end)
-      |> Enum.reduce(%{}, fn({agent, downloads}, acc) ->
-          browser = case UserAgentParser.detect_browser(agent) do
-            %UA.Browser{family: family} -> family
-            :unknown -> "Unknown"
-          end
-          Map.update(acc, browser, downloads, &(&1 + downloads))
-        end)
+      |> browsers_agents_only
+      |> group_agents_by(fn(agent) ->
+        case UserAgentParser.detect_browser(agent) do
+          %UA.Browser{family: family} -> family
+          :unknown -> "Unknown"
+        end
+      end)
     end)
     |> downloads_list_merged_and_sorted
   end
@@ -68,14 +67,13 @@ defmodule Changelog.EpisodeStat do
     |> Enum.map(&(Map.get(&1.demographics, "agents")))
     |> Enum.map(fn(agents) ->
       agents
-      |> Enum.filter(fn({agent, _downloads}) -> String.match?(agent, ~r/^Mozilla\//) end)
-      |> Enum.reduce(%{}, fn({agent, downloads}, acc) ->
-          os = case UserAgentParser.detect_os(agent) do
-            %UA.OS{family: family} -> family
-            :unknown -> "Unknown"
-          end
-          Map.update(acc, os, downloads, &(&1 + downloads))
-        end)
+      |> browsers_agents_only
+      |> group_agents_by(fn(agent) ->
+        case UserAgentParser.detect_os(agent) do
+          %UA.OS{family: family} -> family
+          :unknown -> "Unknown"
+        end
+       end)
     end)
     |> downloads_list_merged_and_sorted
   end
@@ -85,5 +83,16 @@ defmodule Changelog.EpisodeStat do
     list
     |> Enum.reduce(fn(x, acc) -> Map.merge(acc, x, fn(_k, v1, v2) -> v1 + v2 end) end)
     |> Enum.sort(&(elem(&1, 1) > elem(&2, 1)))
+  end
+
+  defp browsers_agents_only(agents) do
+    agents
+    |> Enum.filter(fn({agent, _downloads}) -> String.match?(agent, ~r/^Mozilla\//) end)
+  end
+
+  defp group_agents_by(agents, groupingFn) do
+    Enum.reduce(agents, %{}, fn({agent, downloads}, acc) ->
+      Map.update(acc, groupingFn.(agent), downloads, &(&1 + downloads))
+    end)
   end
 end
