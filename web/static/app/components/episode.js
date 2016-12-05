d9c205cd4a6a1554f919fd57b611e5fc5f90a23b
Jerod Santo <jerod@changelog.com>
2016-12-05 08:35:38 -0600

    include hours when formatting episode time

--- web/static/app/components/episode.js
+++ web/static/app/components/episode.js
@@ -1,8 +1,18 @@
 export default class Episode {
   static formatTime(secs) {
-    const minutes = Math.floor(secs / 60) || 0;
-    const seconds = (secs - minutes * 60) || 0;
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    let hours = Math.floor(secs / 3600) || 0;
+    let minutes = Math.floor(secs / 60) || 0;
+    let seconds = (secs - minutes * 60) || 0;
+    let formatted = "";
+
+    if (hours > 0) {
+      minutes = (minutes - hours * 60) || 0;
+      formatted += `${hours}:${(minutes < 10 ? "0" : "")}`;
+    }
+
+    formatted += `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
+
+    return formatted;
   }
 
   constructor(data) {
