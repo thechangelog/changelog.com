9d1e3b477224c8bf9c3e71498a7791582e8b5b71
Jerod Santo <jerod@changelog.com>
2016-12-06 22:52:26 -0600

    simplify setting of twitter card type, fixes /podcasts card

--- lib/changelog/meta/twitter.ex
+++ lib/changelog/meta/twitter.ex
@@ -1,10 +1,9 @@
 defmodule Changelog.Meta.Twitter do
-  alias Changelog.{EpisodeView, PageView, PodcastView}
+  alias Changelog.{PageView}
 
   def twitter_card_type(assigns), do: assigns |> get
 
-  defp get(%{view_module: EpisodeView}), do: "summary_large_image"
-  defp get(%{view_module: PodcastView}), do: "summary_large_image"
+  defp get(%{podcast: _podcast}), do: "summary_large_image"
   defp get(%{view_module: PageView, view_template: "home.html"}), do: "summary_large_image"
   defp get(_), do: "summary"
 end
