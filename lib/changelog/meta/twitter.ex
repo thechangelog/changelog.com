27ee4a17bdb62c625775862044c7d69e658b4236
Jerod Santo <jerod@changelog.com>
2016-12-09 13:47:06 -0600

    simplify share images: use podcast cover art for now

--- lib/changelog/meta/twitter.ex
+++ lib/changelog/meta/twitter.ex
@@ -3,7 +3,6 @@ defmodule Changelog.Meta.Twitter do
 
   def twitter_card_type(assigns), do: assigns |> get
 
-  defp get(%{podcast: _podcast}), do: "summary_large_image"
   defp get(%{view_module: PageView, view_template: "home.html"}), do: "summary_large_image"
   defp get(_), do: "summary"
 end
