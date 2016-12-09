115a43fff360cf61139b49dbf34ec309d872efb5
Dennis Reimann <mail@dennisreimann.de>
2016-12-09 14:20:45 +0100

    add Episode Sponsors DB seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,16 +11,18 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Changelog.Repo
-alias Changelog.{Episode, EpisodeGuest, EpisodeHost, Person, Podcast, PodcastHost}
+alias Changelog.{Episode, EpisodeGuest, EpisodeHost, EpisodeSponsor, Person, Podcast, PodcastHost, Sponsor}
 
 # Clean slate
 
+Repo.delete_all EpisodeSponsor
 Repo.delete_all EpisodeGuest
 Repo.delete_all EpisodeHost
 Repo.delete_all Episode
 Repo.delete_all PodcastHost
 Repo.delete_all Podcast
 Repo.delete_all Person
+Repo.delete_all Sponsor
 
 # People
 
@@ -234,15 +236,63 @@ rfc = Repo.insert! %Podcast{
   ]
 }
 
+# Sponsors
+
+toptal = Repo.insert! %Sponsor{
+  name: "TopTal",
+  description: "Toptal is an exclusive network of the top freelance software developers, designers, and finance experts in the world.
+Top companies rely on Toptal freelancers for their most important projects.",
+  website: "https://www.toptal.com/",
+  twitter_handle: "toptal"
+}
+
+hackerparadise = Repo.insert! %Sponsor{
+  name: "Hacker Paradisse",
+  description: "Travel the world. Build cool things. Meet awesome people.",
+  website: "http://www.hackerparadise.org/",
+  twitter_handle: "hackerparadise"
+}
+
+gocd = Repo.insert! %Sponsor{
+  name: "GoCD",
+  description: "Simplify Continuous Delivery: Open source continuous delivery server to model and visualize complex workflows with ease.",
+  website: "https://www.go.cd/",
+  github_handle: "gocd",
+  twitter_handle: "goforcd"
+}
+
+rollbar = Repo.insert! %Sponsor{
+  name: "Rollbar",
+  description: "Rollbar helps engineering teams stay focused, be more productive and deliver exceptional error-free experiences.",
+  website: "https://rollbar.com/",
+  github_handle: "rollbar",
+  twitter_handle: "rollbar"
+}
+
+linode = Repo.insert! %Sponsor{
+  name: "Linode",
+  description: "Cloud Hosting for Developers: High performance SSD Linux servers for all of your infrastructure needs.",
+  website: "https://linode.com/",
+  github_handle: "linode",
+  twitter_handle: "linode"
+}
+
+codeschool = Repo.insert! %Sponsor{
+  name: "Code School",
+  description: "Learn by Doing: Interactive programming courses and guided projects.",
+  website: "https://www.codeschool.com/",
+  twitter_handle: "codeschool"
+}
+
 # Podcast Episodes with stub audio file
 
 audio_file = %{file_name: "web/static/assets/california.mp3", updated_at: Ecto.DateTime.utc}
 
 Repo.insert! %Episode{
-  slug: "231",
-  title: "HTTP/2 in Node.js Core",
-  summary: "In this special episode recorded at Node Interactive 2016 in Austin, TX Adam talked with James Snell (IBM Technical Lead for Node and member of Node's TSC and CTC) about the work he's doing on Node's implementation of http2, the state of http2 in Node, what this new spec has to offer, and what the Node community can expect from this new protocol.",
-  notes: "This episode is a preview of our upcoming \"Future of Node.js\" series recorded at Node Interactive 2016 in Austin, TX. The series is produced in partnership with Node.js Foundation and sponsored by IBM. We'll be releasing the full series soon on our new podcast \"Spotlight\". If you haven't subscribed to Changelog Master yet, which includes all the podcasts we produce, now would be a good time to do so. You can also subscribe to Changelog Weekly where we announce all the new things we're up to.",
+  slug: "226",
+  title: "The Road to Font Awesome 5 with Dave Gandy",
+  summary: "Dave Gandy joined the show to talk about the history of Font Awesome, what's to come in Font Awesome 5 and their Kickstarter to fund Font Awesome 5 Pro, and how everything they're doing is funneling back into the forever free and open source — Font Awesome Free.",
+  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
   published: true,
   published_at: Timex.now,
   recorded_at: Timex.now,
@@ -253,13 +303,36 @@ Repo.insert! %Episode{
   episode_guests: [
     %EpisodeGuest{
       position: 1,
-      person: jasnell
+      person: davegandy
     }
   ],
   episode_hosts: [
     %EpisodeHost{
       position: 1,
       person: adamstac
+    }
+  ],
+  episode_sponsors: [
+    %EpisodeSponsor{
+      position: 1,
+      sponsor: codeschool,
+      title: "Code School",
+      link_url: "https://www.codeschool.com/changelog",
+      description: "Learn to program by doing with hands-on courses. Save $10 per month! Sign up for Code School for only $19 per month by using our special URL [codeschool.com/changelog](https://www.codeschool.com/changelog)."
+    },
+    %EpisodeSponsor{
+      position: 2,
+      sponsor: linode,
+      title: "Linode",
+      link_url: "https://linode.com/rfc",
+      description: "*Our cloud server of choice!* Get one of the fastest, most efficient SSD cloud servers for only $10/mo. We host everything we do on Linode servers. Use the code `changelog20` to get 2 months free!"
+    },
+    %EpisodeSponsor{
+      position: 3,
+      sponsor: rollbar,
+      title: "Rollbar",
+      link_url: "https://rollbar.com/changelog",
+      description: "Put errors in their place! Full-stack error tracking for all apps in any language. "
     }
   ]
 }
@@ -291,14 +364,30 @@ Repo.insert! %Episode{
       position: 2,
       person: jerodsanto
     }
+  ],
+  episode_sponsors: [
+    %EpisodeSponsor{
+      position: 1,
+      sponsor: gocd,
+      title: "GoCD",
+      link_url: "https://www.go.cd/changelog",
+      description: "GoCD is an on-premise open source continuous delivery server created by ThoughtWorks that lets you automate and streamline your build-test-release cycle for reliable, continuous delivery of your product. "
+    },
+    %EpisodeSponsor{
+      position: 2,
+      sponsor: toptal,
+      title: "TopTal",
+      link_url: "http://www.toptal.com/?utm_source=changelog&utm_medium=podcast&utm_campaign=changelog-sponsorship",
+      description: "Scale your team and hire the top 3% of developers and designers at Toptal. Email Adam at `adam@changelog.com` for a personal introduction to Toptal."
+    }
   ]
 }
 
 Repo.insert! %Episode{
-  slug: "226",
-  title: "The Road to Font Awesome 5 with Dave Gandy",
-  summary: "Dave Gandy joined the show to talk about the history of Font Awesome, what's to come in Font Awesome 5 and their Kickstarter to fund Font Awesome 5 Pro, and how everything they're doing is funneling back into the forever free and open source — Font Awesome Free.",
-  notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie ut lorem vitae feugiat. Pellentesque vel erat libero. Vivamus rhoncus non erat nec bibendum. Vivamus ullamcorper massa mauris, at dapibus metus tincidunt sit amet. Suspendisse eu nibh vitae libero consectetur vehicula non a dolor. Aenean quis ligula ut risus iaculis blandit.",
+  slug: "231",
+  title: "HTTP/2 in Node.js Core",
+  summary: "In this special episode recorded at Node Interactive 2016 in Austin, TX Adam talked with James Snell (IBM Technical Lead for Node and member of Node's TSC and CTC) about the work he's doing on Node's implementation of http2, the state of http2 in Node, what this new spec has to offer, and what the Node community can expect from this new protocol.",
+  notes: "This episode is a preview of our upcoming \"Future of Node.js\" series recorded at Node Interactive 2016 in Austin, TX. The series is produced in partnership with Node.js Foundation and sponsored by IBM. We'll be releasing the full series soon on our new podcast \"Spotlight\". If you haven't subscribed to Changelog Master yet, which includes all the podcasts we produce, now would be a good time to do so. You can also subscribe to Changelog Weekly where we announce all the new things we're up to.",
   published: true,
   published_at: Timex.now,
   recorded_at: Timex.now,
@@ -309,13 +398,36 @@ Repo.insert! %Episode{
   episode_guests: [
     %EpisodeGuest{
       position: 1,
-      person: davegandy
+      person: jasnell
     }
   ],
   episode_hosts: [
     %EpisodeHost{
       position: 1,
       person: adamstac
+    }
+  ],
+  episode_sponsors: [
+    %EpisodeSponsor{
+      position: 1,
+      sponsor: rollbar,
+      title: "Rollbar",
+      link_url: "https://rollbar.com/changelog",
+      description: "Put errors in their place! `npm install --save rollbar` for error tracking in your Node.js apps."
+    },
+    %EpisodeSponsor{
+      position: 2,
+      sponsor: hackerparadise,
+      title: "Hacker Paradise",
+      link_url: "http://www.hackerparadise.org/changelog",
+      description: "Do you want to spend a month in South America, expenses paid, working on open source? We teamed up with Hacker Paradise to offer two Open Source Fellowships for a month on one of their upcoming trips to either Argentina or Peru. "
+    },
+    %EpisodeSponsor{
+      position: 3,
+      sponsor: gocd,
+      title: "GoCD",
+      link_url: "https://www.go.cd/changelog",
+      description: "GoCD is an on-premise open source continuous delivery server created by ThoughtWorks that lets you automate and streamline your build-test-release cycle for reliable, continuous delivery of your product. "
     }
   ]
 }
@@ -377,6 +489,22 @@ Repo.insert! %Episode{
       position: 3,
       person: bketelsen
     },
+  ],
+  episode_sponsors: [
+    %EpisodeSponsor{
+      position: 1,
+      sponsor: linode,
+      title: "Linode",
+      link_url: "https://linode.com/rfc",
+      description: "*Our cloud server of choice!* Get one of the fastest, most efficient SSD cloud servers for only $10/mo. We host everything we do on Linode servers. Use the code `rfc20` to get 2 months free!"
+    },
+    %EpisodeSponsor{
+      position: 2,
+      sponsor: codeschool,
+      title: "Code School",
+      link_url: "https://www.codeschool.com/go",
+      description: "On Track With Go — Learn what makes Go a great fit for concurrent programs and how you can use it to leverage the power of modern computer architectures in this new course for those getting started with Go."
+    }
   ]
 }
 
@@ -406,6 +534,22 @@ Repo.insert! %Episode{
     %EpisodeHost{
       position: 2,
       person: mikeal
+    }
+  ],
+  episode_sponsors: [
+    %EpisodeSponsor{
+      position: 1,
+      sponsor: toptal,
+      title: "TopTal",
+      link_url: "http://www.toptal.com/?utm_source=changelog&utm_medium=podcast&utm_campaign=changelog-sponsorship",
+      description: "Scale your team and hire the top 3% of developers and designers at Toptal. Email Adam at `adam@changelog.com` for a personal introduction to Toptal."
+    },
+    %EpisodeSponsor{
+      position: 2,
+      sponsor: linode,
+      title: "Linode",
+      link_url: "https://linode.com/rfc",
+      description: "*Our cloud server of choice!* Get one of the fastest, most efficient SSD cloud servers for only $10/mo. We host everything we do on Linode servers. Use the code `rfc20` to get 2 months free!"
     }
   ]
 }
