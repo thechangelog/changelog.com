17d655dd57065774f966538f480e6509e4e7dbe3
Dennis Reimann <mail@dennisreimann.de>
2016-12-23 22:27:24 +0100

    add pagination for search

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -1,45 +1,60 @@
 defmodule Changelog.SearchController do
   use Changelog.Web, :controller
 
-  alias Changelog.{Episode, Post}
+  alias Changelog.{Episode, Post, SearchResults}
 
   require Logger
 
   def search(conn, params) do
-    case params do
-      %{"q" => query} ->
-        episodes = find_episodes(query)
-        posts = find_posts(query)
-
-        render(conn, "search.html", query: query, results: %{
-          count: length(episodes) + length(posts),
-          episodes: episodes,
-          posts: posts
-        })
-
-      _else ->
-        render(conn, "search.html", query: nil, results: nil)
+    results = case params do
+      %{"q" => query, "type" => type, "page" => page} -> fetch_results(query, type, page)
+      %{"q" => query} -> fetch_results(query)
+      _else -> nil
     end
+
+    render(conn, "search.html", results: results, query: params["q"], type: params["type"])
   end
 
-  defp find_episodes(query, limit \\ 10) do
+  defp fetch_results(query) do
+    episodes = find_episodes(query)
+    posts = find_posts(query)
+
+    results(episodes, posts)
+  end
+
+  defp fetch_results(query, type, page) do
+    episodes = if type == "episodes", do: find_episodes(query, page), else: %{total_entries: 0}
+    posts = if type == "posts", do: find_posts(query, page), else: %{total_entries: 0}
+
+    results(episodes, posts)
+  end
+
+  defp results(episodes, posts) do
+    %SearchResults{
+      count_total: episodes.total_entries + posts.total_entries,
+      count_episodes: episodes.total_entries,
+      count_posts: posts.total_entries,
+      episodes: episodes,
+      posts: posts
+    }
+  end
+
+  defp find_episodes(query, page \\ 1, page_size \\ 10) do
     Episode
     |> Episode.published
     |> Episode.search(query)
     |> Episode.newest_first
-    |> Episode.limit(limit)
-    |> Repo.all
-    |> Episode.preload_podcast
-    |> Episode.preload_guests
+    |> preload(:podcast)
+    |> preload(:guests)
+    |> Repo.paginate(page: page, page_size: page_size)
   end
 
-  defp find_posts(query, limit \\ 10) do
+  defp find_posts(query, page \\ 1, page_size \\ 10) do
     Post
     |> Post.published
     |> Post.search(query)
     |> Post.newest_first
-    |> Post.limit(limit)
-    |> Repo.all
-    |> Post.preload_author
+    |> preload(:author)
+    |> Repo.paginate(page: page, page_size: page_size)
   end
 end
--- web/views/search_view.ex
+++ web/views/search_view.ex
@@ -2,4 +2,6 @@ defmodule Changelog.SearchView do
   use Changelog.Web, :view
 
   alias Changelog.{PodcastView, PostView}
+
+  import Scrivener.HTML
 end
