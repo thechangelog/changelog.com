c5686c8dbb99f7f88ef4239600b691fbd82e9cc7
Jerod Santo <jerod@changelog.com>
2016-12-27 15:27:08 -0600

    add ability to edit your profile

--- test/controllers/home_controller_test.exs
+++ test/controllers/home_controller_test.exs
@@ -2,14 +2,29 @@ defmodule Changelog.HomeControllerTest do
   use Changelog.ConnCase
 
   @tag :as_user
-  test "the home page renders", %{conn: conn, user: user} do
+  test "renders the home page", %{conn: conn, user: user} do
     conn = get(conn, home_path(conn, :show))
     assert html_response(conn, 200) =~ user.handle
+  end
+
+  @tag :as_user
+  test "renders the edit form", %{conn: conn} do
+    conn = get(conn, home_path(conn, :edit))
+    assert html_response(conn, 200) =~ "form"
+  end
+
+  @tag :as_inserted_user
+  test "updates person and redirects to home page", %{conn: conn} do
+    conn = put(conn, home_path(conn, :update), person: %{name: "New Name"})
+
+    assert redirected_to(conn) == home_path(conn, :show)
   end
 
   test "requires user on all actions", %{conn: conn} do
     Enum.each([
       get(conn, home_path(conn, :show)),
+      get(conn, home_path(conn, :edit)),
+      put(conn, home_path(conn, :update), person: %{}),
     ], fn conn ->
       assert html_response(conn, 302)
       assert conn.halted
