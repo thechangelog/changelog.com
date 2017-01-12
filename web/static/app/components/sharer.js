9143acea23e5519699e9539c766f131b573d5d3c
Jerod Santo <jerod@changelog.com>
2017-01-12 09:37:43 -0600

    add copy to clipboard functionality to share url, embed code

--- web/static/app/components/sharer.js
+++ web/static/app/components/sharer.js
@@ -1,39 +1,63 @@
 import { u, ajax } from "umbrellajs";
 import template from "templates/share.hbs";
+import Clipboard from "clipboard";
 
 export default class Sharer {
   constructor(selector) {
-    this.attachUI(selector);
-    this.attachEvents();
-  }
-
-  attachUI(selector) {
     this.container = u(selector);
     this.closeButton = this.container.find(".js-share-close");
     this.content = this.container.find(".js-share-content");
+    this.isAttached = false;
   }
 
-  attachEvents() {
+  attach() {
+    // ui
+    this.embed = this.content.find(".js-share-embed");
+    this.toggleEmbedButton = this.content.find(".js-share-embed-toggle");
+
+    this.copyUrlButton = new Clipboard(".js-share-copy-url", {
+      target: function(trigger) { return trigger.previousElementSibling; }
+    });
+
+    this.copyEmbedButton = new Clipboard(".js-share-copy-embed", {
+      target: function(trigger) { return trigger.previousElementSibling; }
+    });
+
+    // events
     this.closeButton.handle("click", () => { this.hide(); });
-    this.container.on("change", ".js-share-embed-toggle", () => { this.toggleEmbed(); })
+    this.toggleEmbedButton.on("change", () => { this.toggleEmbed(); });
+
+    this.isAttached = true;
+  }
+
+  detach() {
+    if (!this.isAttached) {
+      return false;
+    }
+
+    // events
+    this.closeButton.off("click");
+    this.toggleEmbedButton.off("change");
+    this.copyUrlButton.destroy();
+    this.copyEmbedButton.destroy();
+    // ui
+    this.content.html("");
   }
 
   load(detailsUrl) {
-    this.resetUI();
+    this.detach();
     this.show();
+
     ajax(detailsUrl, {}, (error, data) => {
       this.content.html(template(data));
-      this.setEmbed(data.embed);
+      this.attach();
+      this.embed.text(data.embed);
     });
   }
 
-  setEmbed(text) {
-    this.content.find(".js-share-embed").text(text);
-  }
-
   toggleEmbed() {
-    const embed = this.content.find(".js-share-embed");
-    const text = embed.text().replace(/theme="(\w+)"/, function(match, current) {
+    const before = this.embed.text();
+    const after = before.replace(/theme="(\w+)"/, function(match, current) {
       if (current == "night") {
         return "theme=\"day\"";
       } else {
@@ -41,11 +65,7 @@ export default class Sharer {
       }
     });
 
-    embed.text(text);
-  }
-
-  resetUI() {
-   this.content.html("");
+    this.embed.text(after);
   }
 
   show() {
