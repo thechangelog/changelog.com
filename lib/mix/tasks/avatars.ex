18b7a950bfbb6e08246face7903cfeb5a7c69a4d
Jerod Santo <jerod@changelog.com>
2017-03-07 11:58:30 -0600

    add a mix task for easily downloading all avatars

---
+++ lib/mix/tasks/avatars.ex
@@ -0,0 +1,35 @@
+defmodule Mix.Tasks.Changelog.Avatars do
+  use Mix.Task
+
+  alias Changelog.{Person, PersonView, Repo}
+
+  @shortdoc "Downloads all people's avatars to a temp directory"
+
+  def run(_) do
+    Mix.Task.run "app.start"
+
+    File.mkdir("avatars")
+
+    for person <- Repo.all(Person) do
+      try do
+        url = PersonView.avatar_url(person)
+
+        url = if String.starts_with?(url, "http") do
+          url
+        else
+          "https://cdn.changelog.com#{url}"
+        end
+
+        %HTTPoison.Response{body: body} = HTTPoison.get!(url)
+        hash = :crypto.hash(:md5, body) |> Base.encode16(case: :lower)
+
+        # this is the default gravatar's md5
+        if hash != "2899242106ef16dbcfdec0ac41865ff8" do
+          File.write("avatars/#{person.handle}.png", body)
+        end
+      rescue
+        e -> IO.puts "Failed to download #{person.name}. #{e}"
+      end
+    end
+  end
+end
