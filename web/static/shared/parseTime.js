b097d2dbd67872cd8d5d3159587f1b9401af5715
Jerod Santo <jerod@changelog.com>
2017-07-03 08:30:09 -0500

    yoink parseTime function from TimeJump, add unit tests

---
+++ web/static/shared/parseTime.js
@@ -0,0 +1,18 @@
+// courtesy https://github.com/davatron5000/TimeJump
+export default function parseTime(string) {
+  let seconds = /^\d+(\.\d+)?$/g;
+  let colons  = /^(?:colons:)?(?:(?:(\d+):)?(\d\d?):)?(\d\d?)(\.\d+)?$/;
+  let youtube = /^(?:(\d\d?)[hH])?(?:(\d\d?)[mM])?(\d\d?)[sS]$/;
+
+  if (seconds.test(string)) {
+    return parseFloat(string);
+  }
+
+  let match = colons.exec(string) || youtube.exec(string);
+
+  if (match) {
+    return (3600 * (parseInt(match[1], 10) || 0) + 60 * (parseInt(match[2], 10) || 0) + parseInt(match[3], 10) + (parseFloat(match[4]) || 0));
+  }
+
+  return 0;
+}
