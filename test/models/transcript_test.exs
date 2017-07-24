90b65c47b74ca66d587f2c303893a50c1cc2b94c
Jerod Santo <jerod@changelog.com>
2017-07-24 16:39:32 -0500

    WIP parsing

--- test/models/transcript_test.exs
+++ test/models/transcript_test.exs
@@ -14,4 +14,24 @@ defmodule Changelog.TranscriptTest do
       refute changeset.valid?
     end
   end
+
+  # describe "parse_raw" do
+  #   test "The Changelog 200" do
+  #     podcast = insert(:podcast, name: "The Changelog")
+  #     episode = insert(:episode, slug: "200", title: "JavaScript and Robots", podcast: podcast)
+  #     adam = insert(:person, name: "Adam Stacoviak")
+  #     jerod = insert(:person, name: "Jerod Santo")
+  #     raquel = insert(:person, name: "Raquel Vélez")
+
+  #     insert(:episode_host, episode: episode, person: adam)
+  #     insert(:episode_host, episode: episode, person: jerod)
+  #     insert(:episode_guest, episode: episode, person: raquel)
+
+  #     raw = File.read!("#{fixtures_path()}/transcripts/the-changelog-200.md")
+  #     transcript = %Transcript{episode_id: episode.id, raw: raw}
+  #     transcript = Transcript.parse_raw(transcript)
+
+  #     assert length(transcript.fragments) == 400
+  #   end
+  # end
 end
--- web/models/transcript.ex
+++ web/models/transcript.ex
@@ -24,4 +24,31 @@ defmodule Changelog.Transcript do
     |> cast(params, ~w(episode_id raw))
     |> validate_required([:episode_id, :raw])
   end
+
+  def parse_raw(transcript) do
+    transcript = Repo.preload(transcript, :episode)
+    participants = Changelog.Episode.participants(transcript.episode)
+    speaker_pattern = ~r{\*\*(.*?):\*\*}
+
+    speaker_pattern
+    |> Regex.split(transcript.raw, include_captures: true, trim: true)
+    |> Enum.chunk(2)
+    |> Enum.take(1) # temp
+    |> Enum.each(fn(tuple) ->
+      [speaker_section, content_section] = tuple
+
+      speaker = case Regex.run(speaker_pattern, speaker_section) do
+        [_, name] -> Enum.find(participants, fn(x) -> x.name == name end)
+        nil -> nil
+      end
+
+      content_section
+      |> String.split("\r\n\r\n", trim: true)
+      |> Enum.each(fn(line) ->
+        IO.puts "#{speaker.name} said: #{String.trim(line)}"
+      end)
+    end)
+
+    transcript
+  end
 end
