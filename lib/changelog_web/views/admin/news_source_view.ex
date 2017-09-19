554dc764fd80396d86e3bd221723641c5af71ffa
Jerod Santo <jerod@changelog.com>
2017-09-19 14:39:13 -0500

    Add icon field to news source form

--- lib/changelog_web/views/admin/news_source_view.ex
+++ lib/changelog_web/views/admin/news_source_view.ex
@@ -1,3 +1,7 @@
 defmodule ChangelogWeb.Admin.NewsSourceView do
   use ChangelogWeb, :admin_view
+
+  alias ChangelogWeb.NewsSourceView
+
+  def icon_url(news_source), do: NewsSourceView.icon_url(news_source)
 end
