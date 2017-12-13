c7541c3f3c0eb410a568a861db34123f7d156a76
Jerod Santo <jerod@changelog.com>
2017-12-13 12:01:09 -0600

    Intersperse ads throughout items in news issues

--- lib/changelog_web/views/news/news_issue_view.ex
+++ lib/changelog_web/views/news/news_issue_view.ex
@@ -1,7 +1,29 @@
 defmodule ChangelogWeb.NewsIssueView do
   use ChangelogWeb, :public_view
 
+  alias Changelog.{NewsItem, NewsAd}
   alias ChangelogWeb.NewsItemView
+
+  def items_with_ads(items, []), do: items
+  def items_with_ads(items, ads) do
+    items
+    |> Enum.chunk_every(items_per_ad(items, ads))
+    |> Enum.with_index
+    |> Enum.map(fn{items, index} ->
+      case Enum.at(ads, index) do
+        nil -> items
+        ad -> items ++ [ad]
+      end
+    end)
+    |> List.flatten
+  end
+
+  defp items_per_ad(items, ads) do
+    trunc(Float.ceil(length(items) / length(ads))) - 1
+  end
+
+  def render_item_or_ad(item = %NewsItem{}), do: render("_item.html", item: item)
+  def render_item_or_ad(ad = %NewsAd{}), do: render("_ad.html", ad: ad)
 
   def spacer_url do
     "https://changelog-assets.s3.amazonaws.com/weekly/spacer.gif"
