37b5837e1fb2b4315b81c176fa48860d1a1ddff6
Cody Peterson <cody@codyjamespeterson.com>
2018-01-03 15:03:03 -0800

    Add simple body class

--- lib/changelog_web/views/layout_view.ex
+++ lib/changelog_web/views/layout_view.ex
@@ -24,7 +24,9 @@ defmodule ChangelogWeb.LayoutView do
   end
 
   def body_class(%{view_module: LiveView}), do: "page-live"
-  def body_class(_), do: ""
+  def body_class(conn) do
+    "#{Phoenix.Controller.controller_module(conn) |> Phoenix.Naming.resource_name("Controller")}-#{Phoenix.Controller.action_name(conn)}"
+  end
 
   def canonical_url(conn) do
     url(conn) <> conn.request_path
