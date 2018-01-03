b3629f62758208eb2fd36429d6480324af8bd064
Cody Peterson <cody@codyjamespeterson.com>
2018-01-03 13:17:41 -0800

    Add user links tooltip

--- assets/app/modules/tooltip.js
+++ assets/app/modules/tooltip.js
@@ -4,7 +4,7 @@ import { u } from "umbrellajs";
 export default class Tooltip {
   constructor(selector) {
     this.tooltipParents = u(selector);
-    this.tooltipSelector = '.tooltip';
+    this.tooltipSelector = ".tooltip";
 
     if (this.tooltipParents.length) {
       this.attach(this.tooltipParents);
@@ -13,15 +13,20 @@ export default class Tooltip {
 
   attach(parents) {
     parents.each(function attachTooltip(el) {
-      const tooltipElement = u(el).siblings('.tooltip').first();
+      const tooltipElement = u(el).siblings(".tooltip").first();
+      let placement = u(tooltipElement).data("placement");
+
+      if (placement === "") {
+        placement = "bottom";
+      }
 
       const tooltip = new Popper(el, tooltipElement, {
-        placement: 'bottom',
+        placement: placement,
       });
 
-      u(el).on('click', () => {
-        u(el).siblings('.tooltip').toggleClass('is-visible');
-        tooltip.update();
+      u(el).on("click", () => {
+        u(el).siblings(".tooltip").toggleClass("is-visible");
+        tooltip.scheduleUpdate();
       });
     });
   }
