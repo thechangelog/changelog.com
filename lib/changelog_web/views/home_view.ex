9bbec77e3bb48bc7a2f35472e3cd4b220074add8
Cody Peterson <cody@codyjamespeterson.com>
2018-01-04 15:37:51 -0800

    Add account pages

--- lib/changelog_web/views/home_view.ex
+++ lib/changelog_web/views/home_view.ex
@@ -3,6 +3,25 @@ defmodule ChangelogWeb.HomeView do
 
   alias ChangelogWeb.{SharedView, PersonView}
 
+  # TODO: This is a duplicate from Layout View
+  def active_class(conn, controllers) when is_list(controllers) do
+    active_controller =
+      Phoenix.Controller.controller_module(conn)
+      |> Module.split
+      |> List.last
+      |> String.replace("Controller", "")
+
+    if Enum.member?(controllers, active_controller) do
+      "active"
+    else
+      "df"
+    end
+  end
+
+  def active_class(conn, controllers) do
+    active_class(conn, [controllers])
+  end
+
   def checked_class_if(boolean) do
     if boolean do
       "checklist-item--checked"
