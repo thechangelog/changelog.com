652c280deb32e8fd87229210ba7360bbd777cdd3
Jerod Santo <jerod@changelog.com>
2018-01-09 13:13:49 -0600

    Refactor how layout view generates body classes, fix for live page

--- lib/changelog_web/views/layout_view.ex
+++ lib/changelog_web/views/layout_view.ex
@@ -4,13 +4,10 @@ defmodule ChangelogWeb.LayoutView do
   import ChangelogWeb.Meta.{AdminTitle, Title, Image, Description, Feeds, Twitter}
 
   alias ChangelogWeb.{Endpoint, LiveView, PersonView}
+  alias Phoenix.{Controller, Naming}
 
   def active_class(conn, controllers) when is_list(controllers) do
-    active_controller =
-      Phoenix.Controller.controller_module(conn)
-      |> Module.split
-      |> List.last
-      |> String.replace("Controller", "")
+    active_controller = controller_name(conn)
 
     if Enum.member?(controllers, active_controller) do
       "active"
@@ -18,17 +15,19 @@ defmodule ChangelogWeb.LayoutView do
       ""
     end
   end
-
   def active_class(conn, controllers) do
     active_class(conn, [controllers])
   end
 
-  def body_class(%{view_module: LiveView}), do: "page-live"
-  def body_class(conn) do
-    "page-#{Phoenix.Controller.controller_module(conn) |> Phoenix.Naming.resource_name("Controller")}-#{Phoenix.Controller.action_name(conn)}"
+  def action_name(conn) do
+    Controller.action_name(conn)
   end
 
   def canonical_url(conn) do
     url(conn) <> conn.request_path
+  end
+
+  def controller_name(conn) do
+    Controller.controller_module(conn) |> Naming.resource_name("Controller")
   end
 end
