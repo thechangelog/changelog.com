56d0dc692fb82d1b5a53f2ebbda588fc1d260fc4
Jason Sawyer <jason@humanshapes.co>
2018-01-10 16:19:21 -0800

    Add pagination to lists of podcasts

--- lib/changelog_web/controllers/podcast_controller.ex
+++ lib/changelog_web/controllers/podcast_controller.ex
@@ -7,19 +7,18 @@ defmodule ChangelogWeb.PodcastController do
     render(conn, :index)
   end
 
-  def show(conn, %{"slug" => slug}) do
+  def show(conn, params = %{"slug" => slug}) do
     podcast = Podcast.get_by_slug(slug)
 
-    episodes =
+    page =
       Podcast.get_episodes(podcast)
       |> Episode.published
       |> Episode.newest_first
-      |> Episode.limit(5)
-      |> Repo.all
       |> Episode.preload_podcast
       |> Episode.preload_guests
+      |> Repo.paginate(Map.put(params, :page_size, 15))
 
-    render(conn, :show, podcast: podcast, episodes: episodes)
+    render(conn, :show, podcast: podcast, episodes: page.entries, page: page)
   end
 
   def archive(conn, %{"slug" => slug}) do
