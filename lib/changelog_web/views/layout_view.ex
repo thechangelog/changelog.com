df3662b1264ef12da3bcbd82e364ccd51e7b47af
Jerod Santo <jerod@changelog.com>
2018-01-15 10:01:38 -0600

    Make nav links have ‘is-active’ class when appropriate

--- lib/changelog_web/views/layout_view.ex
+++ lib/changelog_web/views/layout_view.ex
@@ -6,28 +6,18 @@ defmodule ChangelogWeb.LayoutView do
   alias ChangelogWeb.{Endpoint, LiveView, PersonView, PodcastView, SharedView}
   alias Phoenix.{Controller, Naming}
 
-  def active_class(conn, controllers) when is_list(controllers) do
-    active_controller = controller_name(conn)
+  def active_class(conn, controllers, class_name \\ "is-active")
+  def active_class(conn, controllers, class_name) when is_binary(controllers), do: active_class(conn, [controllers], class_name)
+  def active_class(conn, controllers, class_name) when is_list(controllers) do
+    active_id = controller_action_combo(conn)
 
-    if Enum.member?(controllers, active_controller) do
-      "active"
-    else
-      ""
+    if Enum.any?(controllers, fn(x) -> String.match?(active_id, ~r/#{x}/) end) do
+      class_name
     end
   end
-  def active_class(conn, controllers) do
-    active_class(conn, [controllers])
-  end
 
-  def action_name(conn) do
-    Controller.action_name(conn)
-  end
-
-  def canonical_url(conn) do
-    url(conn) <> conn.request_path
-  end
-
-  def controller_name(conn) do
-    Controller.controller_module(conn) |> Naming.resource_name("Controller")
-  end
+  def action_name(conn), do: Controller.action_name(conn)
+  def canonical_url(conn), do: url(conn) <> conn.request_path
+  def controller_name(conn), do: Controller.controller_module(conn) |> Naming.resource_name("Controller")
+  def controller_action_combo(conn), do: [controller_name(conn), action_name(conn)] |> Enum.join("-")
 end
