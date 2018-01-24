fa5144979a4fd958208467c34c468180cc0fd6e1
Jerod Santo <jerod@changelog.com>
2018-01-24 14:47:24 -0600

    Fix feed rendering

--- lib/changelog_web/views/feed_view.ex
+++ lib/changelog_web/views/feed_view.ex
@@ -7,11 +7,11 @@ defmodule ChangelogWeb.FeedView do
 
   def render_item(_item = %{object: episode = %Episode{}}, assigns), do: render("_episode.xml", Map.put(assigns, :episode, Episode.preload_all(episode)))
   def render_item(_item = %{object: post = %Post{}}, assigns), do: render("_post.xml", Map.put(assigns, :post, post))
-  def render_item(item = %{object: nil}, assigns), do: render("_news.xml", Map.put(assigns, :item, item))
+  def render_item(item = %{object: nil}, assigns), do: render("_item.xml", Map.put(assigns, :item, item))
 
   def render_title(_item = %{object: episode = %Episode{}}, assigns), do: render("_episode_title.xml", Map.put(assigns, :episode, Episode.preload_all(episode)))
   def render_title(_item = %{object: post = %Post{}}, assigns), do: render("_post_title.xml", Map.put(assigns, :post, post))
-  def render_title(item = %{object: nil}, assigns), do: render("_news_title.xml", Map.put(assigns, :item, item))
+  def render_title(item = %{object: nil}, assigns), do: render("_item_title.xml", Map.put(assigns, :item, item))
 
   def podcast_title(podcast, episode) do
     title = EpisodeView.numbered_title(episode)
