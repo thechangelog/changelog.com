e50635a4c959f0efc944c365539f104fd6065119
Jerod Santo <jerod@changelog.com>
2018-01-30 13:30:36 -0600

    Add ‘beginning of week’ validation on sponsorships

--- test/changelog/data/news/news_sponsorship_test.exs
+++ test/changelog/data/news/news_sponsorship_test.exs
@@ -3,6 +3,21 @@ defmodule Changelog.NewsSponsorshipTest do
 
   alias Changelog.{NewsSponsorship}
 
+  @valid_attrs %{name: "A campaign", sponsor_id: 1, weeks: [~D[2018-01-29]]}
+
+  describe "admin_changeset" do
+    test "is valid with valid attributes" do
+      changeset = NewsSponsorship.admin_changeset(%NewsSponsorship{}, @valid_attrs)
+      assert changeset.valid?
+    end
+
+    test "is invalid when one of the weeks is not a Monday" do
+      attrs = Map.put(@valid_attrs, :weeks, [~D[2018-01-28]])
+      changeset = NewsSponsorship.admin_changeset(%NewsSponsorship{}, attrs)
+      refute changeset.valid?
+    end
+  end
+
   describe "ad_for_issue" do
     test "finds the ad marked 'newsletter'" do
       sponsorship = insert(:news_sponsorship)
