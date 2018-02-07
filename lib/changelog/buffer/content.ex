7d798f57dbabd45b9c452a3902a155223b18c65b
Jerod Santo <jerod@changelog.com>
2018-02-07 09:55:40 -0600

    Remove news item url from buffer text since it’s attached as media

--- lib/changelog/buffer/content.ex
+++ lib/changelog/buffer/content.ex
@@ -2,20 +2,6 @@ defmodule Changelog.Buffer.Content do
   alias Changelog.NewsItem
   alias ChangelogWeb.{Endpoint, Helpers, NewsItemView, Router}
 
-  def news_item_text(nil), do: ""
-  def news_item_text(item) do
-    item = NewsItem.preload_all(item)
-
-    [
-      item.headline,
-      news_item_link(item),
-      news_item_source_and_topics(item)
-    ]
-    |> Enum.reject(&(&1 == ""))
-    |> Enum.reject(&is_nil/1)
-    |> Enum.join("\n\n")
-  end
-
   def news_item_image(nil), do: nil
   def news_item_image(%{image: nil}), do: nil
   def news_item_image(item), do: NewsItemView.image_url(item, :original)
@@ -27,6 +13,19 @@ defmodule Changelog.Buffer.Content do
     else
       Router.Helpers.news_item_url(Endpoint, :show, NewsItemView.slug(item))
     end
+  end
+
+  def news_item_text(nil), do: ""
+  def news_item_text(item) do
+    item = NewsItem.preload_all(item)
+
+    [
+      item.headline,
+      news_item_source_and_topics(item)
+    ]
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.reject(&is_nil/1)
+    |> Enum.join("\n\n")
   end
 
   defp news_item_source_and_topics(item) do
--- test/changelog/buffer/content_test.exs
+++ test/changelog/buffer/content_test.exs
@@ -6,45 +6,6 @@ defmodule Changelog.Buffer.ContentTest do
   alias Changelog.Buffer.Content
   alias ChangelogWeb.{Endpoint, NewsItemView, Router}
 
-  describe "news_item_text" do
-    test "defaults to empty string" do
-      assert Content.news_item_text(nil) == ""
-    end
-
-    test "links to item url when story is less than 20 words" do
-      item = insert(:news_item, story: "This is too short")
-      assert Content.news_item_text(item) == "#{item.headline}\n\n#{item.url}"
-    end
-
-    test "links to news item url when story is 20 words or more" do
-      item = insert(:news_item, story: "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty")
-      news_item_url = Router.Helpers.news_item_url(Endpoint, :show, NewsItemView.slug(item))
-      assert Content.news_item_text(item) == "#{item.headline}\n\n#{news_item_url}"
-    end
-
-    test "includes topic tags when available" do
-      item = insert(:news_item)
-      t1 = insert(:news_item_topic, news_item: item)
-      t2 = insert(:news_item_topic, news_item: item)
-      t3 = insert(:news_item_topic, news_item: item)
-      assert Content.news_item_text(item) == "#{item.headline}\n\n#{item.url}\n\n##{t1.topic.slug} ##{t2.topic.slug} ##{t3.topic.slug}"
-    end
-
-    test "includes 'via' when news source has twitter handle" do
-      source = insert(:news_source, twitter_handle: "wired")
-      item = insert(:news_item, source: source)
-      t1 = insert(:news_item_topic, news_item: item)
-      assert Content.news_item_text(item) == "#{item.headline}\n\n#{item.url}\n\nvia @wired ##{t1.topic.slug}"
-    end
-
-    test "doesn't include 'via' when news source has no twitter handle" do
-      source = insert(:news_source)
-      item = insert(:news_item, source: source)
-      t1 = insert(:news_item_topic, news_item: item)
-      assert Content.news_item_text(item) == "#{item.headline}\n\n#{item.url}\n\n##{t1.topic.slug}"
-    end
-  end
-
   describe "news_item_image" do
     test "defaults to nil" do
       assert is_nil(Content.news_item_image(nil))
@@ -60,6 +21,46 @@ defmodule Changelog.Buffer.ContentTest do
         assert Content.news_item_image(item) == "url"
         assert called(NewsItemView.image_url(item, :original))
       end
+    end
+  end
+
+  describe "news_item_link" do
+    test "returns item url when story is less than 20 words" do
+      item = insert(:news_item, story: "This is too short")
+      assert Content.news_item_link(item) == item.url
+    end
+
+    test "returns news item url when story is 20 words or more" do
+      item = insert(:news_item, story: "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty")
+      assert Content.news_item_link(item) == Router.Helpers.news_item_url(Endpoint, :show, NewsItemView.slug(item))
+    end
+  end
+
+  describe "news_item_text" do
+    test "defaults to empty string" do
+      assert Content.news_item_text(nil) == ""
+    end
+
+    test "includes topic tags when available" do
+      item = insert(:news_item)
+      t1 = insert(:news_item_topic, news_item: item)
+      t2 = insert(:news_item_topic, news_item: item)
+      t3 = insert(:news_item_topic, news_item: item)
+      assert Content.news_item_text(item) == "#{item.headline}\n\n##{t1.topic.slug} ##{t2.topic.slug} ##{t3.topic.slug}"
+    end
+
+    test "includes 'via' when news source has twitter handle" do
+      source = insert(:news_source, twitter_handle: "wired")
+      item = insert(:news_item, source: source)
+      t1 = insert(:news_item_topic, news_item: item)
+      assert Content.news_item_text(item) == "#{item.headline}\n\nvia @wired ##{t1.topic.slug}"
+    end
+
+    test "doesn't include 'via' when news source has no twitter handle" do
+      source = insert(:news_source)
+      item = insert(:news_item, source: source)
+      t1 = insert(:news_item_topic, news_item: item)
+      assert Content.news_item_text(item) == "#{item.headline}\n\n##{t1.topic.slug}"
     end
   end
 end
