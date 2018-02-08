9e1cfd119b580f67343d1a0f7acdc9b2ca2b57af
Jerod Santo <jerod@changelog.com>
2018-02-08 13:15:31 -0600

    Add Google+ profile (lol why not)

--- lib/changelog/buffer/buffer.ex
+++ lib/changelog/buffer/buffer.ex
@@ -2,7 +2,7 @@ defmodule Changelog.Buffer do
   alias Changelog.NewsItem
   alias Changelog.Buffer.{Client, Content}
 
-  @profiles ["4f3ad7c8512f7ef962000004", "506b005149bbd8223400006b"]
+  @profiles ~w(4f3ad7c8512f7ef962000004 506b005149bbd8223400006b 5226807fe2965a1f3100001c)
 
   def queue(%NewsItem{type: :audio}), do: false
   def queue(item = %NewsItem{}) do
