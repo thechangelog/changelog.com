33ca6c10ef20bb668c38df9af582b6dcd0d933d8
Jerod Santo <jerod@changelog.com>
2018-03-05 09:18:18 -0600

    Correct display of images in ads

--- lib/changelog_web/views/news/news_ad_view.ex
+++ lib/changelog_web/views/news/news_ad_view.ex
@@ -4,15 +4,25 @@ defmodule ChangelogWeb.NewsAdView do
   alias Changelog.{Files, Hashid}
   alias ChangelogWeb.{Endpoint, SponsorView}
 
+  def hashid(ad), do: Hashid.encode(ad.id)
+
+  def image_link(ad, version \\ :large) do
+    if ad.image do
+      content_tag :div, class: "news_item-image" do
+        link to: ad.url do
+          tag(:img, src: image_url(ad, version), alt: ad.headline)
+        end
+      end
+    end
+  end
+
   def image_path(ad, version) do
     {ad.image, ad}
     |> Files.Image.url(version)
     |> String.replace_leading("/priv", "")
   end
 
-  def image_url(ad, version) do
-    static_url(Endpoint, image_path(ad, version))
-  end
+  def image_url(ad, version), do: static_url(Endpoint, image_path(ad, version))
 
   def slug(ad) do
     ad.headline
@@ -20,9 +30,5 @@ defmodule ChangelogWeb.NewsAdView do
     |> String.replace(~r/[^a-z0-9\s]/, "")
     |> String.replace(~r/\s+/, "-")
     |> Kernel.<>("-#{hashid(ad)}")
-  end
-
-  def hashid(ad) do
-    Hashid.encode(ad.id)
   end
 end
