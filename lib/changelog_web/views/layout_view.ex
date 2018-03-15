b0a5fbac1461db5ace55ea2ac1bdbc66806db785
Jerod Santo <jerod@changelog.com>
2018-03-15 09:06:06 -0500

    Footer improvements
    - Link to Ping in community section
    - Hard code podcasts in list (saves db call)

--- lib/changelog_web/views/layout_view.ex
+++ lib/changelog_web/views/layout_view.ex
@@ -3,14 +3,18 @@ defmodule ChangelogWeb.LayoutView do
 
   import ChangelogWeb.Meta.{AdminTitle, Title, Image, Description, Feeds, Twitter}
 
-  alias Changelog.{Podcast, Repo}
   alias ChangelogWeb.{Endpoint, PersonView}
 
   def canonical_url(conn), do: url(conn) <> conn.request_path
 
   def footer_podcasts do
-    Podcast.active
-    |> Podcast.oldest_first
-    |> Repo.all
+    [
+      %{name: "The Changelog", slug: "podcast"},
+      %{name: "Go Time", slug: "gotime"},
+      %{name: "JS Party", slug: "jsparty"},
+      %{name: "Practical AI", slug: "practicalai"},
+      %{name: "Founders Talk", slug: "founderstalk"},
+      %{name: "Request For Commits", slug: "rfc"},
+    ]
   end
 end
