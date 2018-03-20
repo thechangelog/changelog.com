3b6790ce6e3710ae5b40de759e4e8e4f2a9546aa
Jerod Santo <jerod@changelog.com>
2018-03-20 09:23:30 -0500

    Improve metadata for topic and news source pages

--- lib/changelog_web/meta/description.ex
+++ lib/changelog_web/meta/description.ex
@@ -2,22 +2,15 @@ defmodule ChangelogWeb.Meta.Description do
 
   import ChangelogWeb.Helpers.SharedHelpers, only: [md_to_text: 1, truncate: 2]
 
-  alias ChangelogWeb.{EpisodeView, NewsItemView, PageView, PodcastView, PostView}
+  alias ChangelogWeb.{EpisodeView, NewsItemView, NewsSourceView, PageView,
+                      PodcastView, PostView, TopicView}
 
   def description(assigns), do: assigns |> get
 
-  defp get(%{view_module: EpisodeView, episode: episode}) do
-    episode.summary |> md_to_text |> truncate(320)
-  end
-
-  defp get(%{view_module: NewsItemView, item: item}) do
-    item.story |> md_to_text |> truncate(320)
-  end
-
-  defp get(%{view_module: PodcastView, podcast: podcast}) do
-    podcast.description
-  end
-
+  defp get(%{view_module: EpisodeView, episode: episode}), do: episode.summary |> md_to_text |> truncate(320)
+  defp get(%{view_module: NewsItemView, item: item}), do: item.story |> md_to_text |> truncate(320)
+  defp get(%{view_module: NewsSourceView, source: source}), do: source.description
+  defp get(%{view_module: PodcastView, podcast: podcast}), do: podcast.description
   defp get(%{view_module: PostView, post: post}) do
     if post.tldr do
       post.tldr |> md_to_text |> truncate(320)
@@ -25,18 +18,15 @@ defmodule ChangelogWeb.Meta.Description do
       post.title
     end
   end
-
   defp get(%{view_module: PageView, view_template: "community.html"}) do
     "Join developers from all over the world with a backstage pass to everything we do."
   end
-
   defp get(%{view_module: PageView, view_template: "weekly.html"}) do
     "Our editorialized take on this week in open source and software development. It's the email you should read so you're not missing out."
   end
-
   defp get(%{view_module: PageView, view_template: "nightly.html"}) do
     "Get the hottest new and top GitHub repos in your inbox every night. No fluff, just repos."
   end
-
+  defp get(%{view_module: TopicView, topic: topic}), do: topic.description
   defp get(_), do: "News and podcasts for developers"
 end
--- lib/changelog_web/meta/image.ex
+++ lib/changelog_web/meta/image.ex
@@ -8,21 +8,30 @@ defmodule ChangelogWeb.Meta.Image do
   def fb_image_height(assigns), do: assigns |> get_fb_height
   def twitter_image(assigns), do: assigns |> get_twitter
 
-  defp get_fb(%{podcast: podcast}), do: static_url(Endpoint, "/images/podcasts/#{podcast.slug}-cover-art.png")
+  defp get_fb(%{podcast: podcast}), do: podcast_image(podcast)
   defp get_fb(_), do: nil
   defp get_fb_width(%{podcast: _podcast}), do: "3000"
   defp get_fb_width(_), do: nil
   defp get_fb_height(%{podcast: _podcast}), do: "3000"
   defp get_fb_height(_), do: nil
 
-  defp get_twitter(%{podcast: podcast}), do: static_url(Endpoint, "/images/podcasts/#{podcast.slug}-cover-art.png")
+  defp get_twitter(%{podcast: podcast}), do: podcast_image(podcast)
   defp get_twitter(%{view_module: NewsItemView, item: item}) do
     cond do
-      item.author -> PersonView.avatar_url(item.author, :large)
-      item.source && item.source.icon -> NewsSourceView.icon_url(item.source, :large)
-      topic = Enum.find(item.topics, &(&1.icon)) -> TopicView.icon_url(topic, :large)
-      true -> static_url(Endpoint, "/images/defaults/type-#{item.type}.png")
+      item.author -> person_image(item.author)
+      item.source && item.source.icon -> source_image(item.source)
+      topic = Enum.find(item.topics, &(&1.icon)) -> topic_image(topic)
+      true -> item_type_image(item)
     end
   end
-  defp get_twitter(_), do: static_url(Endpoint, "/images/share/sitewide-twitter-summary.png")
+  defp get_twitter(%{view_module: NewsSourceView, source: source}), do: source_image(source)
+  defp get_twitter(%{view_module: TopicView, topic: topic}), do: topic_image(topic)
+  defp get_twitter(_), do: static_image("/images/share/sitewide-twitter-summary.png")
+
+  defp item_type_image(item), do: static_image("/images/defaults/type-#{item.type}.png")
+  defp person_image(person), do: PersonView.avatar_url(person, :large)
+  defp podcast_image(podcast), do: static_image("/images/podcasts/#{podcast.slug}-cover-art.png")
+  defp static_image(path), do: static_url(Endpoint, path)
+  defp source_image(source), do: NewsSourceView.icon_url(source, :large)
+  defp topic_image(topic), do: TopicView.icon_url(topic, :large)
 end
