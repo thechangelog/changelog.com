6e43da032de0654044aebb376feb38a3b421bf95
Jerod Santo <jerod@changelog.com>
2018-03-20 13:44:17 -0500

    Fix live view when in ‘upcoming’ mode

--- lib/changelog_web/views/live_view.ex
+++ lib/changelog_web/views/live_view.ex
@@ -2,7 +2,7 @@ defmodule ChangelogWeb.LiveView do
   use ChangelogWeb, :public_view
 
   alias Changelog.Wavestreamer
-  alias ChangelogWeb.{EpisodeView, PersonView}
+  alias ChangelogWeb.{EpisodeView, PersonView, PodcastView}
 
   def host_or_guest(episode, person) do
     if Enum.member?(episode.hosts, person) do
