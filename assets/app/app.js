61bbc80d07156768b9fe7c18f62e9bd2b680bd88
Jerod Santo <jerod@changelog.com>
2018-03-29 10:46:14 -0500

    Turns out you can just wrap an entire form in `FormData`
    Thanks to @franciscop for pointing this out to me 👏

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,7 +1,6 @@
 import "phoenix_html";
 import Turbolinks from "turbolinks";
 import { u, ajax } from "umbrellajs";
-import "ext/u.toFormData";
 import autosize from "autosize";
 import Cookies from "cookies-js";
 import OnsitePlayer from "modules/onsitePlayer";
@@ -144,7 +143,7 @@ u(document).on("submit", "form", function(event) {
     return Turbolinks.visit(`${action}?${form.serialize()}`);
   }
 
-  const options = {method: method, body: form.toFormData(), headers: {"Turbolinks-Referrer": referrer}};
+  const options = {method: method, body: new FormData(form), headers: {"Turbolinks-Referrer": referrer}};
   const andThen = function(err, resp, req) {
     if (req.getResponseHeader("content-type").match(/javascript/)) {
       eval(resp);
