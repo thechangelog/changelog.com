030c3bc8ece79004adf172ebeac7034ad71f88e5
Jerod Santo <jerod@changelog.com>
2018-04-03 13:42:26 -0500

    Update audio buffering logic

--- lib/changelog/buffer/buffer.ex
+++ lib/changelog/buffer/buffer.ex
@@ -2,9 +2,11 @@ defmodule Changelog.Buffer do
   alias Changelog.NewsItem
   alias Changelog.Buffer.{Client, Content}
 
-  @changelog ~w(4f3ad7c8512f7ef962000004 506b005149bbd8223400006b 5226807fe2965a1f3100001c)
-  @jsparty   ~w(5734d7fc1b14578733224a70 506b005149bbd8223400006b 5226807fe2965a1f3100001c)
-  @gotime    ~w(5734d7fc1b14578733224a70 506b005149bbd8223400006b 5226807fe2965a1f3100001c)
+  @errbody     ~w(506b005149bbd8223400006b 5226807fe2965a1f3100001c)
+  @changelog   ~w(4f3ad7c8512f7ef962000004)
+  @jsparty     ~w(5734d7fc1b14578733224a70)
+  @gotime      ~w(5734d7fc1b14578733224a70)
+  @practicalai ~w(5ac3c64b3fda312b116ca788)
 
   def queue(item = %NewsItem{type: :audio, object_id: id}) when is_binary(id) do
     link = Content.episode_link(item)
@@ -13,12 +15,11 @@ defmodule Changelog.Buffer do
     profiles = cond do
       String.starts_with?(id, "gotime") -> @gotime
       String.starts_with?(id, "jsparty") -> @jsparty
-      String.starts_with?(id, "podcast") -> @changelog
-      String.starts_with?(id, "rfc") -> @changelog
-      true -> []
+      String.starts_with?(id, "practicalai") -> @practicalai
+      true -> @changelog
     end
 
-    Client.create(profiles, text, [link: link])
+    Client.create((profiles ++ @errbody), text, [link: link])
   end
   def queue(%NewsItem{type: :audio}), do: false
   def queue(item = %NewsItem{object_id: id}) when is_binary(id) do
