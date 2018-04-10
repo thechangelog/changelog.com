47d06b7f9ab8241311376a5bb63ef209ec346aaa
Jerod Santo <jerod@changelog.com>
2018-04-10 10:44:06 -0500

    Fix code reloading, derp

--- lib/changelog_web/endpoint.ex
+++ lib/changelog_web/endpoint.ex
@@ -21,11 +21,9 @@ defmodule ChangelogWeb.Endpoint do
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
-  # if code_reloading? do
-  #   socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-  #   plug Phoenix.LiveReloader
-  #   plug Phoenix.CodeReloader
-  # end
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
 
   plug Plug.RequestId
   plug Plug.Logger
