93903c9c97bc73039818d450cdb936f086c5dfd9
Cody Peterson <cody@codyjamespeterson.com>
2018-04-19 11:27:28 -0700

    Refine tooltip display, close tooltip if clicking anywhere outside of tooltip

--- assets/app/app.js
+++ assets/app/app.js
@@ -21,6 +21,13 @@ const overlay = new Overlay("#overlay");
 
 window.u = u;
 
+u(document).on("click", function(event) {
+  const target = u(event.target);
+  if ((!target.closest('.tooltip').length) && (!target.hasClass('has-tooltip'))) {
+    u(".tooltip").removeClass("is-visible");
+  }
+});
+
 u(document).handle("click", ".js-toggle-nav", function(event) {
   u("body").toggleClass("nav-open");
 
