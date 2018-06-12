22de2c1720e09f674a56c1bfc5a261206d198215
Jerod Santo <jerod@changelog.com>
2018-06-12 08:23:16 -0500

    Do not use UTC for date segments on sponsorship week picker

--- assets/admin/views/newsSponsorshipView.js
+++ assets/admin/views/newsSponsorshipView.js
@@ -68,9 +68,9 @@ export default class newsSponsorshipView {
     let $addButton = $(".js-add-weeks");
 
     let dateAsValue = function(date) {
-      let year = date.getUTCFullYear();
-      let month = date.getUTCMonth() + 1;
-      let day = date.getUTCDate();
+      let year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      let day = date.getDate();
       if (month < 10) month = `0${month}`;
       if (day < 10) day = `0${day}`;
       return `${year}-${month}-${day}`;
