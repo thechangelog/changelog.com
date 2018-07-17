efef0467420bc5cb3d0206b7893e1925aa339b4b
Cody Peterson <cody@codyjamespeterson.com>
2018-07-16 17:18:14 -0700

    Add permalink styles

--- assets/app/modules/comment.js
+++ assets/app/modules/comment.js
@@ -4,9 +4,18 @@ export default class Commment {
   constructor() {
     // ui
     this.toggleButton = u(".js-toggle_comment");
+    this.permalink = u(".js-permalink");
     // events
     this.toggleButton.on("click", (event) => {
       u(event.currentTarget).closest(".comment").toggleClass("is-collapsed");
+    });
+
+    this.permalink.on("click", (event) => {
+      event.preventDefault();
+      const target = u(event.target);
+      u(".comment").removeClass("is-permalink");
+      target.closest(".comment").toggleClass("is-permalink").removeClass("is-collapsed");
+      location.hash = target.attr("href");
     });
   }
 }
