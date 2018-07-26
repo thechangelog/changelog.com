01d6952a008e7a05cc7292f1f0e2c9ae7bb4a929
Jerod Santo <jerod@changelog.com>
2018-07-26 08:43:33 -0500

    Gracefully handle invalid input to search controller

--- lib/changelog_web/controllers/search_controller.ex
+++ lib/changelog_web/controllers/search_controller.ex
@@ -6,11 +6,19 @@ defmodule ChangelogWeb.SearchController do
   require Logger
 
   def search(conn, params = %{"q" => query}) do
-    page = Search.search(query, [hitsPerPage: 30, page: Map.get(params, "page", 0)])
+    page = Search.search(query, [hitsPerPage: 30, page: page_param(params)])
     render(conn, :search, items: page.items, page: page, query: params["q"])
   end
 
   def search(conn, _params) do
     render(conn, :search, items: [], page: %{total_entries: 0}, query: "")
+  end
+
+  defp page_param(params) do
+    try do
+      params |> Map.get("page", 0) |> String.to_integer
+    rescue
+      _e in ArgumentError -> 0
+    end
   end
 end
