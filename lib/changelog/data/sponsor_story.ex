99c055afdd75f2b96f8b411dfb3bf1e569905e0d
Adam Stacoviak <adam@stacoviak.com>
2018-07-25 23:31:07 -0500

    Update Rollbar mp3 URLs

--- lib/changelog/data/sponsor_story.ex
+++ lib/changelog/data/sponsor_story.ex
@@ -23,9 +23,9 @@ defmodule Changelog.SponsorStory do
         title: "Head of Growth at Rollbar"
       },
       examples: [
-        %{type: "Partner pre-roll", name: "Move fast and fix things", audio: "https://changelog-assets.s3.amazonaws.com/site-sponsors-rollbar-partner-preroll-move-fast-and-fix-things.mp3", duration: 5},
-        %{type: "Customer story", name: "CircleCI: Paul Biggar", audio: "https://changelog-assets.s3.amazonaws.com/site-sponsors-rollbar-adroll-circleci-1.mp3", duration: 63},
-        %{type: "Endorsement", name: "Move fast and fix things", audio: "https://changelog-assets.s3.amazonaws.com/site-sponsors-rollbar-adroll-move-fast-and-fix-things.mp3", duration: 33}
+        %{type: "Partner pre-roll", name: "Move fast and fix things", audio: "https://changelog-assets.s3.amazonaws.com/partner-stories/rollbar-partner-preroll-move-fast-and-fix-things.mp3", duration: 5},
+        %{type: "Customer story", name: "CircleCI: Paul Biggar", audio: "https://changelog-assets.s3.amazonaws.com/partner-stories/rollbar-circleci-1.mp3", duration: 63},
+        %{type: "Endorsement", name: "Move fast and fix things", audio: "https://changelog-assets.s3.amazonaws.com/partner-stories/rollbar-move-fast-and-fix-things.mp3", duration: 33}
       ],
       content_md: """
       ### Who is Rollbar?
