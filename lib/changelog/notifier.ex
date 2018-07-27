5aaab14184d797f662b15fad9ce971450384d1a6
Jerod Santo <jerod@changelog.com>
2018-07-27 14:25:45 -0500

    Add guest thanks email to the notifier

--- lib/changelog/notifier.ex
+++ lib/changelog/notifier.ex
@@ -1,6 +1,18 @@
 defmodule Changelog.Notifier do
-  alias Changelog.{Mailer, NewsItem}
+  alias Changelog.{Mailer, Episode, NewsItem}
   alias ChangelogWeb.Email
+
+  def notify(item = %NewsItem{type: :audio}) do
+    episode =
+      item
+      |> NewsItem.load_object()
+      |> Map.get(:object)
+      |> Episode.preload_all()
+
+    for eg <- Enum.filter(episode.episode_guests, & &1.thanks) do
+      Email.guest_thanks(eg.person, episode) |> Mailer.deliver_later()
+    end
+  end
 
   def notify(item = %NewsItem{}) do
     item = NewsItem.preload_all(item)
--- test/changelog/notifier_test.exs
+++ test/changelog/notifier_test.exs
@@ -5,7 +5,41 @@ defmodule Changelog.NotifierTest do
   alias Changelog.Notifier
   alias ChangelogWeb.Email
 
-  describe "notify" do
+  describe "notify with episode item" do
+    test "when episode has no guests" do
+      episode = insert(:published_episode)
+      item = episode |> episode_news_item |> insert
+      Notifier.notify(item)
+      assert_no_emails_delivered()
+    end
+
+    test "when episode has guests but none of them have 'thanks' set" do
+      g1 = insert(:person)
+      g2 = insert(:person)
+      episode = insert(:published_episode)
+      insert(:episode_guest, episode: episode, person: g1, thanks: false)
+      insert(:episode_guest, episode: episode, person: g2, thanks: false)
+      item = episode |> episode_news_item |> insert
+      Notifier.notify(item)
+      assert_no_emails_delivered()
+    end
+
+    test "when episode has guests and some of them have 'thanks' set" do
+      g1 = insert(:person)
+      g2 = insert(:person)
+      g3 = insert(:person)
+      episode = insert(:published_episode)
+      insert(:episode_guest, episode: episode, person: g1, thanks: false)
+      insert(:episode_guest, episode: episode, person: g2, thanks: true)
+      insert(:episode_guest, episode: episode, person: g3, thanks: true)
+      item = episode |> episode_news_item |> insert
+      Notifier.notify(item)
+      assert_delivered_email Email.guest_thanks(g2, episode)
+      assert_delivered_email Email.guest_thanks(g3, episode)
+    end
+  end
+
+  describe "notify with regular item" do
     test "when item has no submitter or author" do
       item = insert(:news_item)
       Notifier.notify(item)
