b5c1b24ceee8874b7daa93df8fa4009071863425
Jerod Santo <jerod@changelog.com>
2018-08-10 08:05:36 -0500

    Switch to official release of PlugEtsCache, update deps

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Changelog.Mixfile do
       {:bamboo_smtp, "~> 1.3"},
       {:httpoison, "~> 1.0", override: true},
       {:con_cache, "~> 0.13.0"},
-      {:plug_ets_cache, github: "jerodsanto/plug_ets_cache", branch: "per-response-ttl"},
+      {:plug_ets_cache, "~> 0.3.0"},
       {:exjsx, "~> 3.2.1 or ~> 4.0"},
       {:ex_aws, "~> 1.1"}, # can't upgrade to 2.0 until arc supports it
       {:nimble_csv, "~> 0.4"},
