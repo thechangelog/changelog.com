c502bd6d6c5da531d04d1ad67cc77e8dfc099c7e
Jerod Santo <jerod@changelog.com>
2018-10-30 15:32:06 -0500

    Modify the format of episode titles in the master feed

--- lib/changelog_web/views/feed_view.ex
+++ lib/changelog_web/views/feed_view.ex
@@ -1,9 +1,20 @@
 defmodule ChangelogWeb.FeedView do
   use ChangelogWeb, :public_view
 
-  alias Changelog.{Episode, NewsItem, Podcast, Post}
+  alias Changelog.{Episode, NewsItem, Post}
   alias ChangelogWeb.{EpisodeView, NewsItemView, PersonView, PodcastView,
                       PostView, TimeView}
+
+  def episode_title(%{slug: "master"}, episode) do
+    prefix = episode.title
+    suffix =
+      [episode.podcast.name, EpisodeView.number_with_pound(episode)]
+      |> Enum.reject(&is_nil/1)
+      |> Enum.join(" ")
+
+    "#{prefix} (#{suffix})"
+  end
+  def episode_title(_podcast, episode), do: EpisodeView.numbered_title(episode)
 
   def image_link(item = %NewsItem{}) do
     if link = NewsItemView.image_link(item), do: safe_to_string(link)
@@ -16,16 +27,6 @@ defmodule ChangelogWeb.FeedView do
   def render_title(_item = %{object: episode = %Episode{}}, assigns), do: render("_episode_title.xml", Map.put(assigns, :episode, Episode.preload_all(episode)))
   def render_title(_item = %{object: post = %Post{}}, assigns), do: render("_post_title.xml", Map.put(assigns, :post, post))
   def render_title(item = %{object: nil}, assigns), do: render("_item_title.xml", Map.put(assigns, :item, item))
-
-  def podcast_title(podcast, episode) do
-    title = EpisodeView.numbered_title(episode)
-
-    if Podcast.is_master(podcast) do
-      "#{episode.podcast.name} - #{title}"
-    else
-      title
-    end
-  end
 
   def video_embed(item = %NewsItem{}) do
     if embed = NewsItemView.video_embed(item), do: safe_to_string(embed)
