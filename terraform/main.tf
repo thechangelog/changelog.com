f6c48d2a45cf5635ddcaa4bd8c7b5541b4e30eb4
Gerhard Lazu <gerhard@lazu.co.uk>
2018-12-09 17:41:36 +0000

    Finish creating a new Linode instance with Docker pre-installed
    [finishes #162526442]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,37 @@
-variable "linode_token" {
-  description = "Linode APIv4 Personal Access Token: https://cloud.linode.com/profile/tokens"
-}
-
 provider "linode" {
   token = "${var.linode_token}"
 }
 
-data "linode_region" "main" {
-  id = "us-east"
+variable "linode_token" {
+  description = "Linode APIv4 Personal Access Token: https://cloud.linode.com/profile/tokens"
 }
 
-data "linode_instance_type" "default" {
-  id = "g6-standard-2"
+variable "linode_region" {
+  description = "Region in which resources will be provisioned: us-west (Fremont), us-central (Dallas), us-southeast (Atlanta), us-east (Newark), eu-west (London), eu-central (Frankfurt), ap-south (Singapore), ap-northeast (Tokyo 1), ap-northeast-1a (Tokyo 2)"
+  default = "us-east"
 }
 
-data "linode_image" "ubuntu" {
+variable "linode_instance_type" {
+  description = "Instance type to provision for the app instance: https://www.linode.com/pricing#all"
+  default = "g6-standard-2"
+}
+
+data "linode_region" "changelog" {
+  id = "${var.linode_region}"
+}
+
+data "linode_instance_type" "changelog" {
+  id = "${var.linode_instance_type}"
+}
+
+data "linode_image" "changelog" {
   id = "linode/containerlinux"
+}
+
+resource "linode_instance" "www2" {
+  region = "${data.linode_region.changelog.id}"
+  type = "${data.linode_instance_type.changelog.id}"
+  label = "www2"
+  image = "${data.linode_image.changelog.id}"
+  authorized_users = ["gerhard-changelog"]
 }
