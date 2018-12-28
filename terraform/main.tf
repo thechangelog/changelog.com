3a45afdb9c108087e53496a93c33f3479d1096d5
Gerhard Lazu <gerhard@lazu.co.uk>
2018-12-28 16:07:46 +0000

    Automatically mount db & uploads volumes to 2019 Linode instance
    In hindsight, it would have made more sense to link the volumes to the
    instance via the `config` block of the `linode_instance` resource. Since
    the volumes were created after the instance was created, it felt more
    natural to reference the instance id in the volumes. When we transition
    to K8S, I expect all volume management to be handled by K8S, so this
    solution has a short shelf-life, not much value in making it better now.
    
    It's weird that we need to write the mount files to /tmp & then move
    them to /etc/systemd/system, but we are not the only ones to hit this,
    as captured in hashicorp/terraform#492. It's OK though, I expect K8S to
    make this redundant.
    
    It would be nice if the linode_image data source exposed a
    default_ssh_user, as mentioned in
    terraform-providers/terraform-provider-linode#21
    
    [#162511937]

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,11 @@ variable "linode_instance_type" {
   default = "g6-standard-2"
 }
 
+variable "default_ssh_user" {
+  description = "Default SSH user - for CoreOS this is core, for Ubuntu it is root, not sure about other Linode distros"
+  default = "core"
+}
+
 data "linode_region" "changelog" {
   id = "${var.linode_region}"
 }
@@ -36,6 +41,15 @@ resource "linode_instance" "2019" {
   authorized_users = ["gerhard-changelog"]
 }
 
+data "template_file" "db_mount" {
+  template = "${file("${path.module}/volume.mount.tpl")}"
+
+  vars {
+    DISK = "${linode_volume.db.filesystem_path}"
+    MOUNT_PATH = "/db"
+  }
+}
+
 resource "linode_volume" "db" {
   region = "${data.linode_region.changelog.id}"
   label = "db"
@@ -48,6 +62,60 @@ resource "linode_volume" "uploads" {
   label = "uploads"
   size = 100
   linode_id = "${linode_instance.2019.id}"
+}
+
+data "template_file" "uploads_mount" {
+  template = "${file("${path.module}/volume.mount.tpl")}"
+
+  vars {
+    DISK = "${linode_volume.uploads.filesystem_path}"
+    MOUNT_PATH = "/uploads"
+  }
+}
+
+resource "null_resource" "mount_volumes" {
+  connection {
+    user = "${var.default_ssh_user}"
+    host = "${linode_instance.2019.ip_address}"
+  }
+
+  depends_on = [
+    "linode_instance.2019",
+    "linode_volume.db",
+    "linode_volume.uploads",
+  ]
+
+  provisioner "file" {
+    content = "${data.template_file.db_mount.rendered}"
+    destination = "/tmp/db.mount"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/db.mount /etc/systemd/system/db.mount",
+      "sudo systemctl enable db.mount",
+      "sudo systemctl start db.mount",
+      "sudo systemctl status db.mount | grep active.*mounted",
+    ]
+  }
+
+  provisioner "file" {
+    content = "${data.template_file.uploads_mount.rendered}"
+    destination = "/tmp/uploads.mount"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/uploads.mount /etc/systemd/system/uploads.mount",
+      "sudo systemctl enable uploads.mount",
+      "sudo systemctl start uploads.mount",
+      "sudo systemctl status uploads.mount | grep active.*mounted",
+    ]
+  }
+
+  triggers {
+    manually = "2018.12.28-16:05"
+  }
 }
 
 provider "dnsimple" { }
