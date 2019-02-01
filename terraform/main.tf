2739c90aa7f69a1dd0d08f9204111311778dec6c
Gerhard Lazu <gerhard@lazu.co.uk>
2019-02-01 15:03:50 +0000

    Add URL redirects mentioned in the infrastructure diagram
    [finishes #163652938]

--- terraform/main.tf
+++ terraform/main.tf
@@ -301,6 +301,70 @@ resource "dnsimple_record" "www_changelog_com" {
   ttl = 60
 }
 
+resource "dnsimple_record" "code_changelog_com" {
+  domain = "changelog.com"
+  name = "code"
+  value = "https://github.com/thechangelog/changelog.com"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "secrets_changelog_com" {
+  domain = "changelog.com"
+  name = "secrets"
+  value = "https://lastpass.com/?&ac=1"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "dns_changelog_com" {
+  domain = "changelog.com"
+  name = "dns"
+  value = "https://dnsimple.com/a/10898/domains/changelog.com/dns"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "ci_changelog_com" {
+  domain = "changelog.com"
+  name = "ci"
+  value = "https://circleci.com/gh/thechangelog/changelog.com"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "errors_changelog_com" {
+  domain = "changelog.com"
+  name = "errors"
+  value = "https://rollbar.com/changelogmedia/changelog.com/"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "docker_changelog_com" {
+  domain = "changelog.com"
+  name = "docker"
+  value = "https://hub.docker.com/u/thechangelog"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "logs_changelog_com" {
+  domain = "changelog.com"
+  name = "logs"
+  value = "https://papertrailapp.com/systems/2019/"
+  type = "URL"
+  ttl = 60
+}
+
+resource "dnsimple_record" "monitoring_changelog_com" {
+  domain = "changelog.com"
+  name = "monitoring"
+  value = "https://my.pingdom.com/reports/uptime#daterange=7days&tab=uptime_tab&check=2310619"
+  type = "URL"
+  ttl = 60
+}
+
 resource "dnsimple_record" "apex-changelog_fm" {
   domain = "changelog.fm"
   name = ""
