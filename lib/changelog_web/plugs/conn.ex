663115a078cd3748fe688592acfd9c48887f56fc
Jerod Santo <jerod@changelog.com>
2019-03-05 11:11:09 -0600

    Improve referer path extraction from connections, consolidate call sites

--- lib/changelog_web/plugs/conn.ex
+++ lib/changelog_web/plugs/conn.ex
@@ -4,17 +4,25 @@ defmodule ChangelogWeb.Plug.Conn do
   """
 
   import Plug.Conn
+
   alias Plug.Crypto
+  alias ChangelogWeb.Router
 
   @encryption_salt "8675309"
   @signing_salt "9035768"
 
+  @doc """
+  Extracts the user agent from a connection's headers
+  """
   def get_agent(conn) do
     conn
     |> get_req_header("user-agent")
     |> List.first()
   end
 
+  @doc """
+  Extracts an encrypted cookie from the connection with the given key
+  """
   def get_encrypted_cookie(conn, key) do
     cookie = conn.cookies[key]
 
@@ -32,6 +40,9 @@ defmodule ChangelogWeb.Plug.Conn do
       generate(conn, @encryption_salt, key_opts()))
   end
 
+  @doc """
+  Adds an encrypted cookie to the connection with the given value
+  """
   def put_encrypted_cookie(conn, key, value, opts \\ []) do
     opts = Keyword.put_new(opts, :max_age, 31_536_000) # one year
 
@@ -52,5 +63,37 @@ defmodule ChangelogWeb.Plug.Conn do
 
   defp generate(conn, key, opts) do
     Crypto.KeyGenerator.generate(conn.secret_key_base, key, opts)
+  end
+
+  @doc """
+  Extracts and returns the request referer, falling back to the root path
+  """
+  def referer_or_root_path(conn) do
+    with {:ok, referer} <- extract_referer(conn),
+         {:ok, path} <- extract_local_path(conn, referer)
+    do
+      path
+    else
+      _fail -> Router.Helpers.root_path(conn, :index)
+    end
+  end
+
+  defp extract_referer(conn) do
+    if referer = conn |> get_req_header("referer") |> List.last() do
+      {:ok, referer}
+    else
+      {:error, "no referer header"}
+    end
+  end
+
+  defp extract_local_path(conn, referer) do
+    uri = URI.parse(referer)
+    path = Map.get(uri, :path)
+
+    cond do
+      uri.host != conn.host -> {:error, "external referer"}
+      String.starts_with?(path, "//") -> {:error, "invalid path"}
+      true -> {:ok, path}
+    end
   end
 end
