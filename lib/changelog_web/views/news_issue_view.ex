c5cef36f73010869c99622a06cabb7912765dc53
Jerod Santo <jerod@changelog.com>
2019-08-05 15:36:24 -0500

    Add some stubbed templates for news issue v2 layout

--- lib/changelog_web/views/news_issue_view.ex
+++ lib/changelog_web/views/news_issue_view.ex
@@ -5,9 +5,10 @@ defmodule ChangelogWeb.NewsIssueView do
   alias ChangelogWeb.{NewsItemView, SponsorView}
 
   def items_with_ads(items, []), do: items
-  def items_with_ads(items, ads) do
+  def items_with_ads(items, ads), do: items_with_ads(items, ads, 3)
+  def items_with_ads(items, ads, every) do
     items
-    |> Enum.chunk_every(3)
+    |> Enum.chunk_every(every)
     |> Enum.with_index()
     |> Enum.map(fn{items, index} ->
       case Enum.at(ads, index) do
