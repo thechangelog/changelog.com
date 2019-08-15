dbc27fd99b3ecc4fa841e2bbeccf1e017843ca44
Jerod Santo <jerod@changelog.com>
2019-08-15 15:09:44 -0500

    First pass at an EpisodeRequest schema

---
+++ lib/changelog/schema/episode/episode_request.ex
@@ -0,0 +1,30 @@
+defmodule Changelog.EpisodeRequest do
+  use Changelog.Schema
+
+  alias Changelog.{Episode, Podcast, Person}
+
+  defenum Status, declined: -1, submitted: 0, pending: 1, published: 2
+
+  schema "episode_requests" do
+    field :status, Status, default: :submitted
+
+    field :hosts, :string
+    field :guests, :string
+    field :topics, :string
+    field :pitch, :string
+    field :pronunciation, :string
+
+    belongs_to :podcast, Podcast
+    belongs_to :submitter, Person
+    belongs_to :episode, Episode
+
+    timestamps()
+  end
+
+  def submission_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, ~w(podcast_id submitter_id hosts guests topics pitch pronunciation)a)
+    |> validate_required([:podcast_id, :submitter_id, :pitch])
+    |> foreign_key_constraint(:podcast_id)
+  end
+end
