ddd0981c14f2d5cb81de9acd04f1280aed0e6bd4
Cody Peterson <cody@codyjamespeterson.com>
2019-11-19 13:55:32 -0800

    Add lightbox and begin adding content

--- assets/app/modules/ten.js
+++ assets/app/modules/ten.js
@@ -1,16 +1,20 @@
 import { u } from 'umbrellajs';
 import { CountUp } from 'countup.js';
-// import MicroModal from 'micromodal';
+import GLightbox from 'glightbox'
 
 export default class Ten {
   constructor() {
     // Elements
     this.numbers = u('.js-increment');
+    this.lightboxItems = u('.js-lightbox');
 
+    const lightbox = GLightbox();
+
+    // Add observer for number count up
     const observer = new IntersectionObserver(function(entries) {
       entries.forEach(entry => {
         let numberEl = u(entry.target);
-        Ten.animateNumbers(u(numberEl));
+        Ten.incrementStatNumbers(u(numberEl));
         if (!entry.isIntersecting) return;
         observer.unobserve(entry.target);
       });
@@ -20,9 +24,20 @@ export default class Ten {
     if (this.numbers.length) {
       this.numbers.each(el => { observer.observe(el) });
     }
+    if (this.lightboxItems.length) {
+      Ten.initLightbox();
+    }
   }
 
-  static animateNumbers(numberEl) {
+  static initLightbox() {
+    const lightbox = GLightbox({
+      selector: 'js-lightbox',
+      descPosition: 'bottom',
+      width: 800
+    });
+  }
+
+  static incrementStatNumbers(numberEl) {
     const startValue = numberEl.data('start-value');
     const endValue = numberEl.data('end-value');
     const decimalPlaces = numberEl.data('decimal-places') || 0;
