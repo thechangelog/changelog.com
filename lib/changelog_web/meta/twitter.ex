7c04911d258588cdc8c689ce93a5176f722ff48b
Jerod Santo <jerod@changelog.com>
2020-03-10 09:07:55 -0500

    Use post image in social meta when applicable

--- lib/changelog_web/meta/twitter.ex
+++ lib/changelog_web/meta/twitter.ex
@@ -1,10 +1,12 @@
 defmodule ChangelogWeb.Meta.Twitter do
 
-  alias ChangelogWeb.{Endpoint, EpisodeView, PageView, PodcastView, Router.Helpers}
+  alias ChangelogWeb.{Endpoint, EpisodeView, PageView, PodcastView, PostView,
+                      Router.Helpers}
 
   def twitter_card_type(%{view_module: EpisodeView, view_template: "show.html"}), do: "player"
-  def twitter_card_type(%{view_module: PodcastView}), do: "summary_large_image"
   def twitter_card_type(%{view_module: PageView, view_template: "ten.html"}), do: "summary_large_image"
+  def twitter_card_type(%{view_module: PodcastView}), do: "summary_large_image"
+  def twitter_card_type(%{view_module: PostView, post: %{image: img}}) when is_map(img), do: "summary_large_image"
   def twitter_card_type(_), do: "summary"
 
   def twitter_player_url(%{view_module: EpisodeView, view_template: "show.html", podcast: podcast, episode: episode}) do
