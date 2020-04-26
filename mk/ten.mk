cc222c51ae64d27a4486f778f7567f2db80518df
Carmina Lazu <carmina@lazu.co.uk>
2020-04-26 12:10:32 +0100

    Fix changelog ten Docker image assets
    Because the upstream asset URLs change, the easiest thing is to go with
    the single file & embedded assets approach. It's slower to load, but it
    won't break if the asset URLs change as everything is embedded  in the
    HTML.
    
    monolith seemed like a good idea, but in practice the HTML would look
    broken. While SingleFile breaks JS, everything looks the way it should.
    
    Signed-off-by: Carmina Lazu <carmina@lazu.co.uk>

--- mk/ten.mk
+++ mk/ten.mk
@@ -1,54 +1,24 @@
-ifeq ($(PLATFORM),Darwin)
-SED := /usr/local/bin/gsed
-$(SED):
-	brew install gnu-sed
-else
-SED := /bin/sed
-endif
-
-ifeq ($(PLATFORM),Darwin)
-MONOLITH ?= /usr/local/bin/monolith
-$(MONOLITH):
-	brew install monolith
-endif
-ifeq ($(PLATFORM),Linux)
-MONOLITH ?= /usr/bin/monolith
-$(MONOLITH):
-	$(error Please install monolith: https://github.com/Y2Z/monolith#installation)
-endif
-
-define ABSOLUTE_CHANGELOG_LINKS
-$(SED) --in-place --regexp-extended --expression \
-  's|href="/|href="https://changelog.com/|g ; s|action="/|action="https://changelog.com/|g ; s|data-play="/|data-play="https://changelog.com/|g'
-endef
-
-.PHONY: tmp/ten-curl.html
-tmp/ten-curl.html: $(CURL)
-	@mkdir -p tmp \
-	&& $(CURL) --progress-bar --output $(@) https://changelog.com/ten \
-	&& $(ABSOLUTE_CHANGELOG_LINKS) $(@)
-
-.PHONY: tmp/ten-monolith.html
-tmp/ten-monolith.html: $(MONOLITH)
-	@mkdir -p tmp \
-	&& $(MONOLITH) https://changelog.com/ten -I -o $(@) \
-	&& $(ABSOLUTE_CHANGELOG_LINKS) $(@)
+.PHONY: tmp/changelog-ten.html
+tmp/changelog-ten.html:
+	@printf "Using $(BOLD)$(GREEN)https://github.com/gildas-lormeau/SingleFile$(NORMAL) " \
+	; printf "save $(BOLD)https://changelog.com/ten$(NORMAL) to $(BOLD)$(@)$(NORMAL)\n" \
+	; read -rp "(press any key to confirm)" -n 1
 
 .PHONY: ten-image
-ten-image: build-ten-image publish-ten-image
+ten-image: ten-image-build ten-image-publish
 
-.PHONY: build-ten-image
-build-ten-image: BUILD_VERSION = 2019-11-01T10.10.10Z
-build-ten-image: $(DOCKER) tmp/ten-curl.html tmp/ten-monolith.html
+.PHONY: ten-image-build
+ten-image-build: BUILD_VERSION = 2019-11-01T10.10.10Z
+ten-image-build: $(DOCKER) tmp/changelog-ten.html
 	@$(DOCKER) build \
 	  --pull \
 	  --tag thechangelog/ten:$(BUILD_VERSION) \
 	  --file docker/Dockerfile.ten \
 	  $(CURDIR)
 
-.PHONY: publish-ten-image
-publish-ten-image: BUILD_VERSION = 2019-11-01T10.10.10Z
-publish-ten-image: $(DOCKER)
+.PHONY: ten-image-publish
+ten-image-publish: BUILD_VERSION = 2019-11-01T10.10.10Z
+ten-image-publish: $(DOCKER)
 	@$(DOCKER) push thechangelog/ten:$(BUILD_VERSION)
 
 TEN_DEPLOYMENT := ten-changelog
