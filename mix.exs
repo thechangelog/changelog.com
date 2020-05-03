9dabd0cfd35dd3ba71d66315d90104382c733b69
Owen Bickford <historicstork@gmail.com>
2020-05-03 21:12:49 +0000

    add phoenix_live_reload

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Changelog.Mixfile do
       {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
       {:timex, "~> 3.0"},
