fcf4791b1ba39577f51b682ed420a407b067e8ff
Owen Bickford <historicstork@gmail.com>
2020-05-03 21:13:12 +0000

    enable live reloading

--- lib/changelog_web/endpoint.ex
+++ lib/changelog_web/endpoint.ex
@@ -32,6 +32,8 @@ defmodule ChangelogWeb.Endpoint do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
 
