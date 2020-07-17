d6aa5ae106be72066577f4956349974bba8de1b4
Adam Stacoviak <adam@stacoviak.com>
2020-07-17 14:45:19 -0500

    Update partner stories

--- lib/changelog/schema/sponsor_story.ex
+++ lib/changelog/schema/sponsor_story.ex
@@ -58,7 +58,7 @@ defmodule Changelog.SponsorStory do
         }
       ],
       content_md: """
-      ##### Who is Rollbar?
+      ## Who is Rollbar?
 
       Rollbar is an error monitoring platform that helps developer teams move fast and fix things. Catch errors before your users do. Resolve errors in minutes, and deploy your code with confidence.
 
@@ -69,17 +69,17 @@ defmodule Changelog.SponsorStory do
         </footer>
       </blockquote>
 
-      #### Who are you and what do you do at Rollbar?
+      ## Who are you and what do you do at Rollbar?
 
       Hello, I'm Mike Smith. I lead growth and marketing here at Rollbar. Day to day I define and deploy growth and marketing strategies, meet with partners, review campaign results, and generally try to learn as much as I can about our customer.
 
-      #### What do you value most about your partnership with Changelog?
+      ## What do you value most about your partnership with Changelog?
 
       Changelog has been a huge factor in helping Rollbar build brand awareness in the developer community. A few years back we focused our efforts on sponsoring a few developer conferences and communities. We'd go and setup an awesome booth, talk to the community, do demos — at the time, not many people knew who we were or what our service was about.
 
       Several months later at another conference, the same thing — great experience, great meeting with the community, but not many people knew about us. To our suprise, the next conference was different. The community knew who we were and what value we offered developers. Several members of the community mentioned they heard about us because we sponsored some of their favorite podcasts. These folks weren't Rollbar users either. However, a few mentioned they have recommended Rollbar to their friends. That was great news and significant validation. Because we sponsored many Changelog podcasts; including The Changelog, JS Party, Founders Talk, and Go Time, Rollbar was able to gain clear and mesurable brand awareness in the developer community.
 
-      #### Can you share some advice for future Changelog partners?
+      ## Can you share some advice for future Changelog partners?
 
       Don't underestimate the power of brand awareness. We've gained so much brand awareness in the developer community (thanks to Changelog). That awareness has been the foundation we've built Rollbar's developer marketing strategy on.
 
@@ -88,7 +88,7 @@ defmodule Changelog.SponsorStory do
       Partner: Rollbar
       Website: [rollbar.com](https://rollbar.com/)
       Employees: 50
-      Annual Revenue: $1,000,000
+      ARR: $1,000,000
       """
     }
   end
@@ -203,6 +203,36 @@ defmodule Changelog.SponsorStory do
         }
       ],
       content_md: """
+      ## Who is Linode?
+
+      Linode makes cloud computing simple, affordable, and accessible. Whether you're working on a personal project or managing your enterprise's infrastructure, Linode has the pricing, support, and scale you need to take your ideas to the next level.
+
+      <blockquote>
+        <p>Linode raised the bar for us. With LKE, we can declare more of our dependencies via this single API that the entire industry is converging on. Because LKE does more for us, we need less Terraforming and less manual integrations. There is more automation running in LKE with better visibility and tighter integrations. It's a real game-changer.</p>
+        <footer>
+          <strong>Gerhard Lazu</strong> — Changelog, SRE - <a href="https://www.linode.com/spotlight/changelog/">source</a>
+        </footer>
+      </blockquote>
+
+      ## Who are you and what do you do at Rollbar?
+
+      Hello, I'm Tyler Van Fossen. I lead our growth and partner marketing here at Linode. I help to define and deploy growth and marketing strategies, meet with partners, review what's working and what's not, and do what I can to enable our partners to help us reach new customers.
+
+      ## What do you value most about your partnership with Changelog?
+
+      Changelog is one of our highest performing partnerships to reach a wide variety of customer types. They've played a significant role helping Linode to build brand awareness and loyalty with developers. What I value most is how focused they are on delivering value to the relationship. They're always willing to flex, move, and scale to meet and exceed our needs. To put it bluntly, we've been tracking our ROI from this partnership for many years and have gotten a 4-6x return on our investment from this relationship. And that's only based on numbers and metrics we can actually track. I'm sure we're really at a 10x or more return.
+
+      ## Can you share some advice for future Changelog partners?
+
+      If you are considering a partnership with Changelog, know that you are in very capable hands and that they are able to help you connect with the developer community in an authentic way. They are laser-focused on earning and keeping their listener's trust. And thankfully we get to benefit from that through this partnership.
+
+      `---`
+
+      Partner: Linode
+      Website: [linode.com](https://linode.com/)
+      Employees: 175
+      Customers: 800,000
+      ARR: $70M
       """
     }
   end
