23758c9475e6e458a85dfc29b9a6316b6f4f71b4
Carmina Lazu <carmina@lazu.co.uk>
2020-07-27 13:48:37 +0100

    Update nginx-ingress to latest
    Delete jobs so that subsequent upgrades will just work.
    
    Signed-off-by: Carmina Lazu <carmina@lazu.co.uk>

--- mk/nginx-ingress.mk
+++ mk/nginx-ingress.mk
@@ -1,12 +1,14 @@
 # https://github.com/kubernetes/ingress-nginx/releases
-NGINX_INGRESS_VERSION := 0.32.0
+NGINX_INGRESS_VERSION := v0.34.1
 NGINX_INGRESS_NAMESPACE := ingress-nginx
 .PHONY: lke-nginx-ingress
 lke-nginx-ingress: lke-ctx
 	$(KUBECTL) apply \
 	  --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-$(NGINX_INGRESS_VERSION)/deploy/static/provider/cloud/deploy.yaml \
+	&& $(KUBECTL) wait --namespace $(NGINX_INGRESS_NAMESPACE) --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s \
 	&& $(KUBECTL) scale --replicas=$(LKE_NODE_COUNT) deployment/ingress-nginx-controller --namespace $(NGINX_INGRESS_NAMESPACE) \
-	&& $(KUBETREE) deployments ingress-nginx-controller --namespace $(NGINX_INGRESS_NAMESPACE)
+	&& $(KUBETREE) deployments ingress-nginx-controller --namespace $(NGINX_INGRESS_NAMESPACE) \
+	&& $(KUBECTL) delete job ingress-nginx-admission-create ingress-nginx-admission-patch
 lke-provision:: lke-nginx-ingress
 
 .PHONY: lke-nginx-ingress-verify
