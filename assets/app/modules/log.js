ec4d7692bdad64cb87b755807547c81f06d9e440
Jerod Santo <jerod@changelog.com>
2020-08-24 16:48:58 -0500

    Purge GA scripts from site

--- assets/app/modules/log.js
+++ assets/app/modules/log.js
@@ -1,13 +1,11 @@
 export default class Log {
   static track(category, action, label) {
-    if (typeof ga == "undefined") {
-      console.log("[TRACK]", {category: category, action: action, label: label});
-    } else {
-      ga("send", "event", category, action, label);
-    }
-
     if (typeof plausible !== "undefined") {
+      // plausible does not yet support extra event data like ga did
+      // ga("send", "event", category, action, label);
       plausible(category);
+    } else {
+      console.log("[TRACK]", {category: category, action: action, label: label});
     }
   }
 }
