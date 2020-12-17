2835daffdd9d64c7c79c529b47390015894c47dd
Gerhard Lazu <gerhard@lazu.co.uk>
2020-12-17 00:28:48 +0000

    Deploy grafana-agent in the grafana-cloud namespace
    Even though everything is up and running, and there are no errors in the
    logs, the agent is not scraping any targets according to its
    agent_prometheus_ha_configs_created_total metric.
    
    Leaving this here as an intermediary step, but considering that the
    agent is still in beta, configuring Prometheus' remote_write seems like
    a more robust approach.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- mk/grafana-cloud.mk
+++ mk/grafana-cloud.mk
@@ -1,6 +1,17 @@
+GRAFANA_CLOUD_NAMESPACE := grafana-cloud
+
 .PHONY: lke-promtail
 lke-promtail: | lke-ctx $(CURL) $(LPASS)
 	curl -fsS https://raw.githubusercontent.com/grafana/loki/master/tools/promtail.sh \
 	| sh -s 8521 "$$($(LPASS) show --notes 1212835959730479797)" logs-prod-us-central1.grafana.net default | kubectl apply --namespace=default -f  -
 
 lke-provision:: lke-promtail
+
+GRAFANA_AGENT_API_KEY := "$$($(LPASS) show --notes Shared-changelog/secrets/GRAFANA_AGENT_API_KEY)"
+.PHONY: lke-grafana-cloud
+lke-grafana-cloud: | $(YTT) lke-ctx $(LPASS)
+	$(YTT) \
+	    --data-value namespace=$(GRAFANA_CLOUD_NAMESPACE) \
+	    --data-value password=$(GRAFANA_AGENT_API_KEY) \
+	    --file $(CURDIR)/k8s/grafana-cloud \
+	| $(KUBECTL) apply --filename -
