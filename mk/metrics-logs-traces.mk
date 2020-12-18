82da2c7e6ffd500cad2686c30b90f3483497e920
Gerhard Lazu <gerhard@lazu.co.uk>
2020-12-18 00:23:52 +0000

    Use new LastPass secret for Promtail
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- mk/metrics-logs-traces.mk
+++ mk/metrics-logs-traces.mk
@@ -78,7 +78,7 @@ GRAFANA_CLOUD_NAMESPACE := grafana-cloud
 .PHONY: lke-promtail
 lke-promtail: | lke-ctx $(CURL) $(LPASS)
 	curl -fsS https://raw.githubusercontent.com/grafana/loki/master/tools/promtail.sh \
-	| sh -s 8521 "$$($(LPASS) show --notes 1212835959730479797)" logs-prod-us-central1.grafana.net default | kubectl apply --namespace=default -f  -
+	| sh -s 8521 "$$($(LPASS) show --notes Shared-changelog/secrets/GRAFANA_CLOUD_lke-prod-20200426)" logs-prod-us-central1.grafana.net default | kubectl apply --namespace=default -f  -
 
 lke-provision:: lke-promtail
 
