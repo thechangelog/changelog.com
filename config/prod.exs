37d8e6f80ae49f216f76e3d08159f17fd1619ebe
Gerhard Lazu <gerhard@lazu.co.uk>
2020-12-20 01:44:50 +0000

    Fix default datasource & Grafana URL in prod
    Not ready to use Grafana Cloud just yet, using the local Grafana for now.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- config/prod.exs
+++ config/prod.exs
@@ -62,6 +62,6 @@ config :changelog, Changelog.PromEx,
   grafana: [
     host: SecretOrEnv.get("GRAFANA_URL"),
     auth_token: SecretOrEnv.get("GRAFANA_API_KEY"),
-    datasource_id: SecretOrEnv.get("GRAFANA_DATASOURCE_ID", "prometheus")
+    datasource_id: SecretOrEnv.get("GRAFANA_DATASOURCE_ID", "Prometheus")
   ],
   metrics_server: :disabled
