478530f27e2e1cd4bf4be13b47a3ca5f7915e01e
Gerhard Lazu <gerhard@lazu.co.uk>
2020-12-28 03:33:19 +0000

    Allow cookie to be used on all *.changelog.com URLs
    Including https://cdn.changelog.com
    
    cc @jerodsanto
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- lib/changelog_web/endpoint.ex
+++ lib/changelog_web/endpoint.ex
@@ -65,7 +65,8 @@ defmodule ChangelogWeb.Endpoint do
     encryption_salt: System.get_env("ENCRYPTION_SALT") || "8675309",
     max_age: 31_536_000,
     signing_salt: System.get_env("SIGNING_SALT") || "8bAOekZm",
-    extra: "SameSite=Lax"
+    extra: "SameSite=Lax",
+    domain: ".changelog.com"
 
   plug ChangelogWeb.Router
 end
