13b309cb00682a188161aa3562a49034b1871d0c
Alex Koutmos <akoutmos@gmail.com>
2021-01-08 10:46:00 -0500

    Check point for edit form appearing/disappearing

--- assets/app/modules/comment.js
+++ assets/app/modules/comment.js
@@ -1,112 +1,163 @@
-import autosize from "autosize";
-import Prism from "prismjs";
-import { u, ajax } from "umbrellajs";
+import autosize from 'autosize'
+import Prism from 'prismjs'
+import { u, ajax } from 'umbrellajs'
 
 export default class Comment {
   constructor(container) {
-    this.container = u(container);
-    this.id = this.container.data("id");
-    this.attachUI();
-    this.attachEvents();
-    this.attachKeyboardShortcuts();
-    container.comment = this;
+    this.container = u(container)
+    this.id = this.container.data('id')
+    this.attachUI()
+    this.attachEvents()
+    this.attachKeyboardShortcuts()
+    container.comment = this
   }
 
   attachUI() {
-    this.replyForm = this.container.children("form");
-    this.replyTextArea = this.replyForm.find("textarea").first();
-    this.previewArea = this.replyForm.find(".js-comment-preview-area");
-    this.collapseButton = this.container.children(".js-comment-collapse");
-    this.replyButton = this.container.children("footer").find(".js-comment-reply");
-    this.previewButton = this.replyForm.find(".js-comment-preview");
-    this.writeButton = this.replyForm.find(".js-comment-write");
-    this.replies = this.container.children(".comment-replies");
+    // Reply textbox and new text box actions
+    this.replyForm = this.container.children('form[data-context="reply"], form[data-context="new"]')
+    this.replyTextArea = this.replyForm.find('textarea').first()
+    this.previewArea = this.replyForm.find('.js-comment-preview-area')
+    this.collapseButton = this.container.children('.js-comment-collapse')
+    this.replyButton = this.container.children('footer').find('.js-comment-reply')
+    this.previewButton = this.replyForm.find('.js-comment-preview')
+    this.writeButton = this.replyForm.find('.js-comment-write')
+    this.replies = this.container.children('.comment-replies')
+
+    // Edit textbox actions
+    this.editForm = this.container.children('form[data-context="edit"]')
+    this.editButton = this.container.children('footer').find('.js-comment-edit')
+    this.editTextArea = this.editForm.find('textarea').first()
+    this.editPreviewArea = this.editForm.find('.js-comment-preview-area')
+    this.editPreviewButton = this.editForm.find('.js-comment-preview')
+    this.editWriteButton = this.editForm.find('.js-comment-write')
   }
 
   attachEvents() {
-    this.collapseButton.handle("click", _ => { this.container.toggleClass("is-collapsed") });
-    this.replyButton.handle("click", _ => { this.toggleReplyForm() });
-    this.previewButton.handle("click", _ => { this.showPreview() });
-    this.previewArea.handle("click", _ => { this.showWrite() });
-    this.writeButton.handle("click", _ => { this.showWrite() });
+    this.collapseButton.handle('click', (_) => {
+      this.container.toggleClass('is-collapsed')
+    })
+    this.replyButton.handle('click', (_) => {
+      this.toggleReplyForm()
+    })
+    this.editButton.handle('click', (_) => {
+      this.toggleEditForm()
+    })
+    this.previewButton.handle('click', (_) => {
+      this.showPreview()
+    })
+    this.previewArea.handle('click', (_) => {
+      this.showWrite()
+    })
+    this.writeButton.handle('click', (_) => {
+      this.showWrite()
+    })
   }
 
   attachKeyboardShortcuts() {
-    this.replyForm.on("keydown", event => {
+    this.replyForm.on('keydown', (event) => {
       // ctrl+enter or cmd+enter submits form
-      if ((event.metaKey || event.ctrlKey) && event.key == "Enter") {
-        event.preventDefault();
-        this.replyForm.trigger("submit");
+      if ((event.metaKey || event.ctrlKey) && event.key == 'Enter') {
+        event.preventDefault()
+        this.replyForm.trigger('submit')
       }
-    });
+    })
   }
 
   detectPermalink() {
     if (location.hash.match(this.id)) {
-      this.container.addClass("is-permalink");
+      this.container.addClass('is-permalink')
     } else {
-      this.container.removeClass("is-permalink");
+      this.container.removeClass('is-permalink')
     }
   }
 
   showPreview() {
     let options = {
-      method: "POST",
-      body: {"md": this.replyTextArea.value}
+      method: 'POST',
+      body: { md: this.replyTextArea.value }
     }
 
-    ajax("/news/comments/preview", options, (_err, resp) => {
-      this.previewArea.html(resp);
-      Prism.highlightAllUnder(this.previewArea.first());
-      this.replyForm.addClass("comment_form--preview");
-    });
+    ajax('/news/comments/preview', options, (_err, resp) => {
+      this.previewArea.html(resp)
+      Prism.highlightAllUnder(this.previewArea.first())
+      this.replyForm.addClass('comment_form--preview')
+    })
   }
 
   showWrite() {
-    let endPosition = this.replyTextArea.value.length;
-    this.previewArea.html("");
-    this.replyForm.removeClass("comment_form--preview");
-    this.replyTextArea.focus();
-    this.replyTextArea.setSelectionRange(endPosition, endPosition);
+    let endPosition = this.replyTextArea.value.length
+    this.previewArea.html('')
+    this.replyForm.removeClass('comment_form--preview')
+    this.replyTextArea.focus()
+    this.replyTextArea.setSelectionRange(endPosition, endPosition)
   }
 
+  // Reply form related functions
   toggleReplyForm() {
     if (!this.replyForm.length) {
-      Turbolinks.visit("/in");
-    } else if (this.replyForm.hasClass("is-hidden")) {
-      this.openReplyForm();
+      Turbolinks.visit('/in')
+    } else if (this.replyForm.hasClass('is-hidden')) {
+      this.openReplyForm()
     } else {
-      this.closeReplyForm();
+      this.closeReplyForm()
     }
   }
 
   openReplyForm() {
-    this.replyForm.removeClass("is-hidden");
-    this.replyTextArea.focus();
-    this.replyButton.text("cancel");
+    this.replyForm.removeClass('is-hidden')
+    this.replyTextArea.focus()
+    this.editButton.addClass('is-hidden')
+    this.replyButton.text('cancel')
   }
 
   closeReplyForm() {
-    this.replyForm.addClass("is-hidden");
-    this.replyButton.text("reply");
+    this.replyForm.addClass('is-hidden')
+    this.editButton.removeClass('is-hidden')
+    this.replyButton.text('reply')
   }
 
   clearReplyForm() {
-    this.showWrite();
-    this.replyForm.first().reset();
-    autosize.update(this.replyTextArea);
+    this.showWrite()
+    this.replyForm.first().reset()
+    autosize.update(this.replyTextArea)
+  }
+
+  // Edit form related functions
+  toggleEditForm() {
+    console.log(this.editForm)
+
+    if (!this.editForm.length) {
+      //Turbolinks.visit('/in')
+    } else if (this.editForm.hasClass('is-hidden')) {
+      this.openEditForm()
+    } else {
+      this.closeEditForm()
+    }
+  }
+
+  openEditForm() {
+    this.editForm.removeClass('is-hidden')
+    this.editTextArea.focus()
+    this.replyButton.addClass('is-hidden')
+    this.editButton.text('cancel')
+  }
+
+  closeEditForm() {
+    this.editForm.addClass('is-hidden')
+    this.replyButton.removeClass('is-hidden')
+    this.editButton.text('edit')
   }
 
   // this is only called on the very first comment form, not replies
   addComment(content) {
-    this.clearReplyForm();
-    this.container.after(content);
+    this.clearReplyForm()
+    this.container.after(content)
   }
 
   addReply(comment) {
-    this.replies.prepend(comment);
-    this.container.addClass("comment--has_replies");
-    this.clearReplyForm();
-    this.closeReplyForm();
+    this.replies.prepend(comment)
+    this.container.addClass('comment--has_replies')
+    this.clearReplyForm()
+    this.closeReplyForm()
   }
 }
