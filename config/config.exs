129070148afb9f13c630bf999ed84223a0c60284
Jerod Santo <jerod@changelog.com>
2021-03-17 08:35:23 -0500

    Set up an event filter for Sentry errors

--- config/config.exs
+++ config/config.exs
@@ -81,7 +81,8 @@ config :mime, :types, %{"application/javascript" => ["js"], "application/xml" =>
 config :sentry,
   dsn: "https://2b1aed8f16f5404cb2bc79b855f2f92d@o546963.ingest.sentry.io/5668962",
   included_environments: [:prod],
-  environment_name: Mix.env()
+  environment_name: Mix.env(),
+  filter: Changelog.Sentry.EventFilter
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
---
+++ lib/changelog/sentry.ex
@@ -0,0 +1,11 @@
+defmodule Changelog.Sentry do
+  defmodule EventFilter do
+    @behaviour Sentry.EventFilter
+
+    def exclude_exception?(%Phoenix.Router.NoRouteError{}, :plug), do: true
+    def exclude_exception?(%Ecto.NoResultsError{}, :plug), do: true
+    def exclude_exception?(%Phoenix.NotAcceptableError{}, :plug), do: true
+
+    def exclude_exception?(_exception, _source), do: false
+  end
+end
