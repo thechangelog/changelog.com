8e6ff3bf7021265f75247ba4351e6fb2953e79ee
Gerhard Lazu <gerhard@lazu.co.uk>
2021-04-06 00:18:30 +0100

    Use Helm chart version for ingress-nginx and use LBs
    external-dns-dnsimple only recognizes the first IP address, and we want
    to keep the same LKE stack that we had in 2020.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2021/ingress-nginx.mk
+++ 2021/ingress-nginx.mk
@@ -1,10 +1,10 @@
 INGRESS_NGINX_RELEASES := https://github.com/kubernetes/ingress-nginx/releases
-INGRESS_NGINX_VERSION := 0.44.0
+INGRESS_NGINX_VERSION := 3.26.0
 INGRESS_NGINX_DIR := $(CURDIR)/tmp/ingress-nginx-$(INGRESS_NGINX_VERSION)
 
 $(INGRESS_NGINX_DIR):
 	git clone \
-	  --branch controller-v$(INGRESS_NGINX_VERSION) --single-branch --depth 1 \
+	  --branch helm-chart-$(INGRESS_NGINX_VERSION) --single-branch --depth 1 \
 	  https://github.com/kubernetes/ingress-nginx.git $(INGRESS_NGINX_DIR)
 tmp/ingress-nginx: $(INGRESS_NGINX_DIR)
 
@@ -17,8 +17,6 @@ lke-ingress-nginx: | $(INGRESS_NGINX_DIR) lke-ctx $(HELM)
 	  --set controller.dnsPolicy=ClusterFirstWithHostNet \
 	  --set controller.hostNetwork=true \
 	  --set controller.kind=DaemonSet \
-	  --set controller.service.enabled=false \
-	  --set controller.publishService.enabled=false \
 	  --version $(INGRESS_NGINX_VERSION)
 	$(KUBECTL) apply --filename $(CURDIR)/manifests/ingress-nginx
 lke-bootstrap:: lke-ingress-nginx
