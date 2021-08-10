19c4dcef8c5d840547e8303ad7965fc27a024a5c
Jerod Santo <jerod@changelog.com>
2021-08-10 12:57:51 -0500

    Add apple podcasts meta element to episode pages

--- lib/changelog_web/meta/apple.ex
+++ lib/changelog_web/meta/apple.ex
@@ -1,6 +1,12 @@
 defmodule ChangelogWeb.Meta.Apple do
-  alias ChangelogWeb.PodcastView
+  alias ChangelogWeb.{EpisodeView, PodcastView}
 
+  # Episode page
+  def apple_podcasts_id(%{view_module: EpisodeView, view_template: "show.html", podcast: podcast, episode: _episode}) do
+    apple_podcasts_id(%{view_module: PodcastView, podcast: podcast})
+  end
+
+  # Podcast pages
   def apple_podcasts_id(%{view_module: PodcastView, podcast: podcast}) do
     if url = podcast.apple_url do
       url
