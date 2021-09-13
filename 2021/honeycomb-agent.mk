5d6b9754c2c8bf9e5cc66e292ffdff162491aee6
Gerhard Lazu <gerhard@lazu.co.uk>
2021-09-13 23:37:12 +0100

    Integrate Honeycomb with LKE
    The first step after https://changelog.com/shipit/11 - more is coming 😉
    
    Somewhat related to
    https://github.com/thechangelog/changelog.com/issues/383, especially the
    "With better observability tooling..." part.
    
    There is a write-up to this that will be shared with @charity and
    @christineyen directly 🙌
    
    cc @adamstac @jerodsanto
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

---
+++ 2021/honeycomb-agent.mk
@@ -0,0 +1,23 @@
+HONEYCOMB_AGENT_RELEASES := https://github.com/honeycombio/helm-charts/releases
+HONEYCOMB_AGENT_VERSION := 1.0.4
+HONEYCOMB_AGENT_DIR := $(CURDIR)/tmp/honeycomb-agent-$(HONEYCOMB_AGENT_VERSION)
+
+.PHONY: releases-honeycomb-agent
+releases-honeycomb-agent:
+	$(OPEN) $(HONEYCOMB_AGENT_RELEASES)
+
+$(HONEYCOMB_AGENT_DIR):
+	git clone \
+	  --branch v$(HONEYCOMB_AGENT_VERSION)-honeycomb --single-branch --depth 1 \
+	  https://github.com/honeycombio/helm-charts.git $(@)
+tmp/honeycomb-agent: $(HONEYCOMB_AGENT_DIR)
+
+lke-honeycomb-agent: | $(HONEYCOMB_AGENT_DIR) lke-ctx $(HELM)
+	$(HELM) upgrade honeycomb $(HONEYCOMB_AGENT_DIR)/charts/honeycomb \
+	  --install \
+	  --namespace honeycomb --create-namespace \
+	  --values $(CURDIR)/manifests/honeycomb-agent.values.yml \
+	  --set honeycomb.apiKey="$$($(LPASS) show --notes Shared-changelog/secrets/HONEYCOMB_API_KEY)" \
+	  --set metrics.clusterName=$(LKE_LABEL)
+
+lke-bootstrap:: | lke-honeycomb-agent
