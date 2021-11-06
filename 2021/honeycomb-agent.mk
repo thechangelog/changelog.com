58e3aebc2f0f9ac36f4db2397b5df7f1dbbdb942
Gerhard Lazu <gerhard@lazu.co.uk>
2021-11-06 13:18:16 +0000

    Save ingress-nginx, Fastly content & service stats Honeycomb boards
    If config is not versioned, it's ephemeral and can be lost. We don't
    want to lose these boards, which makes me think that we should do the
    same for our Fastly service & Grafana Cloud dashboards.
    
    Useful links:
    - https://docs.fastly.com/en/guides/setting-up-remote-log-streaming
    - https://docs.fastly.com/en/guides/log-streaming-honeycomb#example-format
    - https://docs.fastly.com/en/guides/useful-variables-to-log
    - https://3.basecamp.com/4543941/buckets/19947330/todos/4295436527
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2021/honeycomb-agent.mk
+++ 2021/honeycomb-agent.mk
@@ -22,3 +22,26 @@ lke-honeycomb-agent: | $(HONEYCOMB_AGENT_DIR) lke-ctx $(HELM) $(LPASS)
 	  --set metrics.clusterName=$(LKE_LABEL)
 
 lke-bootstrap:: | lke-honeycomb-agent
+
+define honeycomb_board_save
+@printf "Adding Honeycomb board $(BOLD)$(1)$(NORMAL) to this repository... "
+@$(CURL) --fail --location --silent https://api.honeycomb.io/1/boards/$(2) \
+	--header "X-Honeycomb-Team: $(HONEYCOMB_API_KEY)" \
+| $(JQ) . \
+> $(CURDIR)/../priv/honeycomb_boards/$(1).json
+@printf "$(BOLD)$(GREEN)OK!$(NORMAL)\n"
+endef
+
+honeycomb-boards-save:: | $(CURL) $(LPASS) $(JQ)
+
+honeycomb-board-save-ingress-nginx:
+	$(call honeycomb_board_save,ingress-nginx,qGFoG4oBrJ)
+honeycomb-boards-save:: honeycomb-board-save-ingress-nginx
+
+honeycomb-board-save-fastly-content-stats:
+	$(call honeycomb_board_save,Fastly.Content.Stats,GBTjsTkY7A5)
+honeycomb-boards-save:: honeycomb-board-save-fastly-content-stats
+
+honeycomb-board-save-fastly-service-stats:
+	$(call honeycomb_board_save,Fastly.Service.Stats,FX2Pg5y6NDw)
+honeycomb-boards-save:: honeycomb-board-save-fastly-service-stats
