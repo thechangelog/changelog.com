eacdd50683ff150baa8543855257685807cf31a3
Gerhard Lazu <gerhard@lazu.co.uk>
2021-11-17 07:56:27 +0000

    Start the Honeycomb experiment
    So that we can answer
    https://github.com/thechangelog/changelog.com/issues/383
    and other reasons 😉
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2021/honeycomb-agent.mk
+++ 2021/honeycomb-agent.mk
@@ -23,6 +23,7 @@ lke-honeycomb-agent: | $(HONEYCOMB_AGENT_DIR) lke-ctx $(HELM) $(LPASS)
 
 lke-bootstrap:: | lke-honeycomb-agent
 
+# @lizthegrey: "Instead of boards, start from queries, and keep refining those until you find your answer"
 define honeycomb_board_save
 @printf "Adding Honeycomb board $(BOLD)$(1)$(NORMAL) to this repository... "
 @$(CURL) --fail --location --silent https://api.honeycomb.io/1/boards/$(2) \
@@ -45,3 +46,11 @@ honeycomb-boards-save:: honeycomb-board-save-fastly-content-stats
 honeycomb-board-save-fastly-service-stats:
 	$(call honeycomb_board_save,Fastly.Service.Stats,FX2Pg5y6NDw)
 honeycomb-boards-save:: honeycomb-board-save-fastly-service-stats
+
+# - [ ] Answer https://github.com/thechangelog/changelog.com/issues/383
+# - [ ] Integrate CircleCI using https://circleci.com/developer/orbs/orb/honeycombio/buildevents
+# - [ ] Integrate Phoenix using https://github.com/open-telemetry/opentelemetry-erlang-contrib/tree/main/instrumentation/opentelemetry_phoenix
+# - [ ] Use derived columns in boards
+#   	- time_elapsed_seconds in https://ui.honeycomb.io/changelog/datasets/fastly/schema
+#   	- https://docs.honeycomb.io/working-with-your-data/customizing-your-query/derived-columns/reference/
+#   	- https://www.honeycomb.io/resources/derived-columns/
