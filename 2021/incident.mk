130e5e1ee74a2422832d7ce022a63f68071dd186
Gerhard Lazu <gerhard@lazu.co.uk>
2021-11-17 08:02:17 +0000

    Start the incident.io experiment
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

---
+++ 2021/incident.mk
@@ -0,0 +1,7 @@
+# - [ ] Start a new incident
+# - [ ] Describe why we are doing it
+# - [ ] Capture the first step
+#   	- Experiment Driven Incidents (E.D.I. for short)
+# - [ ] Run the first step
+# - [ ] Continue with the other steps
+# - [ ] Wrap-up the incident
