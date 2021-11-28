018194e864e8e4d2cfbd6aa7e3c64a8c171c7d50
Gerhard Lazu <gerhard@lazu.co.uk>
2021-11-28 12:05:30 +0000

    Install up CLI & connect to a new Upbound Cloud hosted control plane
    This worked at charm:
    
        make .env -B && . .env
        make up
        up ctp create christmas-2021-gift
    
        up ctp list
        NAME                  ID                                     SELF-HOSTED   STATUS
        shipit-15-followup    16e3a96e-6505-4ad8-af96-098d5ba1eba4   false         deleting
        christmas-2021-gift   bd465974-fb45-47a4-b9a5-001db23c89c8   false         ready
    
        m up_connect
    
    While the previous control plane is still deleting, the new one is ready.
    
    As for the new control plane, I was not expecting the following:
    - runs on K8s 1.18 - EOS since 2021.04
    - uses Crossplane 1.3.1-up.1 - latest is 1.5.1
    
    I think the latter will be a blocker for Terrajet, which I suspect to
    require Crossplane 1.4, even though my instinct was to go with 1.5. I
    will experiment with the hosted control plane, even though I am
    expecting to need to go with a self-hosted Crossplane 1.5.
    
    In terms of feedback, it would have been great to see in the up cli -
    up ctp list - which version of Crossplane the control planes hosted in
    Upbound are running. Same for K8s. It would have been even more amazing
    to be able to install these with min versions, e.g. 1.21 min. for K8s &
    1.5 min. version for Crossplane.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2021/crossplane.mk
+++ 2021/crossplane.mk
@@ -1,10 +1,44 @@
-# - [ ] Delete providers from https://cloud.upbound.io/changelogmedia/controlPlanes/16e3a96e-6505-4ad8-af96-098d5ba1eba4/settings
-# - [ ] Delete control plane
-# - [ ] Create new control plane - ensure it gets provisioned as v1.4.x-up
-# - [ ] Use provider-tf-template to generate a Linode Crossplane provider from Terraform
-# 	- https://github.com/crossplane-contrib/provider-tf-template
-# 	- https://github.com/linode/terraform-provider-linode + https://registry.terraform.io/providers/linode/linode/latest/docs
-# 	- https://github.com/crossplane-contrib/terrajet (the tool behind it)
-# - [ ] Use a composite resource to provision the cluster + node pools, don't provision them separately
-# 	- https://github.com/upbound/platform-ref-multi-k8s
-# - [ ] Provision baseline K8s components via the composite resource, e.g. external-dns, traefik, etc.
+UP_RELEASES := https://cli.upbound.io/stable
+UP_VERSION := 0.5.0
+UP_BIN := up-$(UP_VERSION)-$(platform)-amd64
+https://cli.upbound.io/stable/v0.5.0/bin/darwin_amd64/up
+UP_URL := $(UP_RELEASES)/v$(UP_VERSION)/bin/$(platform)_amd64/up
+UP := $(LOCAL_BIN)/$(UP_BIN)
+$(UP): | $(CURL) $(LOCAL_BIN)
+	$(CURL) --progress-bar --fail --location --output $(UP) "$(UP_URL)"
+	touch $(UP)
+	chmod +x $(UP)
+	$(UP) --version | grep $(UP_VERSION)
+	ln -sf $(UP) $(LOCAL_BIN)/up
+.PHONY: up
+up: $(UP)
+.PHONY: releases-up
+releases-up:
+	$(OPEN) $(UP_RELEASES)
+
+# christmas-2021-gift:
+# https://cloud.upbound.io/changelogmedia/controlPlanes/bd465974-fb45-47a4-b9a5-001db23c89c8
+UPBOUND_CONTROLPLANE_ID := bd465974-fb45-47a4-b9a5-001db23c89c8
+UPBOUND_KUBECONFIG := $(XDG_CONFIG_HOME)/upbound.$(UPBOUND_CONTROLPLANE_ID).yml
+$(UPBOUND_KUBECONFIG):
+	touch $(UPBOUND_KUBECONFIG)
+
+env:: | $(LPASS)
+	@$(LPASS) status --quiet \
+	|| ( printf "$(RED)LastPass session expired, run $(BOLD)lpass login <YOUR_EMAIL_ADDRESS>$(NORMAL)\n" ; exit 1 )
+	@echo 'export UP_TOKEN="$(shell $(LPASS) show --notes Shared-changelog/secrets/UPBOUND_CLOUD_USER_TOKEN)"'
+	@echo 'export UP_ACCOUNT="changelogmedia"'
+	@echo 'export UP_PROFILE="default"'
+
+$(HOME)/.up/config.json: | $(UP)
+	$(UP) login
+
+up_connect: $(HOME)/.up/config.json $(UPBOUND_KUBECONFIG)
+ifndef UP_TOKEN
+	$(call env_not_set,UP_TOKEN)
+endif
+	@printf "Connecting to Upbound Cloud Control Plane $(BOLD)$(UPBOUND_CONTROLPLANE_ID)$(NORMAL) ... "
+	@KUBECONFIG=$(UPBOUND_KUBECONFIG) $(UP) controlplane kubeconfig get \
+		   --token=$(UP_TOKEN) $(UPBOUND_CONTROLPLANE_ID)
+	@printf "$(BOLD)$(GREEN)OK!$(NORMAL)\n"
+	@printf "To use this control plane, run: $(BOLD)export KUBECONFIG=$(UPBOUND_KUBECONFIG)$(NORMAL)\n"
