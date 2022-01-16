764d4650498ad973e8c2c7759d8bbfbe13cbc5ac
Gerhard Lazu <gerhard@lazu.co.uk>
2022-01-16 22:38:53 +0000

    Install latest external-dns via Helm in the new 2022 LKE
    Sticking to Helm so that we can Crossplane this via the Helm provider.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2022/external-dns.mk
+++ 2022/external-dns.mk
@@ -1,31 +1,43 @@
 EXTERNAL_DNS_RELEASES := https://github.com/kubernetes-sigs/external-dns/releases
-EXTERNAL_DNS_VERSION := v0.9.0
-EXTERNAL_DNS_IMAGE := k8s.gcr.io/external-dns/external-dns:$(EXTERNAL_DNS_VERSION)
+EXTERNAL_DNS_VERSION := 1.7.1
+EXTERNAL_DNS_DIR := $(CURDIR)/tmp/external-dns-$(EXTERNAL_DNS_VERSION)
 EXTERNAL_DNS_NAMESPACE := external-dns
-EXTERNAL_DNS_CRD_MANIFEST := $(MANIFESTS)/external-dns-crd-manifest-$(EXTERNAL_DNS_VERSION).yml
-EXTERNAL_DNS_CRD_MANIFEST_URL := https://raw.githubusercontent.com/kubernetes-sigs/external-dns/$(EXTERNAL_DNS_VERSION)/docs/contributing/crd-source/crd-manifest.yaml
 EXTERNAL_DNS_TXT_OWNER_ID ?= linode-kubernetes-engine
 EXTERNAL_DNS_LOG_LEVEL ?= debug
 # Change to sync when existing DNS records should be updated
 EXTERNAL_DNS_POLICY ?= upsert-only
+
+$(EXTERNAL_DNS_DIR):
+	git clone \
+	  --branch external-dns-helm-chart-$(EXTERNAL_DNS_VERSION) --single-branch --depth 1 \
+	  https://github.com/kubernetes-sigs/external-dns $(EXTERNAL_DNS_DIR)
+tmp/external-dns: $(EXTERNAL_DNS_DIR)
+
 .PHONY: lke-external-dns
-lke-external-dns: | lke-ctx $(ENVSUBST) $(EXTERNAL_DNS_CRD_MANIFEST)
-	$(KUBECTL) $(K_CMD) --filename $(EXTERNAL_DNS_CRD_MANIFEST)
-	export NAMESPACE=$(EXTERNAL_DNS_NAMESPACE) \
-	; export IMAGE=$(EXTERNAL_DNS_IMAGE) \
-	; export TXT_OWNER_ID=$(EXTERNAL_DNS_TXT_OWNER_ID) \
-	; export LOG_LEVEL=$(EXTERNAL_DNS_LOG_LEVEL) \
-	; export POLICY=$(EXTERNAL_DNS_POLICY) \
-	; cat $(CURDIR)/manifests/external-dns.yml \
-	| $(ENVSUBST) -no-unset \
-	| $(KUBECTL) $(K_CMD) --filename -
+lke-external-dns: | $(EXTERNAL_DNS_DIR) lke-ctx $(HELM)
+	$(HELM) upgrade external-dns $(EXTERNAL_DNS_DIR)/charts/external-dns \
+	  --install \
+	  --namespace $(EXTERNAL_DNS_NAMESPACE) --create-namespace \
+	  --values $(EXTERNAL_DNS_DIR)/charts/external-dns/values.yaml \
+	  --set txtOwnerId=$(EXTERNAL_DNS_TXT_OWNER_ID) \
+	  --set txtPrefix=_external-dns. \
+	  --set logLevel=$(EXTERNAL_DNS_LOG_LEVEL) \
+	  --set sources[0]=crd \
+	  --set policy=$(EXTERNAL_DNS_POLICY) \
+	  --set extraArgs[0]=--crd-source-apiversion=externaldns.k8s.io/v1alpha1 \
+	  --set extraArgs[1]=--crd-source-kind=DNSEndpoint \
+	  --set extraArgs[2]=--events \
+	  --set provider=dnsimple \
+	  --set env[0].name=DNSIMPLE_OAUTH \
+	  --set env[0].valueFrom.secretKeyRef.name=dnsimple \
+	  --set env[0].valueFrom.secretKeyRef.key=token \
+	  --version $(EXTERNAL_DNS_VERSION)
+	$(KUBECTL) $(K_CMD) --filename $(EXTERNAL_DNS_DIR)/docs/contributing/crd-source/crd-manifest.yaml
 	$(KUBECTL) create secret generic dnsimple \
 	  --from-literal=token="$$($(LPASS) show --notes Shared-changelog/secrets/DNSIMPLE_TOKEN)" --dry-run=client --output json \
 	| $(KUBECTL) $(K_CMD) --namespace $(EXTERNAL_DNS_NAMESPACE) --filename -
 lke-bootstrap:: | lke-external-dns
 
-$(EXTERNAL_DNS_CRD_MANIFEST): | $(CURL)
-	$(CURL) --progress-bar --fail --location --output $(EXTERNAL_DNS_CRD_MANIFEST) $(EXTERNAL_DNS_CRD_MANIFEST_URL)
 
 .PHONY: releases-external-dns
 releases-external-dns:
