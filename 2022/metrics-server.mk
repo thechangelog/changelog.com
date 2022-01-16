db655268f51dc3af922f9cdcbfb9505cb1413de5
Gerhard Lazu <gerhard@lazu.co.uk>
2022-01-16 18:00:22 +0000

    Install metrics-server via Helm chart in new 2022 LKE
    This no longer uses gsed - it's cleaner! - and it will work better when
    Crossplane will provision this part of K8s cluster bootstrapping.
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2022/metrics-server.mk
+++ 2022/metrics-server.mk
@@ -1,15 +1,24 @@
 METRICS_SERVER_RELEASES := https://github.com/kubernetes-sigs/metrics-server/releases
-METRICS_SERVER_VERSION := v0.5.2
+METRICS_SERVER_VERSION := 3.7.0
+METRICS_SERVER_DIR := $(CURDIR)/tmp/metrics-server-$(METRICS_SERVER_VERSION)
 
-releases-metrics-server:
-	$(OPEN) $(METRICS_SERVER_RELEASES)
+$(METRICS_SERVER_DIR):
+	git clone \
+	  --branch metrics-server-helm-chart-$(METRICS_SERVER_VERSION) --single-branch --depth 1 \
+	  https://github.com/kubernetes-sigs/metrics-server $(METRICS_SERVER_DIR)
+tmp/metrics-server: $(METRICS_SERVER_DIR)
 
 # Add metrics-server for k9s pulses view to work fully
-# TODO: replace gnu-sed with yq, ytt, or any tool that was BUILT to manipulate YAML
 .PHONY: lke-metrics-server
-lke-metrics-server: | lke-ctx
-	$(CURL) --silent --location https://github.com/kubernetes-sigs/metrics-server/releases/download/$(METRICS_SERVER_VERSION)/components.yaml \
-	| gsed '/- --secure-port/a\ \ \ \ \ \ \ \ - --kubelet-insecure-tls' \
-	| $(KUBECTL) $(K_CMD) --filename -
-
+lke-metrics-server: | $(METRICS_SERVER_DIR) lke-ctx $(HELM)
+	$(HELM) upgrade metrics-server $(METRICS_SERVER_DIR)/charts/metrics-server \
+	  --install \
+	  --namespace metrics-server --create-namespace \
+	  --values $(METRICS_SERVER_DIR)/charts/metrics-server/values.yaml \
+	  --set args[0]=--kubelet-insecure-tls \
+	  --version $(METRICS_SERVER_VERSION)
 lke-bootstrap:: | lke-metrics-server
+
+.PHONY: releases-metrics-server
+releases-metrics-server:
+	$(OPEN) $(METRICS_SERVER_RELEASES)
