1597ad6084e6213b019e762a3bc35198c935b53b
Jerod Santo <jerod@changelog.com>
2022-06-28 12:13:36 -0500

    Embed podcast clips on our 404 and 500 error pages

--- lib/changelog_web/views/error_view.ex
+++ lib/changelog_web/views/error_view.ex
@@ -1,9 +1,9 @@
 defmodule ChangelogWeb.ErrorView do
   use ChangelogWeb, :public_view
 
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
-  def template_not_found(_template, assigns) do
-    render("500.html", assigns)
+  alias ChangelogWeb.SharedView
+
+  def template_not_found(template, _assigns) do
+    Phoenix.Controller.status_message_from_template(template)
   end
 end
