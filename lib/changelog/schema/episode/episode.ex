0a2dedcecde5d01252465a1683f67eae78ffd33e
Jerod Santo <jerod@changelog.com>
2022-08-04 10:55:09 -0500

    Move EpisodeChapter module to its own file

--- lib/changelog/schema/episode/episode.ex
+++ lib/changelog/schema/episode/episode.ex
@@ -4,8 +4,9 @@ defmodule Changelog.Episode do
   require Logger
 
   alias Changelog.{
-    EpisodeHost,
+    EpisodeChapter,
     EpisodeGuest,
+    EpisodeHost,
     EpisodeRequest,
     EpisodeTopic,
     EpisodeStat,
@@ -21,39 +22,6 @@ defmodule Changelog.Episode do
   }
 
   alias ChangelogWeb.{TimeView}
-
-  defmodule Chapter do
-    use Changelog.Schema
-
-    embedded_schema do
-      field :title, :string
-      field :starts_at, :float
-      field :ends_at, :float
-      field :link_url, :string
-      field :image_url, :string
-
-      field :delete, :boolean, virtual: true
-    end
-
-    def changeset(chapter, attrs \\ %{}) do
-      chapter
-      |> cast(attrs, ~w(title starts_at ends_at link_url image_url delete)a)
-      |> validate_required([:title, :starts_at])
-      |> validate_ends_at_after_starts_at()
-      |> mark_for_deletion()
-    end
-
-    defp validate_ends_at_after_starts_at(changeset) do
-      starts_at = get_field(changeset, :starts_at)
-      ends_at = get_field(changeset, :ends_at)
-
-      cond do
-        !is_nil(ends_at) && is_nil(starts_at) -> add_error(changeset, :ends_at, "cannot be set without 'starts at'")
-        !is_nil(ends_at) && starts_at >= ends_at -> add_error(changeset, :ends_at, "must be later than 'starts at'")
-        true -> changeset
-      end
-    end
-  end
 
   defenum(Type, full: 0, bonus: 1, trailer: 2)
 
@@ -83,13 +51,13 @@ defmodule Changelog.Episode do
     field :audio_bytes, :integer
     field :audio_duration, :integer
 
-    embeds_many :audio_chapters, Chapter
+    embeds_many :audio_chapters, EpisodeChapter, on_replace: :delete
 
     field :plusplus_file, Files.PlusPlus.Type
     field :plusplus_bytes, :integer
     field :plusplus_duration, :integer
 
-    embeds_many :plusplus_chapters, Chapter
+    embeds_many :plusplus_chapters, EpisodeChapter, on_replace: :delete
 
     field :download_count, :float
     field :import_count, :float
---
+++ lib/changelog/schema/episode/episode_chapter.ex
@@ -0,0 +1,32 @@
+defmodule Changelog.EpisodeChapter do
+  use Changelog.Schema
+
+  embedded_schema do
+    field :title, :string
+    field :starts_at, :float
+    field :ends_at, :float
+    field :link_url, :string
+    field :image_url, :string
+
+    field :delete, :boolean, virtual: true
+  end
+
+  def changeset(chapter, attrs \\ %{}) do
+    chapter
+    |> cast(attrs, ~w(title starts_at ends_at link_url image_url delete)a)
+    |> validate_required([:title, :starts_at])
+    |> validate_ends_at_after_starts_at()
+    |> mark_for_deletion()
+  end
+
+  defp validate_ends_at_after_starts_at(changeset) do
+    starts_at = get_field(changeset, :starts_at)
+    ends_at = get_field(changeset, :ends_at)
+
+    cond do
+      !is_nil(ends_at) && is_nil(starts_at) -> add_error(changeset, :ends_at, "cannot be set without 'starts at'")
+      !is_nil(ends_at) && starts_at >= ends_at -> add_error(changeset, :ends_at, "must be later than 'starts at'")
+      true -> changeset
+    end
+  end
+end
