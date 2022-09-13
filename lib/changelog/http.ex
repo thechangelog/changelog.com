12093a24223edbcbfa8ac7e7f0e00af9d8b8970f
Jerod Santo <jerod@changelog.com>
2022-09-13 17:11:55 -0500

    Only handle OTP 24.3.4.4 bug when it causes errors

--- lib/changelog/http.ex
+++ lib/changelog/http.ex
@@ -4,26 +4,43 @@ defmodule Changelog.HTTP do
   headers & options when making outbound HTTP requests
   """
 
-  @options []
+  @options [ssl: [{:middlebox_comp_mode, false}]]
 
   def request(method, body \\ "", headers \\ [], options \\ []) do
-    HTTPoison.request(method, body, headers, with_shared_options(options))
+    case HTTPoison.request(method, body, headers, options) do
+      {:error, _} -> HTTPoison.request(method, body, headers, with_shared_options(options))
+      response -> response
+    end
   end
 
   def get(url, headers \\ [], options \\ []) do
-    HTTPoison.get(url, headers, with_shared_options(options))
+    case HTTPoison.get(url, headers, options) do
+      {:error, _} -> HTTPoison.get(url, headers, with_shared_options(options))
+      response -> response
+    end
   end
 
   def get!(url, headers \\ [], options \\ []) do
-    HTTPoison.get(url, headers, with_shared_options(options))
+    try do
+      HTTPoison.get!(url, headers, options)
+    rescue
+      _ -> HTTPoison.get!(url, headers, with_shared_options(options))
+    end
   end
 
   def post(url, body, headers \\ [], options \\ []) do
-    HTTPoison.post!(url, body, headers, with_shared_options(options))
+    case HTTPoison.post(url, body, headers, options) do
+      {:error, _} -> HTTPoison.post(url, body, headers, with_shared_options(options))
+      response -> response
+    end
   end
 
   def post!(url, body, headers \\ [], options \\ []) do
-    HTTPoison.post!(url, body, headers, with_shared_options(options))
+    try do
+      HTTPoison.post!(url, body, headers, options)
+    rescue
+      _ -> HTTPoison.post!(url, body, headers, with_shared_options(options))
+    end
   end
 
   defp with_shared_options(options), do: Keyword.merge(options, @options)
--- test/changelog/http_test.exs
+++ test/changelog/http_test.exs
@@ -5,9 +5,16 @@ defmodule Changelog.HTTPTest do
 
   test "we can make HTTP requests to AWS" do
     assert {:ok, _body} = HTTP.get("https://aws.amazon.com")
+    assert %{body: _} = HTTP.get!("https://aws.amazon.com")
   end
 
   test "we can make HTTP requests to GitHub" do
     assert {:ok, _body} = HTTP.get("https://api.github.com")
+    assert %{body: _} = HTTP.get!("https://api.github.com")
+  end
+
+  test "we can make HTTP requests to cdn.changelog.com" do
+    assert {:ok, _body} = HTTP.get("https://cdn.changelog.com")
+    assert %{body: _} = HTTP.get!("https://cdn.changelog.com")
   end
 end
