be5234b5e55230438a7dfffc49952bff54ef35f0
Jerod Santo <jerod@changelog.com>
2023-03-27 14:08:41 -0500

    Fix a redirect

--- lib/changelog_web/plugs/vanity_domains.ex
+++ lib/changelog_web/plugs/vanity_domains.ex
@@ -45,6 +45,9 @@ defmodule ChangelogWeb.Plug.VanityDomains do
   defp determine_destination(%{slug: "podcast"}, ["edu"]) do
     "https://changelog.fm/462"
   end
+  defp determine_destination(%{slug: "podcast"}, ["news-2023-03-37"]) do
+    "https://changelog.fm/news-2023-03-27"
+  end
   defp determine_destination(%{slug: "gotime"}, ["gs"]) do
     "https://changelog.typeform.com/to/IR4twWc6"
   end
