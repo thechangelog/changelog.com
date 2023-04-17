734b15e2487b38ed03e338cd7a20ccd8960be96d
Jerod Santo <jerod@changelog.com>
2023-04-17 12:05:21 -0500

    [admin] Remove some unused aliases

--- lib/changelog_web/views/admin/page_view.ex
+++ lib/changelog_web/views/admin/page_view.ex
@@ -3,7 +3,7 @@ defmodule ChangelogWeb.Admin.PageView do
 
   alias Changelog.{Episode, Repo}
   alias ChangelogWeb.TimeView
-  alias ChangelogWeb.Admin.{EpisodeView, EpisodeRequestView, NewsItemView, NewsletterView}
+  alias ChangelogWeb.Admin.{EpisodeView, EpisodeRequestView}
 
   def recent_episodes(podcast, limit \\ 5) do
     podcast
