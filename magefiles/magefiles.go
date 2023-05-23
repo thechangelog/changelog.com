a19f94ab7b2f43c075df53c40a5fe3c5eb211dc5
Gerhard Lazu <gerhard@changelog.com>
2023-05-23 12:43:12 +0100

    Share the same Dagger Client across all mage tasks
    Rather than opening multiple connections from each task when they run in
    parallel, open one connection and share it via Go's context.
    
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>

--- magefiles/magefiles.go
+++ magefiles/magefiles.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"dagger.io/dagger"
@@ -16,6 +17,13 @@ import (
 
 // Run the CI pipeline
 func CI(ctx context.Context) {
+	defer sysexit.Handle()
+
+	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	mustBeAvailable(err)
+	defer dag.Close()
+
+	ctx = context.WithValue(ctx, "dag", dag)
 	mg.CtxDeps(ctx, Image.Runtime, Test, Image.Production)
 }
 
@@ -30,9 +38,14 @@ type Image mg.Namespace
 func (Image) Runtime(ctx context.Context) {
 	defer sysexit.Handle()
 
-	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	mustBeAvailable(err)
-	defer dag.Close()
+	var dag *dagger.Client
+	var err error
+	dag, ok := ctx.Value("dag").(*dagger.Client)
+	if !ok {
+		dag, err = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+		mustBeAvailable(err)
+		defer dag.Close()
+	}
 
 	image.New(ctx, dag.Pipeline("📦 RUNTIME IMAGE")).
 		Runtime().
@@ -43,9 +56,14 @@ func (Image) Runtime(ctx context.Context) {
 func (Image) Production(ctx context.Context) {
 	defer sysexit.Handle()
 
-	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	mustBeAvailable(err)
-	defer dag.Close()
+	var dag *dagger.Client
+	var err error
+	dag, ok := ctx.Value("dag").(*dagger.Client)
+	if !ok {
+		dag, err = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+		mustBeAvailable(err)
+		defer dag.Close()
+	}
 
 	image.New(ctx, dag.Pipeline("🎁 PRODUCTION IMAGE")).
 		ProductionClean().
@@ -55,9 +73,15 @@ func (Image) Production(ctx context.Context) {
 // Run tests
 func Test(ctx context.Context) {
 	defer sysexit.Handle()
-	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	mustBeAvailable(err)
-	defer dag.Close()
+
+	var dag *dagger.Client
+	var err error
+	dag, ok := ctx.Value("dag").(*dagger.Client)
+	if !ok {
+		dag, err = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+		mustBeAvailable(err)
+		defer dag.Close()
+	}
 
 	image.New(ctx, dag.Pipeline("🧰 TEST")).Test()
 }
@@ -68,9 +92,14 @@ type Fly mg.Namespace
 func (Fly) Deploy(ctx context.Context) {
 	defer sysexit.Handle()
 
-	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	mustBeAvailable(err)
-	defer dag.Close()
+	var dag *dagger.Client
+	var err error
+	dag, ok := ctx.Value("dag").(*dagger.Client)
+	if !ok {
+		dag, err = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+		mustBeAvailable(err)
+		defer dag.Close()
+	}
 
 	image.New(ctx, dag.Pipeline("🏁 DEPLOY")).Deploy()
 }
