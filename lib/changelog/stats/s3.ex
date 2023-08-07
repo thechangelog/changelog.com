33f236b0494b5e06c89fa991e75211af41bd69e4
Jerod Santo <jerod@changelog.com>
2023-08-07 07:37:46 -0500

    Move S3 stats config to function so it's evaluated at run time

--- lib/changelog/stats/s3.ex
+++ lib/changelog/stats/s3.ex
@@ -1,15 +1,9 @@
 defmodule Changelog.Stats.S3 do
-  @config [
-      access_key_id: SecretOrEnv.get("AWS_ACCESS_KEY_ID"),
-      secret_access_key: SecretOrEnv.get("AWS_SECRET_ACCESS_KEY"),
-      host: SecretOrEnv.get("AWS_API_HOST")
-    ]
-
   def get_logs(date, slug) do
     bucket = "changelog-logs-#{slug}"
 
     ExAws.S3.list_objects(bucket, prefix: date)
-    |> ExAws.request!(@config)
+    |> ExAws.request!(config())
     |> get_in([:body, :contents])
     |> Task.async_stream(fn %{key: key} -> get_log(bucket, key) end)
     |> Enum.map(fn {:ok, log} -> log end)
@@ -17,7 +11,15 @@ defmodule Changelog.Stats.S3 do
 
   defp get_log(bucket, key) do
     ExAws.S3.get_object(bucket, key)
-    |> ExAws.request!(@config)
+    |> ExAws.request!(config())
     |> Map.get(:body)
+  end
+
+  defp config do
+    [
+      access_key_id: SecretOrEnv.get("AWS_ACCESS_KEY_ID"),
+      secret_access_key: SecretOrEnv.get("AWS_SECRET_ACCESS_KEY"),
+      host: SecretOrEnv.get("AWS_API_HOST")
+    ]
   end
 end
