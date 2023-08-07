cd84f90794e7c36f7e6d3f946be2840f34003476
Jerod Santo <jerod@changelog.com>
2023-08-07 10:17:56 -0500

    Only read the S3 config values from env once per run

--- lib/changelog/stats/s3.ex
+++ lib/changelog/stats/s3.ex
@@ -2,24 +2,22 @@ defmodule Changelog.Stats.S3 do
   def get_logs(date, slug) do
     bucket = "changelog-logs-#{slug}"
 
-    ExAws.S3.list_objects(bucket, prefix: date)
-    |> ExAws.request!(config())
-    |> get_in([:body, :contents])
-    |> Task.async_stream(fn %{key: key} -> get_log(bucket, key) end)
-    |> Enum.map(fn {:ok, log} -> log end)
-  end
-
-  defp get_log(bucket, key) do
-    ExAws.S3.get_object(bucket, key)
-    |> ExAws.request!(config())
-    |> Map.get(:body)
-  end
-
-  defp config do
-    [
+    config = [
       access_key_id: SecretOrEnv.get("AWS_ACCESS_KEY_ID"),
       secret_access_key: SecretOrEnv.get("AWS_SECRET_ACCESS_KEY"),
       host: SecretOrEnv.get("AWS_API_HOST")
     ]
+
+    ExAws.S3.list_objects(bucket, prefix: date)
+    |> ExAws.request!(config)
+    |> get_in([:body, :contents])
+    |> Task.async_stream(fn %{key: key} -> get_log(bucket, key, config) end)
+    |> Enum.map(fn {:ok, log} -> log end)
+  end
+
+  defp get_log(bucket, key, config) do
+    ExAws.S3.get_object(bucket, key)
+    |> ExAws.request!(config)
+    |> Map.get(:body)
   end
 end
