ddd00d03e3bbe3c6b417facc92ebf981e79d306c
Gerhard Lazu <gerhard@users.noreply.github.com>
2023-09-20 11:00:54 +0200

    Limit app traces sent to Honeycomb.io (#482)
    * Limit app traces sent to Honeycomb.io to 10%
    
    Otherwise we will exceed out monthly 100M limit.
    
    FWIW: https://github.com/open-telemetry/opentelemetry-erlang/blob/4812195bfa52031dfceeddf6290d0a4cfbc5470c/apps/opentelemetry/src/otel_configuration.erl#L219-L237
    
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>
    
    * Enable gzip compression when sending event traces
    
    Typically results in ~80% less bandwidth used.
    
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>
    
    ---------
    
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>

--- config/config.exs
+++ config/config.exs
@@ -118,6 +118,7 @@ if System.get_env("HONEYCOMB_API_KEY") do
   config :opentelemetry, traces_exporter: :otlp
   config :opentelemetry_exporter,
     otlp_protocol: :http_protobuf,
+    otlp_compression: :gzip,
     otlp_endpoint: "https://api.honeycomb.io:443",
     otlp_headers: [
       {"x-honeycomb-team", SecretOrEnv.get("HONEYCOMB_API_KEY")},
