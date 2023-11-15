8c78bc26149633ae9c3e515ce3efab108ce3331f
Jerod Santo <jerod@changelog.com>
2023-11-15 12:53:01 -0600

    Teach S3 log lister to fetch *all* keys

--- lib/changelog/stats/s3.ex
+++ lib/changelog/stats/s3.ex
@@ -1,23 +1,39 @@
 defmodule Changelog.Stats.S3 do
-  def get_logs(date, slug) do
-    bucket = "changelog-logs-#{slug}"
-
-    config = [
+  def config do
+    [
       access_key_id: SecretOrEnv.get("AWS_ACCESS_KEY_ID"),
       secret_access_key: SecretOrEnv.get("AWS_SECRET_ACCESS_KEY"),
       host: SecretOrEnv.get("AWS_API_HOST")
     ]
+  end
 
-    ExAws.S3.list_objects(bucket, prefix: date)
-    |> ExAws.request!(config)
-    |> get_in([:body, :contents])
-    |> Task.async_stream(fn %{key: key} -> get_log(bucket, key, config) end)
+  def get_logs(date, slug) do
+    bucket = "changelog-logs-#{slug}"
+
+    list_logs(bucket, date)
+    |> Task.async_stream(fn %{key: key} -> get_log(bucket, key) end)
     |> Enum.map(fn {:ok, log} -> log end)
   end
 
-  defp get_log(bucket, key, config) do
+  defp get_log(bucket, key) do
     ExAws.S3.get_object(bucket, key)
-    |> ExAws.request!(config)
+    |> ExAws.request!(config())
     |> Map.get(:body)
+  end
+
+  @doc """
+  returns a list of log object data for a given bucket and prefix, continuing
+  past the S3's 1000 key limit when necessary
+  """
+  def list_logs(bucket, prefix, next \\ nil, accumulated \\ []) do
+    options = if next, do: [prefix: prefix, continuation_token: next], else: [prefix: prefix]
+    request = ExAws.S3.list_objects_v2(bucket, options)
+
+    case ExAws.request!(request, config()) do
+      %{body: %{is_truncated: "false", contents: contents}} ->
+        accumulated ++ contents
+      %{body: %{next_continuation_token: next, contents: contents}} ->
+        list_logs(bucket, prefix, next, accumulated ++ contents)
+    end
   end
 end
