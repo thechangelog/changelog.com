845da8476d35cc66c735a4ee70f2716be9738b51
Gerhard Lazu <gerhard@changelog.com>
2024-01-12 08:14:25 +0000

    Make :verify_peer work when connecting to Neon.tech
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>

--- config/prod.exs
+++ config/prod.exs
@@ -34,46 +34,17 @@ config :changelog, Changelog.Repo,
   username: System.get_env("DB_USER", "postgres"),
   ssl: true,
 
-  # 💥 According to https://neon.tech/docs/guides/elixir-ecto#configure-ecto, server_name_indication should work, but it fails with:
-  # ** (Postgrex.Error) ERROR 26000 (invalid_sql_statement_name) prepared statement "ecto_1922" does not exist
-  # ssl_opts: [
-  #   server_name_indication: String.to_charlist(System.get_env("DB_HOST", "db")),
-  #   verify: :verify_none
-  # ],
-
-  # 💥 According to https://neon.tech/docs/guides/elixir-ecto#configure-ecto, this should work, but it fails with:
-  # 17:41:53.361 [notice] Application changelog exited: Changelog.Application.start(:normal, []) returned an error: shutdown: failed to start child: Changelog.EpisodeTracker
-  #    ** (EXIT) an exception was raised:
-  #        ** (DBConnection.ConnectionError) connection not available and request was dropped from queue after 2955ms. This means requests are coming in and your connection pool cannot serve them fast enough. You can address this by:
-  #
-  #  1. Ensuring your database is available and that you can connect to it
-  #  2. Tracking down slow queries and making sure they are running fast enough
-  #  3. Increasing the pool_size (although this increases resource consumption)
-  #  4. Allowing requests to wait longer by increasing :queue_target and :queue_interval
-  #
-  #See DBConnection.start_link/2 for more information
-  #
-  #            (ecto_sql 3.10.2) lib/ecto/adapters/sql.ex:1047: Ecto.Adapters.SQL.raise_sql_call_error/1
-  #            (ecto_sql 3.10.2) lib/ecto/adapters/sql.ex:945: Ecto.Adapters.SQL.execute/6
-  #            (ecto 3.10.3) lib/ecto/repo/queryable.ex:229: Ecto.Repo.Queryable.execute/4
-  #            (ecto 3.10.3) lib/ecto/repo/queryable.ex:19: Ecto.Repo.Queryable.all/3
-  #            (changelog 0.0.1) lib/changelog/schema/episode/episode.ex:421: Changelog.Episode.flatten_for_filtering/1
-  #            (changelog 0.0.1) lib/changelog/episode_tracker.ex:130: Changelog.EpisodeTracker.refresh_episodes/0
-  #            (changelog 0.0.1) lib/changelog/episode_tracker.ex:57: Changelog.EpisodeTracker.init/1
-  #            (stdlib 5.2) gen_server.erl:980: :gen_server.init_it/2
-  # ssl_opts: [
-  #   cacerts: :public_key.cacerts_get(), # available since OTP26
-  #   verify: :verify_peer,
-  #   server_name_indication: 'ep-polished-lake-61433986-pooler.us-east-1.aws.neon.tech',
-  #   customize_hostname_check: [
-  #     match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-  #   ]
-  # ],
-  # password: SecretOrEnv.get("DB_PASS"),
-
-  # 🤔 This combined with prepending endpoint=<endpoint_id> is the only thing that works 🤔
-  ssl_opts: [ verify: :verify_none ],
-  password: SecretOrEnv.get("DB_ENDPOINT_AND_PASS"),
+  ssl_opts: [
+    # The cacertfile values implies Ubuntu,
+    # which is what we use when building the prod container image
+    cacertfile: "/etc/ssl/certs/ca-certificates.crt",
+    verify: :verify_peer,
+    server_name_indication: String.to_charlist(System.get_env("DB_HOST", "db")),
+    customize_hostname_check: [
+      match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+    ]
+  ],
+  password: SecretOrEnv.get("DB_PASS"),
   timeout: 60000,
   pool_size: 40
 
