02c052d33ddf177eb360bc4b088037ba032572a8
Gerhard Lazu <gerhard@changelog.com>
2024-01-13 13:15:32 +0000

    Fix app path for Fly.io config
    Signed-off-by: Gerhard Lazu <gerhard@changelog.com>

--- magefiles/image/fly.go
+++ magefiles/image/fly.go
@@ -109,7 +109,7 @@ func (image *Image) flyctl() *Image {
 
 func (image *Image) app() *Image {
 	image.container = image.container.
-		WithMountedFile("fly.toml", image.dag.Host().Directory("fly.io/changelog-2023-12-17").File("fly.toml"))
+		WithMountedFile("fly.toml", image.dag.Host().Directory("fly.io/changelog-2024-01-12").File("fly.toml"))
 
 	return image
 }
