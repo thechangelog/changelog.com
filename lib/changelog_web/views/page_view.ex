8780dbb976c02af5e883c8f54e8d94fed53b4a17
Jerod Santo <jerod@changelog.com>
2024-02-07 10:13:34 -0600

    Migrate guest guide to heex

--- lib/changelog_web/views/page_view.ex
+++ lib/changelog_web/views/page_view.ex
@@ -1,7 +1,7 @@
 defmodule ChangelogWeb.PageView do
   use ChangelogWeb, :public_view
 
-  alias Changelog.{Person, Podcast, Repo, Sponsor, StringKit}
+  alias Changelog.{Person, Podcast, Repo, Sponsor}
   alias ChangelogWeb.{EpisodeView, SponsorView, TimeView}
 
   def members_count, do: Repo.count(Person.joined())
