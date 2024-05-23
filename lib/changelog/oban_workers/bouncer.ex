031cd6fe77474a106e817f07071d3ccc6a7e24f8
Jerod Santo <jerod@changelog.com>
2024-05-23 10:48:33 -0700

    Add a function for deleting unconfirmed people

--- lib/changelog/oban_workers/bouncer.ex
+++ lib/changelog/oban_workers/bouncer.ex
@@ -3,9 +3,9 @@ defmodule Changelog.ObanWorkers.Bouncer do
   This module defines the Oban worker for getting hard bounces/spam from CM and
   deleting the associated people records from the system
   """
-  require Logger
 
-  alias Changelog.{EventLog, ListKit, Person, Repo}
+  alias Changelog.{EventLog, ListKit, Newsletters, Person, Repo}
+  alias ChangelogWeb.PersonView
   alias Craisin.Client
 
   use Oban.Worker, queue: :scheduled
@@ -41,6 +41,28 @@ defmodule Changelog.ObanWorkers.Bouncer do
     {deleted_count, _} = Person.with_email(spam) |> Repo.delete_all()
 
     log("Deleted #{deleted_count} people from #{length(spam)} emails marked as spam.")
+  end
+
+  def delete_unconfirmed do
+    older_than = Timex.shift(Timex.today(), weeks: -1)
+    people = Person.older_than(older_than) |> Person.needs_confirmation() |> Repo.all()
+
+    results = Enum.map(people, fn person ->
+      person = Person.preload_subscriptions(person)
+
+      if PersonView.is_subscribed(person, Newsletters.nightly()) do
+        false
+      else
+        log("Deleted #{person.id} #{person.name} (#{person.email})")
+        Repo.delete!(person)
+        true
+      end
+    end)
+
+    true_count = results |> Enum.filter(&(&1)) |> length()
+    false_count = results |> Enum.filter(&(!&1)) |> length()
+
+    log("Deleted #{true_count} unconfirmed people older than #{older_than}. #{false_count} skipped.")
   end
 
   defp log(message), do: EventLog.insert(message, "Bouncer")
