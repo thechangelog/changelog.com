54eb7e99d6f5fde190fa70511c0afbe022a1fcd1
Jerod Santo <jerod@changelog.com>
2024-08-20 10:37:33 -0500

    [admin] Minor improvements to person action links

--- lib/changelog_web/views/admin/person_view.ex
+++ lib/changelog_web/views/admin/person_view.ex
@@ -11,27 +11,30 @@ defmodule ChangelogWeb.Admin.PersonView do
 
   def list_of_links(person) do
     [
-      %{value: person.email, icon: "envelope", url: "mailto:#{person.email}"},
+      %{value: person.email, icon: "envelope", url: "mailto:#{person.email}", title: "Email"},
       %{
         value: person.twitter_handle,
         icon: "twitter",
-        url: SharedHelpers.twitter_url(person.twitter_handle)
+        url: SharedHelpers.twitter_url(person.twitter_handle),
+        title: "On Twitter"
       },
       %{
         value: person.github_handle,
         icon: "github",
-        url: SharedHelpers.github_url(person.github_handle)
+        url: SharedHelpers.github_url(person.github_handle),
+        title: "On GitHub"
       },
       %{
         value: person.linkedin_handle,
         icon: "linkedin",
-        url: SharedHelpers.linkedin_url(person.linkedin_handle)
+        url: SharedHelpers.linkedin_url(person.linkedin_handle),
+        title: "On LinkedIn"
       },
-      %{value: person.website, icon: "share", url: person.website}
+      %{value: person.website, icon: "share", url: person.website, title: "Website"}
     ]
     |> Enum.reject(fn x -> x.value == nil end)
     |> Enum.map(fn x ->
-      ~s{<a href="#{x.url}" class="ui icon button" target="_blank"><i class="#{x.icon} icon"></i></a>}
+      ~s{<a href="#{x.url}" title="#{x.title}" class="ui icon button" target="_blank"><i class="#{x.icon} icon"></i></a>}
     end)
     |> Enum.join("")
   end
