b98d4accd5f0f78482dd31d3a71e9b9dc07619f5
Jerod Santo <jerod@changelog.com>
2024-10-24 11:34:20 -0500

    Reduce Overcast ping rate limit

--- lib/changelog/oban_workers/overcast_pinger.ex
+++ lib/changelog/oban_workers/overcast_pinger.ex
@@ -1,11 +1,11 @@
 defmodule Changelog.ObanWorkers.OvercastPinger do
   @moduledoc """
   This module defines the Oban worker for pinging Overcast about new episodes.
-  Each unique URL can only be queued once every hour.
+  Each unique URL can only be queued once every five minutes.
   """
   use Oban.Worker,
     queue: :scheduled,
-    unique: [fields: [:args], period: 3600]
+    unique: [fields: [:args], period: {5, :minutes}]
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"url" => url}}) do
