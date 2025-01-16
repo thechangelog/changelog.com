db57966923705dc2bb13cb431c6b6d70886c1686
Jerod Santo <jerod@changelog.com>
2025-01-16 12:33:22 -0600

    Add YouTube ID to top of published episode form

--- assets/admin/views/episodeView.js
+++ assets/admin/views/episodeView.js
@@ -15,23 +15,25 @@ export default class EpisodeView {
         height: 400
       },
       yaxis: {
-         decimalsInFloat: 0,
-         labels: {
-          formatter: function(val) {
+        decimalsInFloat: 0,
+        labels: {
+          formatter: function (val) {
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-           }
-         }
+          }
+        }
       }
-    }
+    };
   }
 
   index() {
     new FilterWidget();
 
-    let scheduled = $(".ui.calendar").data("scheduled").map((string) => {
-      let date = new Date(string);
-      return date.toDateString();
-    });
+    let scheduled = $(".ui.calendar")
+      .data("scheduled")
+      .map((string) => {
+        let date = new Date(string);
+        return date.toDateString();
+      });
 
     $(".ui.calendar").calendar({
       type: "date",
@@ -48,10 +50,10 @@ export default class EpisodeView {
 
     let chartOptions = this.chartOptions;
 
-    $(".performance-chart").each(function(_index) {
+    $(".performance-chart").each(function (_index) {
       let container = this;
 
-      $.getJSON($(container).data("source"), function(data) {
+      $.getJSON($(container).data("source"), function (data) {
         let options = $.extend(chartOptions, {
           series: data.series,
           title: {
@@ -69,13 +71,16 @@ export default class EpisodeView {
           },
           tooltip: {
             x: {
-              formatter: function(value, {_series, seriesIndex, dataPointIndex, _w}) {
+              formatter: function (
+                value,
+                { _series, seriesIndex, dataPointIndex, _w }
+              ) {
                 let title = data.series[seriesIndex].data[dataPointIndex].title;
                 let slug = data.series[seriesIndex].data[dataPointIndex].x;
                 return `${slug}: ${title}`;
               }
             },
-            marker: {show: false}
+            marker: { show: false }
           }
         });
 
@@ -87,23 +92,25 @@ export default class EpisodeView {
 
   show() {
     let clipboard = new Clipboard(".clipboard.button", {
-      target: function(trigger) {
+      target: function (trigger) {
         return trigger.previousElementSibling;
       }
     });
 
-    clipboard.on("success", function(e) {
-      $(e.trigger).popup({variation: "inverted", content: "Copied!"}).popup("show");
+    clipboard.on("success", function (e) {
+      $(e.trigger)
+        .popup({ variation: "inverted", content: "Copied!" })
+        .popup("show");
       e.clearSelection();
     });
 
-    clipboard.on("error", function(e) {
+    clipboard.on("error", function (e) {
       console.log(e);
     });
 
     let chartOptions = this.chartOptions;
 
-    $(".chart").each(function(index) {
+    $(".chart").each(function (index) {
       let data = $(this).data("chart");
 
       let options = $.extend(chartOptions, {
@@ -122,70 +129,66 @@ export default class EpisodeView {
   }
 
   new() {
-    new SearchWidget("person", "episode", "episode_hosts")
-    new SearchWidget("person", "episode", "episode_guests")
-    new SearchWidget("sponsor", "episode", "episode_sponsors")
-    new SearchWidget("topic", "episode", "episode_topics")
-    new CalendarField(".ui.calendar")
-    new Modal(".js-title-guide-modal", ".title-guide.modal")
-    new Modal(".js-subtitle-guide-modal", ".subtitle-guide.modal")
-    new ChaptersWidget("audio", $(".js-episode_sponsors"))
-    new ChaptersWidget("plusplus", [])
+    new SearchWidget("person", "episode", "episode_hosts");
+    new SearchWidget("person", "episode", "episode_guests");
+    new SearchWidget("sponsor", "episode", "episode_sponsors");
+    new SearchWidget("topic", "episode", "episode_topics");
+    new CalendarField(".ui.calendar");
+    new Modal(".js-title-guide-modal", ".title-guide.modal");
+    new Modal(".js-subtitle-guide-modal", ".subtitle-guide.modal");
+    new ChaptersWidget("audio", $(".js-episode_sponsors"));
+    new ChaptersWidget("plusplus", []);
 
-    let clipboard = new Clipboard(".clipboard.button")
+    let clipboard = new Clipboard(".clipboard.button");
 
-    clipboard.on("success", function(e) {
-      $(e.trigger).popup({variation: "inverted", content: "Copied!"}).popup("show")
-    })
+    clipboard.on("success", function (e) {
+      $(e.trigger)
+        .popup({ variation: "inverted", content: "Copied!" })
+        .popup("show");
+    });
 
-    clipboard.on("error", function(e) { console.log(e) })
+    clipboard.on("error", function (e) {
+      console.log(e);
+    });
 
-    let requestedInput = $("input[name='episode[requested]']")
-    let requestSelect = $("select[name='episode[request_id]']")
+    let requestedInput = $("input[name='episode[requested]']");
+    let requestSelect = $("select[name='episode[request_id]']");
 
-    requestedInput.on("change", function() {
+    requestedInput.on("change", function () {
       if (requestedInput.is(":checked")) {
         requestSelect.closest(".field").removeClass("hidden");
       } else {
         requestSelect.closest(".field").addClass("hidden");
         requestSelect.dropdown("clear");
       }
-    })
+    });
 
-    let liveInput = $("input[name='episode[recorded_live]']")
-    let youTubeInputContainer = $("input[name='episode[youtube_id]']").closest("div")
-
-    liveInput.on("change", function () {
-      if (liveInput.is(":checked")) {
-        youTubeInputContainer.removeClass("hidden")
-      } else {
-        youTubeInputContainer.addClass("hidden")
-      }
-    })
-
-    $("form").on("change", ".js-time-in-seconds", function(event) {
-      let currentTime = $(event.target).val()
-      let newTime = parseTime(currentTime)
+    $("form").on("change", ".js-time-in-seconds", function (event) {
+      let currentTime = $(event.target).val();
+      let newTime = parseTime(currentTime);
 
       if (currentTime != newTime) {
-        $(event.target).val(newTime)
+        $(event.target).val(newTime);
       }
-    })
+    });
   }
 
   edit() {
-    this.new()
+    this.new();
 
-    new Modal(".js-publish-modal", ".publish.modal")
+    new Modal(".js-publish-modal", ".publish.modal");
 
-    let newsInput = $("input[name=news]")
-    let thanksInput = $("input[name=thanks]")
+    let newsInput = $("input[name=news]");
+    let thanksInput = $("input[name=thanks]");
 
-    newsInput.on("change", function() {
+    newsInput.on("change", function () {
       if (newsInput.is(":checked")) {
-        thanksInput.closest(".checkbox").checkbox("set enabled")
+        thanksInput.closest(".checkbox").checkbox("set enabled");
       } else {
-        thanksInput.closest(".checkbox").checkbox("set disabled").checkbox("uncheck")
+        thanksInput
+          .closest(".checkbox")
+          .checkbox("set disabled")
+          .checkbox("uncheck");
       }
     });
   }
